709e3513731642b049ea7adca6b9f8cb0aa86aa3
Adam LaDine <captainmeta4@users.noreply.github.com>
2019-07-04 15:08:50 -0400

    app.run not needed with gunicorn

--- teedee/app.py
+++ teedee/app.py
@@ -1,13 +1,7 @@
 from teedee.creds.config import app
 from teedee.classes.dbModels import *
-
-
-
+from os import environ
 
 @app.route("/test")
 def test():
     return "Hello Bros"
-
-
-if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
