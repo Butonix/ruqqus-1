fed06584ce60939635610979bf8e611204b3a921
arkitekt <exodust@protonmail.com>
2019-07-04 13:11:26 -0400

    add ips to dbModels

--- teedee/classes/dbModels.py
+++ teedee/classes/dbModels.py
@@ -1,9 +1,9 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import *
 from os import environ
-from __init__ import app
+from teedee.classes.__init__ import app
 
 DBUNAME = "root"
 DBPASS = ""
@@ -31,6 +31,8 @@ class User(Base):
     is_admin = Column(Boolean, default=False)
     is_mod = Column(Boolean, default=False)
     is_banned = Column(Boolean, default=False)
+    ips = relationship('IPs', backref='users')
+
 
     def hashPass(self, password):
         self.hash = generate_password_hash(password, method='pbkdf2:sha512', salt_length=8)
@@ -42,10 +44,19 @@ class User(Base):
     def __repr__(self):
         return "<User(id=%s, username=%s, email=%s, passhash=%s, " \
                "created_utc=%s, is_activated=%s, is_admin=%s, " \
-               "is_mod=%s, is_banned=%s)>" \
+               "is_mod=%s, is_banned=%s, ips=%s)>" \
                "" % (self.id, self.username, self.email, self.passhash,
                     self.created_utc, self.is_activated, self.is_admin,
-                     self.is_mod, self.is_banned)
+                     self.is_mod, self.is_banned, self.ips)
+class IPs(Base):
+    __tablename__ = "ips"
+    id = Column(BigInteger, primary_key=True)
+    uid = Column(BigInteger, ForeignKey(User.id), default=0)
+    ip = Column(VARCHAR(40), default=None)
+
+    def __repr__(self):
+        return "<Ips(id=%s, uid=%s, ip=%s)>" % (self.id, self.uid, self.ip)
+
 
 class Submission(Base):
 
