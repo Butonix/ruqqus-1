3ead4dfed06f537ddcd806bcdc8357558ca57474
Adam LaDine <captainmeta4@users.noreply.github.com>
2019-07-04 15:22:16 -0400

    Delete config.py

--- teedee/creds/config.py
+++
@@ -1,8 +0,0 @@
-from teedee.creds.credentials import *
-from os import environ
-from flask import Flask
-
-
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
