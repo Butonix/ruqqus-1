7264c3399718bf1e2541960d060d8adaaebc82af
Adam LaDine <captainmeta4@users.noreply.github.com>
2019-07-04 15:07:28 -0400

    Update config.py

--- teedee/creds/config.py
+++ teedee/creds/config.py
@@ -1,10 +1,8 @@
 from teedee.creds.credentials import *
+from os import environ
 from flask import Flask
 
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{0}:{1}@127.0.0.1/{2}?host=127.0.0.1?port={3}/charset=utf8/' \
-                                        ''.format(DBUNAME, DBPASS, DB, DBPORT)
-
-
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
