e6112b7080c95b8d24507f5943464635191d9ebc
arkitekt <exodust@protonmail.com>
2019-07-04 00:58:26 -0400

    dev wsgi app

--- teedee/classes/__init__.py
+++ teedee/classes/__init__.py
@@ -1,1 +1,12 @@
 from dbModels import *
+from flask import Flask
+
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+@app.route("/")
+def index():
+    return "Hello Bros"
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
