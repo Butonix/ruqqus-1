b396af0b69806d2c18cd9bf03c76c94fd0622338
Adam LaDine <captainmeta4@users.noreply.github.com>
2019-07-04 15:23:10 -0400

    Update __main__.py

--- teedee/__main__.py
+++ teedee/__main__.py
@@ -1,5 +1,10 @@
-from teedee.creds.config import app
 from teedee.classes.dbModels import *
+from os import environ
+from flask import *
+
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
 
 @app.route("/test")
 def test():
