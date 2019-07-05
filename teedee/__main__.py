48b22b7eecb0ff705884eda835f5c0b7850c01ca
Adam LaDine <captainmeta4@users.noreply.github.com>
2019-07-05 19:09:37 -0400

    Create style.css (#4)
    * Create style.css
    
    * add templates
    
    * add error handling
    
    * Delete .DS_Store
    
    * Update 500.html

--- teedee/__main__.py
+++ teedee/__main__.py
@@ -10,6 +10,26 @@ app = Flask(__name__,
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
 
+@app.errorhandler(401)
+def error_401(e):
+    return render_template('401.html'), 401
+
+@app.errorhandler(403)
+def error_403(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(404)
+def error_404(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(405)
+def error_405(e):
+    return render_template('405.html'), 405
+
+@app.errorhandler(500)
+def error_500(e):
+    return render_template('500.html', e=e), 500
+
 @app.route("/test")
 def test():
     return "Hello Bros"
---
+++ teedee/templates/readme.md
@@ -0,0 +1,6 @@
+# Variable conventions within templates
+
+* v - the logged in viewer
+* u - the user (whose page is being viewed)
+* s - submission
+* c - comment
