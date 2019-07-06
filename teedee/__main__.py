e1e125602b58f054556ccb8307ceeccc3cdbb28d
captainmeta4 <admin@MAGAbook.local>
2019-07-05 22:36:41 -0400

    Update __main__.py

--- teedee/__main__.py
+++ teedee/__main__.py
@@ -1,9 +1,7 @@
 from teedee.classes.dbModels import *
 from os import environ
 from flask import *
-from flask.ext.session import Session
 from teedee.helpers import *
-from sqlalchemy import func
 
 app = Flask(__name__,
             template_folder='./templates',
@@ -13,10 +11,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
 app.config['SECRET_KEY']=environ.get('Flask_secret_key')
 app.config["SERVER_NAME"]="tee-dee.herokuapp.com"
-app.config["SESSION_TYPE"]="sqlalchemy"
-app.config["SESSION_USE_SIGNER"]=True
-
-Session(app)
 
 #import and bind all routing functions
 from teedee.routes import *
