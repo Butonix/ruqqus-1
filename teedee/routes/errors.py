734c5b92efe1fe2f056782f1ffe2c82f51c3d723
captainmeta4 <admin@MAGAbook.local>
2019-07-05 21:33:08 -0400

    allow user-specific info to show up on error pages

--- teedee/routes/errors.py
+++ teedee/routes/errors.py
@@ -1,22 +1,28 @@
 from teedee.__main__ import app
+from teedee.helpers.wrappers import *
 
 #Errors
 @app.errorhandler(401)
-def error_401(e):
-    return render_template('401.html'), 401
+@auth_desired
+def error_401(e, v):
+    return render_template('401.html', v=v), 401
 
 @app.errorhandler(403)
-def error_403(e):
+@auth_desired
+def error_403(e, v):
     return render_template('403.html'), 403
 
 @app.errorhandler(404)
-def error_404(e):
+@auth_desired
+def error_404(e, v):
     return render_template('404.html'), 404
 
 @app.errorhandler(405)
-def error_405(e):
+@auth_desired
+def error_405(e, v):
     return render_template('405.html'), 405
 
 @app.errorhandler(500)
-def error_500(e):
-    return render_template('500.html', e=e), 500
+@auth_desired
+def error_500(e, v):
+    return render_template('500.html', e=e, v=v), 500
