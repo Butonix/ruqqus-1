a8a57c21e517f00ac78868e982100fa30dc8989b
captainmeta4 <admin@MAGAbook.local>
2019-07-06 11:05:44 -0400

    Update login.py

--- teedee/routes/login.py
+++ teedee/routes/login.py
@@ -59,5 +59,11 @@ def logout(v):
     session.pop("session_id", None)
 
     return redirect("/")
-        
+
+@app.route("/make-session-id")
+def make_id(v):
+
+    session["session_id"]=token_hex(16)
+
+    return redirect("/")
     
