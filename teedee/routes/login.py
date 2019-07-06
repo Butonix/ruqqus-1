a06f0f99fa110725c4dc0cebd13a081ce04d0627
captainmeta4 <admin@MAGAbook.local>
2019-07-06 19:41:47 -0400

    Update login.py

--- teedee/routes/login.py
+++ teedee/routes/login.py
@@ -164,8 +164,8 @@ def sign_up_post(v):
         return new_signup("Password must be 8 characters or longer")
 
     #Check for existing acocunts
-    if (db.query(User).filter(func.lower(email)==func.lower(request.form.get("email"))).first()
-        or db.query(User).filter(func.lower(username)==func.lower(request.form.get("username"))).first()):
+    if (db.query(User).filter(func.lower(User.email)==func.lower(request.form.get("email"))).first()
+        or db.query(User).filter(func.lower(User.username)==func.lower(request.form.get("username"))).first()):
         return new_signup("An account with that username or email already exists.")
     
     #success
