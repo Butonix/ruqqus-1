3391e1c8b2dd79d89491eaff9a112549f58b0118
Adam LaDine <captainmeta4@users.noreply.github.com>
2019-07-06 09:34:33 -0400

    Update wrappers.py

--- teedee/helpers/wrappers.py
+++ teedee/helpers/wrappers.py
@@ -38,6 +38,33 @@ def auth_required(f):
     wrapper.__name__=f.__name__
     return wrapper
 
+def is_not_banned(f):
+    #decorator that enforces lack of ban
+
+    def wrapper(*args, **kwargs):
+
+        if "user_id" in session:
+            v=db.query(User).filter_by(id=session["user_id"]).all()
+            if not v:
+                abort(401)
+            v=v[0]
+            
+            if v.is_banned:
+                abort(403)
+            
+            if args:
+                args=tuple(list(args).append(v))
+            else:
+                args=(v,)
+        else:
+            abort(401)
+
+        return f(*args, **kwargs)
+
+    wrapper.__name__=f.__name__
+    return wrapper
+    
+
 def admin_level_required(f, x):
     #decorator for any api that requires admin perms
 
