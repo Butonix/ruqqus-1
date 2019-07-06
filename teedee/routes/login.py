5231cf708f8e2ec3ee5ad46379b3d00a4e951579
Adam LaDine <captainmeta4@users.noreply.github.com>
2019-07-06 15:45:08 -0400

    Update login.py

--- teedee/routes/login.py
+++ teedee/routes/login.py
@@ -3,6 +3,9 @@ from teedee.classes import *
 from teedee.helpers.wrappers import *
 from teedee.helpers.base36 import *
 from secrets import token_hex
+from time import time
+import hmac
+
 from teedee.__main__ import app
 
 #login form
@@ -60,3 +63,96 @@ def logout(v):
     session.pop("session_id", None)
 
     return redirect("/")
+
+#signing up
+@app.route("/signup", methods=["GET"])
+@auth_desired
+def sign_up_get(v):
+    if v:
+        return redirect("/")
+    
+    agent=request.headers.get("User-Agent", None)
+    if not agent:
+        abort(403)
+    
+    #Make a unique form key valid for one account creation
+    now = int(time())
+    token = token_hex(16)
+    session["signup_token"]=token
+    ip=request.remote_addr
+    
+    formkey_hashstr = str(now)+token+ip+agent
+
+    
+    #formkey is a hash of session token, timestamp, and IP address
+    formkey = hmac(key=bytes(os.environ.get("MASTER_KEY"), "utf-16"),
+                   msg=bytes(formkey_hashstr, "utf-16")
+                  ).hexdigest()
+    
+    return render_template("sign_up.html", formkey=formkey, now=now)
+
+#signup api
+@app.route("/signup", methods=["POST"])
+@auth_desired
+def sign_up_post(v):
+    if v:
+        abort(403)
+        
+    agent=request.headers.get("User-Agent", None)
+    if not agent:
+        abort(403)
+    
+    form_timestamp = request.form.get("now", 0)
+    form_formkey = request.form.get("formkey","none")
+    
+    submitted_token=session["signup_token"]
+    ip=request.remote_addr
+    
+    correct_formkey_hashstr = str(form_timestamp)+submitted_token+ip+agent
+    
+    correct_formkey = hmac(key=bytes(os.environ.get("MASTER_KEY"), "utf-16"),
+                   msg=bytes(correct_formkey_hashstr, "utf-16")
+                  ).hexdigest()
+    
+    now=int(time())
+
+    if (now-form_timestamp>120
+        or not hmac.compare_digest(form_formkey, correct_formkey)
+        or not request.form.get("password") == request.form.get("password_confirm")
+       ):
+        
+        #Reset tokens and return to signup form
+        
+        token = token_hex(16)
+        session["signup_token"]=token
+
+        new_formkey_hashstr=str(now)+submitted_token+ip+agent
+        correct_formkey = hmac(key=bytes(os.environ.get("MASTER_KEY"), "utf-16"),
+                   msg=bytes(correct_formkey_hashstr, "utf-16")
+                  ).hexdigest()
+        
+        return render_template("sign_up.html", formkey=correct_formkey, now=now, error="There was a problem. Please try again.")
+    
+    
+    #success
+    
+    #kill tokens
+    session.pop("signup_token")
+    
+    session["session_id"]=token_hex(16)
+    
+    #make new user
+    try:
+        new_user=User(username=request.form.get("username"),
+                  password=request.form.get("password"),
+                  email=request.form.get("email")
+                 )
+    except:
+        abort(500)
+    
+    db.add(new_user)
+    db.commit()
+    
+    v=db.query(User).filter_by(username=request.form.get("username")).first()
+    return redirect(v.url)
+    
