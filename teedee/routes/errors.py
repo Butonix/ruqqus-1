41e63f37b0ffcc0843a74203999f8c063fef67d1
captainmeta4 <admin@MAGAbook.local>
2019-07-05 23:37:47 -0400

    Update errors.py

--- teedee/routes/errors.py
+++ teedee/routes/errors.py
@@ -5,25 +5,25 @@ from teedee.__main__ import app
 #Errors
 @app.errorhandler(401)
 @auth_desired
-def error_401(e, v):
+def error_401(e, v=None):
     return render_template('401.html', v=v), 401
 
 @app.errorhandler(403)
 @auth_desired
-def error_403(e, v):
+def error_403(e, v=None):
     return render_template('403.html'), 403
 
 @app.errorhandler(404)
 @auth_desired
-def error_404(e, v):
+def error_404(e, v=None):
     return render_template('404.html'), 404
 
 @app.errorhandler(405)
 @auth_desired
-def error_405(e, v):
+def error_405(e, v=None):
     return render_template('405.html'), 405
 
 @app.errorhandler(500)
 @auth_desired
-def error_500(e, v):
+def error_500(e, v=None):
     return render_template('500.html', e=e, v=v), 500
