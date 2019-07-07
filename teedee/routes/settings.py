3f7af64f83f990e48341b09585b09d06cabb7e75
captainmeta4 <admin@MAGAbook.local>
2019-07-06 22:22:05 -0400

    fix settings

--- teedee/routes/settings.py
+++ teedee/routes/settings.py
@@ -3,7 +3,7 @@ from teedee.classes import *
 from teedee.helpers.wrappers import *
 from teedee.__main__ import db, app
 
-@app.route("/settings", methods=["POST"])
+@app.route("/api/settings", methods=["POST"])
 @auth_required
 @validate_formkey
 def settings_post(v):
@@ -13,4 +13,4 @@ def settings_post(v):
     db.add(v)
     db.commit()
 
-    return render_template("settings.html", v=v, msg="Settings updated")
+    return redirect("/settings")
