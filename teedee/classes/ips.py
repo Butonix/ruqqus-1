03a2ffd2669d44db9d28d93224fc751c6855c051
Adam LaDine <captainmeta4@users.noreply.github.com>
2019-07-07 12:30:43 -0400

    Update ips.py

--- teedee/classes/ips.py
+++ teedee/classes/ips.py
@@ -2,13 +2,13 @@ from teedee.helpers.base36 import *
 from teedee.__main__ import Base, db
 from time import strftime
 from sqlalchemy import *
-from .user import User
+from sqlalchemy.orm import relationship
 
     
 class IP(Base):
     __tablename__ = "ips"
     id = Column(BigInteger, primary_key=True)
-    user_id = Column(BigInteger, ForeignKey(User.id), default=0)
+    user_id = Column(Integer, ForeignKey("User.id"))
     ip = Column(VARCHAR(40), default=None)
 
     def __repr__(self):
