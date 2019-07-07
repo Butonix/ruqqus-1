8cfa353b074d93ac044fb31ea32da51ee5b6270e
captainmeta4 <admin@MAGAbook.local>
2019-07-06 22:06:02 -0400

    prefs api, also some admin

--- teedee/routes/admin_api.py
+++ teedee/routes/admin_api.py
@@ -37,3 +37,29 @@ def unban_user(user_id, v):
     db.commit()
     
     return redirect(user.url)
+
+@app.route("/api/promote/<user_id>/<level>", methods=["POST"])
+@admin_level_required(1)
+@validate_formkey
+def promote(user_id, level, v):
+
+    u = db.query(User).filter_by(id=user_id).first()
+    if not u:
+        abort(404)
+
+    max_level_involved = max(level, u.admin_level)
+
+    try:
+        admin_level_needed={1:3, 2:3, 3:5, 4:5, 5:6}[max_level_involved]
+    except KeyError:
+        abort(400)
+
+    if v.admin_level < admin_level_needed:
+        abort(403)
+
+    u.admin_level=level
+
+    db.add(u)
+    db.commit()
+    
+    
---
+++ teedee/routes/settings.py
@@ -0,0 +1,14 @@
+from flask import *
+from teedee.classes import *
+from teedee.helpers.wrappers import *
+from teedee.__main__ import db, app
+
+@app.route("/settings", methods=["POST"])
+@auth_required
+@validate_formkey
+def settings_post(v):
+
+    v.over_18=request.form.get("over18", False)
+
+    db.add(v)
+    db.commit()
