ff2463c794471ab3b522edeab37d205c867b3edb
captainmeta4 <admin@MAGAbook.local>
2019-07-07 20:46:10 -0400

    Update user.py

--- teedee/classes/user.py
+++ teedee/classes/user.py
@@ -81,7 +81,7 @@ class User(Base):
 
         posts = [post for post in self.submissions]
         print(posts)
-        posts = posts.sort(key=lambda x: x.created_utc, reverse=True)
+        posts.sort(key=lambda x: x.created_utc, reverse=True)
         print(posts)
         if len(posts)>50:
             posts=posts[0:50]
