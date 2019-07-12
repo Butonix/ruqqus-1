1cf7891c99ed451c78cfa34d392a720d0eeb859b
captainmeta4 <admin@MAGAbook.local>
2019-07-12 09:52:10 -0400

    change password

--- teedee/routes/settings.py
+++ teedee/routes/settings.py
@@ -8,9 +8,28 @@ from teedee.__main__ import db, app
 @validate_formkey
 def settings_post(v):
 
-    v.over_18=bool(request.form.get("over18", None))
+    updated=False
 
-    db.add(v)
-    db.commit()
+    if request.form.get("new_password"):
+        if request.form.get("new_password") != request.form.get("cnf_password"):
+            return render_template("settings.html", v=v, error="Passwords do not match.")
 
-    return render_template("settings.html", v=v, msg="Your settings have been saved")
+        if not v.confirm_password(request.form.get("old_password")):
+            return render_template("settings.html", v=v, error="Incorrect password")
+
+        v.passhash=v.hash_password(request.form.get("new_password"))
+        updated=True
+                                  
+
+    if request.form.get("over18") != v.over_18
+        updated=True
+        v.over_18=bool(request.form.get("over18", None))
+
+    if updated:
+        db.add(v)
+        db.commit()
+
+        return render_template("settings.html", v=v, msg="Your settings have been saved.")
+
+    else:
+        return render_template("settings.html", v=v, error="You didn't change anything.")
