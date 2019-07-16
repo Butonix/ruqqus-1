b8b7ab40bed9874cc2d83d826233d506721b1aee
captainmeta4 <admin@MAGAbook.local>
2019-07-16 14:25:47 -0400

    Update filters.py

--- ruqqus/helpers/filters.py
+++ ruqqus/helpers/filters.py
@@ -4,6 +4,6 @@ config=yaml.load(open("banned.txt", "r+"))
 
 def filter_post(post):
 
-    if any([post.domain.endswith(x) for x in config["nosubmit"]):
+    if any([post.domain.endswith(x) for x in config["nosubmit"]]):
         return render_template("submit.html", error=f"{post.domain} isn't allowed.")
 
