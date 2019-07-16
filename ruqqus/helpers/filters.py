85bee64a2a21dc1e058f0a1f31853b910401f731
captainmeta4 <admin@MAGAbook.local>
2019-07-16 15:20:49 -0400

    Update filters.py

--- ruqqus/helpers/filters.py
+++ ruqqus/helpers/filters.py
@@ -1,10 +1,12 @@
 import yaml
 from flask import *
 
-config=yaml.load(open("/app/ruqqus/helpers/banned.txt", "r+"))
+config=yaml.safe_load(open("/app/ruqqus/helpers/banned.txt", "r+"))
 
 def filter_post(post):
 
     if any([post.domain.endswith(x) for x in config["nosubmit"]]):
         return f"Domain {post.domain} isn't allowed."
+
+    return False
 
