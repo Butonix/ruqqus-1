d54ca22cbbc98ad4535c6369271bebf7cedabcf7
captainmeta4 <admin@MAGAbook.local>
2019-07-18 19:45:09 -0400

    Update jinja2.py

--- ruqqus/helpers/jinja2.py
+++ ruqqus/helpers/jinja2.py
@@ -1,7 +1,7 @@
 import time
 
 from ruqqus.__main__ import app, db
-from ruqqus.classes import IP
+from ruqqus.classes import IP, User
 
 @app.template_filter("users_here")
 def users_here(x):
@@ -10,5 +10,9 @@ def users_here(x):
 
     cutoff=now-300
 
-    return len(db.query(IP).filter(IP.created_utc>=cutoff).all())
+    return len(db.query(User).join(IP).filter(IP.created_utc>=cutoff).all())
 
+@app.template_filter("total_users")
+def total_users(x):
+
+    return len(db.query(User).filter_by(is_banned=False).all())
