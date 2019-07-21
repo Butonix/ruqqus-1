c23e9e454d86b0441f03869eabb1a3e63787422f
captainmeta4 <captainmeta4@gmail.com>
2019-07-20 23:47:04 -0400

    Update comment.py

--- ruqqus/classes/comment.py
+++ ruqqus/classes/comment.py
@@ -25,6 +25,14 @@ class Comment(Base):
     body_html = Column(String)
     votes = relationship("CommentVote")
 
+    def __init__(self, *args, **kwargs):
+
+        if "created_utc" not in kwargs:
+            kwargs["created_utc"]=int(time.time())
+            kwargs["created_str"]=time.strftime("%I:%M %p on %d %b %Y", time.gmtime(kwargs["created_utc"]))
+        
+        super().__init__(*args, **kwargs)
+        
     def __repr__(self):
         return f"<Comment(id={self.id})"
  
