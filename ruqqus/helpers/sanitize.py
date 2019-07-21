81ee7edb542cdfeac5ab8aca098a12f5844623b0
captainmeta4 <captainmeta4@gmail.com>
2019-07-21 08:26:44 -0400

    Update sanitize.py

--- ruqqus/helpers/sanitize.py
+++ ruqqus/helpers/sanitize.py
@@ -22,11 +22,11 @@ _allowed_attributes={'a': ['href', 'title']}
 _allowed_protocols=['http', 'https']
 
 #filter to make all links show domain on hover
-def _url_on_hover(attrs, new=False):
-    attrs["title"]=urlparse(attrs["href"]).netloc
-    return attrs
+##def _url_on_hover(attrs, new=False):
+##    attrs["title"]=urlparse(attrs["href"]).netloc
+##    return attrs
 
-_callback_functions=bleach.linkifier.DEFAULT_CALLBACKS+[_url_on_hover]
+##_callback_functions=bleach.linkifier.DEFAULT_CALLBACKS+[_url_on_hover]
 
 _clean_wo_links = bleach.Cleaner(tags=_allowed_tags,
                                   attributes=_allowed_attributes,
@@ -38,7 +38,6 @@ _clean_w_links = bleach.Cleaner(tags=_allowed_tags,
                                   filters=[partial(LinkifyFilter,
                                                    skip_tags=["pre"],
                                                    parse_email=False,
-                                                   callbacks=_callback_functions
                                                    )
                                           ]
                                   )
