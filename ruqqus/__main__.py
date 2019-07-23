aeb88d17babfa7aa095e1e4f540864f64d716985
captainmeta4 <captainmeta4@gmail.com>
2019-07-22 20:22:18 -0400

    increment version

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import *
 
-_version = "0.1.1"
+_version = "0.2.0"
 
 app = Flask(__name__,
             template_folder='./templates',
