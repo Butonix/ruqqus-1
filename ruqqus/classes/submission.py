06f60aff550f4d66f80ac3c1c3bfafb1f250960a
captainmeta4 <captainmeta4@gmail.com>
2019-07-23 09:47:42 -0400

    fix comment count

--- ruqqus/classes/submission.py
+++ ruqqus/classes/submission.py
@@ -142,7 +142,7 @@ class Submission(Base):
     @property
     @_lazy
     def comment_count(self):
-        return len(self.comments)
+        return self.comments.count()
 
     def tree_comments(self):
 
