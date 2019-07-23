397b0d32bfea0ea320982c974df7c000258e631e
captainmeta4 <captainmeta4@gmail.com>
2019-07-23 08:52:54 -0400

    comment js

---
+++ ruqqus/assets/js/comment_replies.js
@@ -0,0 +1,49 @@
+// https://stackoverflow.com/a/42183824/11724748
+
+function toggleDropdown(e) {
+    const _d = $(e.target).closest('.dropdown'),
+        _m = $('.dropdown-menu', _d);
+    setTimeout(function () {
+        const shouldOpen = e.type !== 'click' && _d.is(':hover');
+        _m.toggleClass('show', shouldOpen);
+        _d.toggleClass('show', shouldOpen);
+        $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+    }, e.type === 'mouseleave' ? 150 : 0);
+}
+
+// Display profile card on hover
+
+$('body')
+    .on('mouseenter mouseleave', '.user-profile', toggleDropdown)
+    .on('click', '.dropdown-menu a', toggleDropdown);
+
+// Toggle comment collapse
+
+$(".toggle-collapse").click(function (event) {
+    event.preventDefault();
+
+    var id = $(this).parent().attr("id");
+
+    document.getElementById(id).classList.toggle("collapsed");
+});
+
+// Reply to parent comment
+
+function addReplyForm(commentId) {
+
+    var id = "reply-to-" + commentId;
+
+    document.getElementById(id).innerHTML = '<div class="comment-write child"> <form id="reply-to-t3_'+commentId+'" action="/api/comment" method="post" class="input-group"> <input type="hidden" name="formkey" value="'+formkey()+'"> <input type="hidden" name="parent_fullname" value="t3_'+commentId+'"> <input type="hidden" name="submission" value="{{ p.base36id }}"> <textarea name="body" form="reply-to-{{ p.fullname }}" class="form-control rounded" aria-label="With textarea" placeholder="Add your comment..." rows="7"></textarea> <div class="bg-light comment-format"> <small class="format"><i class="fas fa-bold" aria-hidden="true"></i></small> <small class="format"><i class="fas fa-italic" aria-hidden="true"></i></small> <small class="format"><i class="fas fa-quote-right" aria-hidden="true"></i></small> <small class="format"><i class="fas fa-link" aria-hidden="true"></i></small> <button onclick="delReplyForm('+commentId+')" class="btn btn-link text-danger ml-auto cancel-form">Cancel</button> <button form="reply-to-t3_'+commentId+'" class="btn btn-primary ml-2">Comment</button> </div> </form> </div>';
+
+}
+
+    // Removes reply form innerHTML on click
+
+function delReplyForm(commentId) {
+
+    var id = "reply-to-" + commentId;
+
+    document.getElementById(id).innerHTML = '';
+
+});
+
---
+++ ruqqus/assets/js/voting.js
@@ -0,0 +1,84 @@
+
+
+function vote(post_id, direction) {
+url="/api/vote/post/"+post_id+"/"+direction;
+post(url);
+
+thing = document.getElementById("post-"+post_id);
+uparrow1=document.getElementById("post-"+post_id+"-up");
+downarrow1=document.getElementById("post-"+post_id+"-down");
+scoreup1=document.getElementById("post-"+post_id+"-score-up");
+scorenone1=document.getElementById("post-"+post_id+"-score-none");
+scoredown1=document.getElementById("post-"+post_id+"-score-down");
+
+if (direction=="1") {
+thing.classList.add("upvoted");
+thing.classList.remove("downvoted");
+uparrow1.onclick=function(){vote(post_id, 0)};
+downarrow1.onclick=function(){vote(post_id, -1)};
+scoreup1.classList.remove("noshow");
+scorenone1.classList.add("noshow");
+scoredown1.classList.add("noshow");
+}
+else if (direction=="-1"){
+thing.classList.remove("upvoted");
+thing.classList.add("downvoted");
+uparrow1.onclick=function(){vote(post_id, 1)};
+downarrow1.onclick=function(){vote(post_id, 0)};
+scoreup1.classList.add("noshow");
+scorenone1.classList.add("noshow");
+scoredown1.classList.remove("noshow");
+}
+else if (direction=="0"){
+thing.classList.remove("upvoted");
+thing.classList.remove("downvoted");
+uparrow1.onclick=function(){vote(post_id, 1)};
+downarrow1.onclick=function(){vote(post_id, -1)};
+scoreup1.classList.add("noshow");
+scorenone1.classList.remove("noshow");
+scoredown1.classList.add("noshow");
+}
+
+}
+
+
+function vote_comment(comment_id, direction) {
+url="/api/vote/comment/"+ comment_id +"/"+direction;
+post(url);
+
+thing = document.getElementById("comment-"+ comment_id+"-voting");
+uparrow1=document.getElementById("comment-"+ comment_id +"-up");
+downarrow1=document.getElementById("comment-"+ comment_id +"-down");
+scoreup1=document.getElementById("comment-"+ comment_id +"-score-up");
+scorenone1=document.getElementById("comment-"+ comment_id +"-score-none");
+scoredown1=document.getElementById("comment-"+ comment_id +"-score-down");
+
+if (direction=="1") {
+thing.classList.add("upvoted");
+thing.classList.remove("downvoted");
+uparrow1.onclick=function(){vote_comment(comment_id, 0)};
+downarrow1.onclick=function(){vote_comment(comment_id, -1)};
+scoreup1.classList.remove("noshow");
+scorenone1.classList.add("noshow");
+scoredown1.classList.add("noshow");
+}
+else if (direction=="-1"){
+thing.classList.remove("upvoted");
+thing.classList.add("downvoted");
+uparrow1.onclick=function(){vote_comment(comment_id, 1)};
+downarrow1.onclick=function(){vote_comment(comment_id, 0)};
+scoreup1.classList.add("noshow");
+scorenone1.classList.add("noshow");
+scoredown1.classList.remove("noshow");
+}
+else if (direction=="0"){
+thing.classList.remove("upvoted");
+thing.classList.remove("downvoted");
+uparrow1.onclick=function(){vote_comment(comment_id, 1)};
+downarrow1.onclick=function(){vote_comment(comment_id, -1)};
+scoreup1.classList.add("noshow");
+scorenone1.classList.remove("noshow");
+scoredown1.classList.add("noshow");
+}
+
+}
