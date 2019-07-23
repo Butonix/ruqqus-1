05f978ab6d698c840fea911e7b6ea77f84d1f05b
captainmeta4 <captainmeta4@gmail.com>
2019-07-23 09:24:12 -0400

    comment parent logic

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import *
 
-_version = "0.2.6"
+_version = "0.2.7"
 
 app = Flask(__name__,
             template_folder='./templates',
