bfacf5e55e9e5eb3c26d92a07ae45aeff828f94f
captainmeta4 <captainmeta4@gmail.com>
2019-07-26 19:22:00 -0400

    Update filters.py

--- ruqqus/helpers/filters.py
+++ ruqqus/helpers/filters.py
@@ -6,6 +6,8 @@ config=yaml.safe_load(open(environ.get("banlist"), "r+"))
 
 def filter_post(post):
 
+    print(post.domain)
+
     if any([post.domain.endswith(x) for x in config["nosubmit"]]):
         return f"Domain {post.domain} isn't allowed."
 
