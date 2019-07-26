bb186054e863def92bae85de955c1824de8c6000
captainmeta4 <captainmeta4@gmail.com>
2019-07-26 07:54:18 -0400

    WIP build

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import *
 
-_version = "0.3.3.2"
+_version = "0.3.3.3"
 
 app = Flask(__name__,
             template_folder='./templates',
