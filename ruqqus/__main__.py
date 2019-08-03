35fc92c46b640e345c843fbd4f9453356c1a56d8
captainmeta4 <captainmeta4@gmail.com>
2019-08-03 08:05:00 -0400

    patch build

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import *
 
-_version = "0.3.5.4"
+_version = "0.3.6"
 
 app = Flask(__name__,
             template_folder='./templates',
