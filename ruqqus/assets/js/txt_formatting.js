4d6321b5cb49d97bc50ade6df92f4624985d0010
kek7198 <memezdank140@gmail.com>
2019-11-14 15:13:01 -0600

    reply comment formatting

--- ruqqus/assets/js/txt_formatting.js
+++ ruqqus/assets/js/txt_formatting.js
@@ -20,10 +20,54 @@ makeBold = function () {
     }
 }
 
-// Bold Text
+// Italicize Comment Text
 
 makeItalics = function () {
     var text = document.getElementById("comment-form");
+    var startIndex = text.selectionStart,
+        endIndex = text.selectionEnd;
+    var selectedText = text.value.substring(startIndex, endIndex);
+
+    var format = '*'
+    
+    if (selectedText.includes('*')) {
+    text.value = selectedText.replace(/\*/g, '');
+    
+    }
+    else if (selectedText.length == 0) {
+    text.value = text.value.substring(0, startIndex) + selectedText + text.value.substring(endIndex);
+    }
+    else {
+        text.value = text.value.substring(0, startIndex) + format + selectedText + format + text.value.substring(endIndex);
+    }
+}
+
+// Bold Reply Text
+
+makeReplyBold = function () {
+    var text = document.getElementById("reply-form");
+    var startIndex = text.selectionStart,
+        endIndex = text.selectionEnd;
+    var selectedText = text.value.substring(startIndex, endIndex);
+
+    var format = '**'
+    
+    if (selectedText.includes('**')) {
+    text.value = selectedText.replace(/\*/g, '');
+    
+    }
+    else if (selectedText.length == 0) {
+    text.value = text.value.substring(0, startIndex) + selectedText + text.value.substring(endIndex);
+    }
+    else {
+        text.value = text.value.substring(0, startIndex) + format + selectedText + format + text.value.substring(endIndex);
+    }
+}
+
+// Italicize Reply Text
+
+makeReplyItalics = function () {
+    var text = document.getElementById("reply-form");
     var startIndex = text.selectionStart,
         endIndex = text.selectionEnd;
     var selectedText = text.value.substring(startIndex, endIndex);
