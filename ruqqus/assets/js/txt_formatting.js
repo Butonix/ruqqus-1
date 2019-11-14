7a034828e5a907a0672b1e40369281b09f730e15
kek7198 <memezdank140@gmail.com>
2019-11-14 13:46:46 -0600

    comment text formatting js file

---
+++ ruqqus/assets/js/txt_formatting.js
@@ -0,0 +1,21 @@
+// Bold Text
+
+makeBold = function () {
+    var text = document.getElementById("comment-form");
+    var startIndex = text.selectionStart,
+        endIndex = text.selectionEnd;
+    var selectedText = text.value.substring(startIndex, endIndex);
+
+    var format = '**'
+    
+    if (selectedText.includes('**')) {
+    text.value = selectedText.replace(/\*/g, '');
+    
+    }
+    else if (selectedText.length == 0) {
+    text.value = text.value.substring(0, startIndex) + selectedText + text.value.substring(endIndex);
+    }
+    else {
+        text.value = text.value.substring(0, startIndex) + format + selectedText + format + text.value.substring(endIndex);
+    }
+}
