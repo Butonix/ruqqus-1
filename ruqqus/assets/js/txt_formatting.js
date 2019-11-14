9e5e943418564ee9fba362717065aa8bb20f31f5
kek7198 <memezdank140@gmail.com>
2019-11-14 16:47:57 -0600

    quote formatting

--- ruqqus/assets/js/txt_formatting.js
+++ ruqqus/assets/js/txt_formatting.js
@@ -42,6 +42,28 @@ makeItalics = function () {
     }
 }
 
+// Quote Comment Text
+
+makeQuote = function () {
+    var text = document.getElementById("comment-form");
+    var startIndex = text.selectionStart,
+        endIndex = text.selectionEnd;
+    var selectedText = text.value.substring(startIndex, endIndex);
+
+    var format = '>'
+    
+    if (selectedText.includes('>')) {
+    text.value = selectedText.replace(/\>/g, '');
+    
+    }
+    else if (selectedText.length == 0) {
+    text.value = text.value.substring(0, startIndex) + selectedText + text.value.substring(endIndex);
+    }
+    else {
+        text.value = text.value.substring(0, startIndex) + format + selectedText + text.value.substring(endIndex);
+    }
+}
+
 // Bold Reply Text
 
 makeReplyBold = function () {
@@ -83,5 +105,27 @@ makeReplyItalics = function () {
     }
     else {
         text.value = text.value.substring(0, startIndex) + format + selectedText + format + text.value.substring(endIndex);
+    }
+}
+
+// Quote Reply Text
+
+makeReplyQuote = function () {
+    var text = document.getElementById("reply-form");
+    var startIndex = text.selectionStart,
+        endIndex = text.selectionEnd;
+    var selectedText = text.value.substring(startIndex, endIndex);
+
+    var format = '>'
+    
+    if (selectedText.includes('>')) {
+    text.value = selectedText.replace(/\>/g, '');
+    
+    }
+    else if (selectedText.length == 0) {
+    text.value = text.value.substring(0, startIndex) + selectedText + text.value.substring(endIndex);
+    }
+    else {
+        text.value = text.value.substring(0, startIndex) + format + selectedText + text.value.substring(endIndex);
     }
 }
