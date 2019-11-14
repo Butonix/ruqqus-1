0c0d76264206ebc1b55926c22a49077e80252b4c
kek7198 <memezdank140@gmail.com>
2019-11-14 15:00:29 -0600

    italics

--- ruqqus/assets/js/txt_formatting.js
+++ ruqqus/assets/js/txt_formatting.js
@@ -19,3 +19,25 @@ makeBold = function () {
         text.value = text.value.substring(0, startIndex) + format + selectedText + format + text.value.substring(endIndex);
     }
 }
+
+// Bold Text
+
+makeItalics = function () {
+    var text = document.getElementById("comment-form");
+    var startIndex = text.selectionStart,
+        endIndex = text.selectionEnd;
+    var selectedText = text.value.substring(startIndex, endIndex);
+
+    var format = '*'
+    
+    if (selectedText.includes('*')) {
+    text.value = selectedText.replace(/\*/g, '');
+    
+    }
+    else if (selectedText.length == 0) {
+    text.value = text.value.substring(0, startIndex) + selectedText + text.value.substring(endIndex);
+    }
+    else {
+        text.value = text.value.substring(0, startIndex) + format + selectedText + format + text.value.substring(endIndex);
+    }
+}
