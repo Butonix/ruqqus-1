9056d0644caed77ed5ccbf055d5ec799d74db259
kek7198 <memezdank140@gmail.com>
2019-11-16 13:58:05 -0600

    firefox comment form fix

--- ruqqus/assets/js/comment_form.js
+++ ruqqus/assets/js/comment_form.js
@@ -1,7 +1,7 @@
 
 // Expand comment box on focus, hide otherwise
 
-$('.comment-box').focus(function () {
+$('.comment-box').focus(function (event) {
     event.preventDefault();
 
     $(this).parent().parent().addClass("collapsed");
