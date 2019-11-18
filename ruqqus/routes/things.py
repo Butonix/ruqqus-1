64e1113bea4e590f8142694655284d89ccf90ec2
arkit3kt <38845651+arkit3kt@users.noreply.github.com>
2019-11-18 08:33:57 -0500

    Feed (#81)
    * add rss feed
    
    * remove "new" posts from feed
    fix username for author

--- ruqqus/routes/things.py
+++ ruqqus/routes/things.py
@@ -12,6 +12,8 @@ from ruqqus.helpers.markdown import *
 from ruqqus.classes import *
 from flask import *
 from ruqqus.__main__ import app, db
+from werkzeug.contrib.atom import AtomFeed
+from datetime import datetime
 
 BAN_REASONS=['',
             "URL shorteners are not permitted."
@@ -322,3 +324,34 @@ def delete_comment(cid, v):
 
     return redirect(c.permalink)
 
+
+@app.route('/feeds/<sort>')
+def feeds(sort=None):
+    cutoff = int(time.time()) - (60 * 60 * 24) # 1 day
+
+    posts = db.query(Submission).filter(Submission.created_utc>=cutoff,
+                                        Submission.is_banned == False,
+                                        Submission.is_deleted == False,
+                                        Submission.stickied == False)
+
+    if sort == "hot":
+        posts = posts.order_by(text("submissions.rank_hot desc"))
+    elif sort == "fiery":
+        posts = posts.order_by(text("submissions.rank_fiery desc"))
+    elif sort == "top":
+        posts = posts.order_by(text("submissions.score desc"))
+
+
+    feed = AtomFeed(title=f'Top 5 {sort} Posts from ruqqus',
+                    feed_url=request.url, url=request.url_root)
+
+    posts = posts.limit(5).all()
+
+    for post in posts:
+        feed.add(post.title, post.body_html,
+                 content_type='html',
+                 author=post.author.username,
+                 url=f"https://ruqqus.com{post.permalink}",
+                 updated=datetime.fromtimestamp(post.created_utc),
+                 published=datetime.fromtimestamp(post.created_utc))
+    return feed.get_response()
