b700d4c6ced651cdd9f66cb47c5663302f59d995
kek <52423867+kek7198@users.noreply.github.com>
2019-11-17 18:40:54 -0600

    Css 6 (#80)
    * colors
    
    * collapse color
    
    * about page design
    
    * removed hr, added margin to "update bio" button
    
    * Update about.html
    
    * btn primary
    
    * makeReplyQuote
    
    * front-end username validation
    
    * regex
    
    * dark and black colors
    
    * else if
    
    * event
    
    * username validation and error messages
    
    * Update signup.js
    
    * Update signup.js
    
    * Update signup.js
    
    * regex
    
    * styling
    
    * text
    
    * text
    
    * max char regex
    
    * username check availability
    
    * use slim version of jquery
    
    * use minified jquery file
    
    * fixed wrong api link
    
    * console log
    
    * api url
    
    * ruqqus api url

--- ruqqus/assets/js/signup.js
+++ ruqqus/assets/js/signup.js
@@ -9,25 +9,6 @@ $('#password-register').on('input', function () {
     console.log(charCount.length);
 
     if (charCount.length >= 8) {
-            id.classList.add("d-none");
-            successID.classList.remove("d-none");
-    }
-    else {
-            id.classList.remove("d-none");
-            successID.classList.add("d-none");
-    };
-
-});
-
-$('#username-register').on('input', function () {
-
-    var charCount = document.getElementById("username-register").value;
-    var id = document.getElementById("usernameHelpRegister");
-    var successID = document.getElementById("usernameHelpSuccess");
-
-    console.log(charCount.length);
-
-    if (charCount.length >= 5) {
         id.classList.add("d-none");
         successID.classList.remove("d-none");
     }
@@ -35,5 +16,40 @@ $('#username-register').on('input', function () {
         id.classList.remove("d-none");
         successID.classList.add("d-none");
     };
+
+});
+
+// Check username length, special chars
+
+$('#username-register').on('input', function () {
+
+    var charCount = document.getElementById("username-register").value;
+    var id = document.getElementById("usernameHelpRegister");
+    var successID = document.getElementById("usernameHelpSuccess");
+
+    var ruqqusAPI = 'https://www.ruqqus.com/api/is_available/' + charCount;
+
+    $.getJSON(ruqqusAPI, function(result) {
+        $.each(result, function(i, field) {
+          if (field == false) {
+            id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">Username already taken :(';
+        }
+    });
+    });
+
+    if (!/[^a-zA-Z0-9_$]/.test(charCount)) {
+    // Change alert text
+    id.innerHTML = '<span class="form-text font-weight-bold text-success mt-1">Username is a-okay!';
+
+    if (charCount.length < 5) {
+      id.innerHTML = '<span class="form-text font-weight-bold text-muted mt-1">Username must be at least 5 characters long.';
+  }
+  else if (charCount.length > 25) {
+      id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">Username must be 25 characters or less.';
+  }
+}
+else {
+    id.innerHTML = '<span class="form-text font-weight-bold text-danger mt-1">No special characters or spaces allowed.</span>';
+};
 
 });
