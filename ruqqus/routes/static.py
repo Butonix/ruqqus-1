dbf81f12ff7e387f83a41fcf100e1b4b8742ce4f
captainmeta4 <captainmeta4@gmail.com>
2019-11-19 18:41:34 -0500

    Update static.py

--- ruqqus/routes/static.py
+++ ruqqus/routes/static.py
@@ -34,16 +34,6 @@ def settings_security(v):
 def help(v):
     return redirect("/help/terms")
 
-@app.route("/help/terms", methods=["GET"])
-@auth_desired
-def help_terms(v):
-    return render_template("terms_conduct.html", v=v)
-
-@app.route("/help/conduct", methods=["GET"])
-@auth_desired
-def help_conduct(v):
-    return render_template("terms_conduct.html", v=v)
-
 @app.route("/favicon.ico", methods=["GET"])
 def favicon():
     return send_file("./assets/images/logo/favicon.png")
