d9e48000bc19237de4c9fc561825f94d1ed19635
captainmeta4 <captainmeta4@gmail.com>
2019-11-21 08:39:28 -0500

    Update jinja2.py

--- ruqqus/helpers/jinja2.py
+++ ruqqus/helpers/jinja2.py
@@ -3,19 +3,12 @@ import time
 from sqlalchemy import text
 
 from ruqqus.classes.user import User
-from ruqqus.classes.ips import IP
 from ruqqus.__main__ import app, db, cache
 
-@app.template_filter("users_here")
-def users_here(x):
 
-    now=time.time()
-
-    cutoff=now-300
-
-    return db.query(User).join(IP).filter(IP.created_utc>=cutoff).count()
 
 @app.template_filter("total_users")
+@cache.memoize(timeout=60)
 def total_users(x):
 
     return db.query(User).filter(text("ban_state>-1")).count()
