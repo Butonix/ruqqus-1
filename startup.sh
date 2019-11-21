ea4286cfd4886c6e7070daf9618555e75c787d27
captainmeta4 <captainmeta4@gmail.com>
2019-11-21 15:48:41 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@ echo "startup script running"
 python3.7 ruqqus/helpers/db_prep.py
 echo "database functions set"
 echo "starting server"
-gunicorn ruqqus.__main__:app -w 1 -k gevent --worker-connections 18
+gunicorn ruqqus.__main__:app -w 2 -k gevent --worker-connections 9
