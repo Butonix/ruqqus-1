1a53f33c02f3cb4bde0b50d2235be9254eb212a3
captainmeta4 <captainmeta4@gmail.com>
2019-11-22 23:53:35 -0500

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -50,7 +50,7 @@ limiter = Limiter(
 )
 
 #setup db
-_engine = create_engine(environ.get("DATABASE_URL"))
+_engine = create_engine(environ.get("HEROKU_POSTGRESQL_TEAL_URL"))
 db = sessionmaker(bind=_engine)()
 Base = declarative_base()
 
