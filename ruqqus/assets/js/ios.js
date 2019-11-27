b93b46ee1af66a4555867b39c4da48e65191b3e1
captainmeta4 <captainmeta4@gmail.com>
2019-11-27 09:14:44 -0500

    Update ios.js

--- ruqqus/assets/js/ios.js
+++ ruqqus/assets/js/ios.js
@@ -2,7 +2,7 @@
 if(("standalone" in window.navigator) && window.navigator.standalone){
 
 	// If you want to prevent remote links in standalone web apps opening Mobile Safari, change 'remotes' to true
-	var noddy, remotes = false;
+	var noddy, remotes = true;
 	
 	document.addEventListener('click', function(event) {
 		
