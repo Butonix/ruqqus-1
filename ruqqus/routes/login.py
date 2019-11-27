ac0fb159301a3731edde3a9d10f717b662501651
captainmeta4 <captainmeta4@gmail.com>
2019-11-26 20:32:21 -0500

    Update login.py

--- ruqqus/routes/login.py
+++ ruqqus/routes/login.py
@@ -235,8 +235,8 @@ def sign_up_post(v):
         email=None
 
     if (db.query(User).filter(User.username.ilike(request.form.get("username"))).first()
-        or db.query(User).filter(User.email.ilike(email)).first()):
-        return new_signup("An account with that username or email already exists.")       
+        or (email and db.query(User).filter(User.email.ilike(email)).first()))
+        return new_signup("An account with that username or email already exists.")
     
     #success
     
