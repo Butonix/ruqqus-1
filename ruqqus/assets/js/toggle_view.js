f178831a215c9ffce2cc76b925d49029c6ce3f9b
kek7198 <memezdank140@gmail.com>
2019-11-28 14:42:37 -0600

    mobile function

--- ruqqus/assets/js/toggle_view.js
+++ ruqqus/assets/js/toggle_view.js
@@ -37,3 +37,39 @@ toggle_list_view = function() {
 }
 
 // Mobile
+
+toggle_card_view_sm = function() {
+
+	var toggleCard = document.getElementById('toggle-card-view-btn');
+	var toggleList = document.getElementById('toggle-list-view-btn');
+
+	var posts = document.getElementById('posts');
+
+	posts.classList.add('toggle-card-view');
+
+	if ( posts.classList.contains('toggle-list-view')) {
+		posts.classList.remove('toggle-list-view')
+	}
+
+	toggleCard.classList.add('active');
+	toggleList.classList.remove('active');
+
+}
+
+toggle_list_view_sm = function() {
+
+	var toggleCard = document.getElementById('toggle-card-view-btn-sm');
+	var toggleList = document.getElementById('toggle-list-view-btn-sm');
+
+	var posts = document.getElementById('posts');
+
+	posts.classList.add('toggle-list-view');
+
+	if ( posts.classList.contains('toggle-card-view')) {
+		posts.classList.remove('toggle-card-view')
+	}
+
+	toggleList.classList.add('active');
+	toggleCard.classList.remove('active');
+
+}
