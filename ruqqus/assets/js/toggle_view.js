9329d7128247697e2fdb1fea450e01c6bc4603ef
kek7198 <memezdank140@gmail.com>
2019-11-28 12:57:56 -0600

    Update toggle_view.js

--- ruqqus/assets/js/toggle_view.js
+++ ruqqus/assets/js/toggle_view.js
@@ -2,7 +2,7 @@ toggle_card_view = function() {
 
 	var posts = document.getElementById('posts');
 
-	posts.classList.add('toggle-card-vew');
+	posts.classList.add('toggle-card-view');
 
 	if ( posts.classList.contains('toggle-list-view')) {
 		posts.classList.remove('toggle-list-view')
@@ -14,7 +14,7 @@ toggle_list_view = function() {
 
 	var posts = document.getElementById('posts');
 
-	posts.classList.add('toggle-list-vew');
+	posts.classList.add('toggle-list-view');
 
 	if ( posts.classList.contains('toggle-card-view')) {
 		posts.classList.remove('toggle-card-view')
