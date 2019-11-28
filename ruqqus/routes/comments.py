768c10cc7037b201e187019ec56bc0fa360393a9
captainmeta4 <captainmeta4@gmail.com>
2019-11-27 20:40:52 -0500

    Update comments.py

--- ruqqus/routes/comments.py
+++ ruqqus/routes/comments.py
@@ -53,10 +53,15 @@ def api_comment(v):
     body_html=sanitize(body_md, linkgen=True)
 
     #Run safety filter
-    ban=filter_comment_html(body_html)
+    bans=filter_comment_html(body_html)
 
-    if ban:
-        abort(422)
+    if bans:
+        return render_template("comment_failed.html",
+                               parent_submission=parent_submission,
+                               parent_fullname=parent_fullname,
+                               badlinks=[x.domain for x in bans],
+                               body=body
+                               )
 
     #check existing
     existing=db.query(Comment).filter_by(author_id=v.id,
