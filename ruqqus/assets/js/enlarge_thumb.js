66354be6b7ff4ad5ed50117fe8815e4e8f3ef56d
kek7198 <memezdank140@gmail.com>
2019-11-28 16:44:52 -0600

    Update enlarge_thumb.js

--- ruqqus/assets/js/enlarge_thumb.js
+++ ruqqus/assets/js/enlarge_thumb.js
@@ -3,7 +3,7 @@
 $(".post-img").click(function (event) {
     event.preventDefault();
 
-    var id = $(this).parent().parent().parent().attr("id");
+    var id = $(this).parent().parent().parent().parent().attr("id");
 
     document.getElementById(id).classList.toggle("enlarged");
 
