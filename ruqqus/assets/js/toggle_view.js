5c3886450853f9415fe3002ca5bddf03a0203776
kek7198 <memezdank140@gmail.com>
2019-11-28 23:56:19 -0600

    Update toggle_view.js

--- ruqqus/assets/js/toggle_view.js
+++ ruqqus/assets/js/toggle_view.js
@@ -1,5 +1,47 @@
 // Desktop
 
+if (localStorage.view_pref == 'list') {
+
+	var toggleCard = document.getElementById('toggle-card-view-btn');
+	var toggleList = document.getElementById('toggle-list-view-btn');
+
+	var contentCol = document.getElementById('main-content-col');
+
+	var posts = document.getElementById('posts');
+
+	posts.classList.add('toggle-list-view');
+	contentCol.classList.add('bg-white','shadow-sm','shadow-sm-0');
+
+	if ( posts.classList.contains('toggle-card-view')) {
+		posts.classList.remove('toggle-card-view')
+	}
+
+	toggleList.classList.add('active');
+	toggleCard.classList.remove('active');
+  
+} else {
+
+	var toggleCard = document.getElementById('toggle-card-view-btn');
+	var toggleList = document.getElementById('toggle-list-view-btn');
+
+	var contentCol = document.getElementById('main-content-col');
+
+	var posts = document.getElementById('posts');
+
+	posts.classList.add('toggle-card-view');
+
+	if ( contentCol.classList.contains('bg-white','shadow-sm','shadow-sm-0')) {
+	contentCol.classList.remove('bg-white','shadow-sm','shadow-sm-0')
+	}
+
+	if ( posts.classList.contains('toggle-list-view')) {
+		posts.classList.remove('toggle-list-view')
+	}
+
+	toggleCard.classList.add('active');
+	toggleList.classList.remove('active');
+}
+
 toggle_card_view = function() {
 
 	// Store Pref
@@ -51,10 +93,13 @@ toggle_list_view = function() {
 
 }
 
-if (localStorage.view_pref == 'list') {
 
-	var toggleCard = document.getElementById('toggle-card-view-btn');
-	var toggleList = document.getElementById('toggle-list-view-btn');
+// Mobile
+
+if (localStorage.view_pref_mobile == 'list') {
+
+	var toggleCard = document.getElementById('toggle-card-view-btn-sm');
+	var toggleList = document.getElementById('toggle-list-view-btn-sm');
 
 	var contentCol = document.getElementById('main-content-col');
 
@@ -69,11 +114,11 @@ if (localStorage.view_pref == 'list') {
 
 	toggleList.classList.add('active');
 	toggleCard.classList.remove('active');
-  
+	
 } else {
 
-	var toggleCard = document.getElementById('toggle-card-view-btn');
-	var toggleList = document.getElementById('toggle-list-view-btn');
+	var toggleCard = document.getElementById('toggle-card-view-btn-sm');
+	var toggleList = document.getElementById('toggle-list-view-btn-sm');
 
 	var contentCol = document.getElementById('main-content-col');
 
@@ -92,9 +137,6 @@ if (localStorage.view_pref == 'list') {
 	toggleCard.classList.add('active');
 	toggleList.classList.remove('active');
 }
-
-
-// Mobile
 
 toggle_card_view_sm = function() {
 
@@ -145,46 +187,4 @@ toggle_list_view_sm = function() {
 	toggleList.classList.add('active');
 	toggleCard.classList.remove('active');
 
-}
-
-if (localStorage.view_pref_mobile == 'list') {
-
-	var toggleCard = document.getElementById('toggle-card-view-btn-sm');
-	var toggleList = document.getElementById('toggle-list-view-btn-sm');
-
-	var contentCol = document.getElementById('main-content-col');
-
-	var posts = document.getElementById('posts');
-
-	posts.classList.add('toggle-list-view');
-	contentCol.classList.add('bg-white','shadow-sm','shadow-sm-0');
-
-	if ( posts.classList.contains('toggle-card-view')) {
-		posts.classList.remove('toggle-card-view')
-	}
-
-	toggleList.classList.add('active');
-	toggleCard.classList.remove('active');
-	
-} else {
-
-	var toggleCard = document.getElementById('toggle-card-view-btn-sm');
-	var toggleList = document.getElementById('toggle-list-view-btn-sm');
-
-	var contentCol = document.getElementById('main-content-col');
-
-	var posts = document.getElementById('posts');
-
-	posts.classList.add('toggle-card-view');
-
-	if ( contentCol.classList.contains('bg-white','shadow-sm','shadow-sm-0')) {
-	contentCol.classList.remove('bg-white','shadow-sm','shadow-sm-0')
-	}
-
-	if ( posts.classList.contains('toggle-list-view')) {
-		posts.classList.remove('toggle-list-view')
-	}
-
-	toggleCard.classList.add('active');
-	toggleList.classList.remove('active');
 }
