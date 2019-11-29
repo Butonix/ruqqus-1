59e5a1ad9a0c5c25f1dbe81f050d1452d958a19e
Adam LaDine <captainmeta4@users.noreply.github.com>
2019-11-29 12:18:37 -0500

    admin color (#99)
    * admin color
    
    * Update single_comment.html
    
    * Update single_comment.html
    
    * submissions
    
    * Update single_comment.html
    
    * fix spacing
    
    * Update submission_listing.html
    
    * Update jinja2.py
    
    * Update jinja2.py
    
    * Update about.html
    
    * Update about.html
    
    * Update about.html
    
    * try adding sql entry
    
    * Revert "try adding sql entry"
    
    This reverts commit 2c8c26ed75a4c26984c92a855339123cdc894875.
    
    * Update submission_listing.html
    
    * Update submission.html
    
    * Update submission.html
    
    * Update submission_listing.html
    
    * Update submission.html
    
    * Update submission.html

---
+++ scripts/css.sh
@@ -0,0 +1,2 @@
+sass ruqqus/assets/style/main.scss ruqqus/assets/style/main.css
+sass ruqqus/assets/style/main_dark.scss ruqqus/assets/style/main_dark.css
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,2 @@
-echo "startup script running"
 python3.7 ruqqus/helpers/db_prep.py
-echo "database functions set"
-echo "starting server"
 gunicorn ruqqus.__main__:app -w 2 -k gevent --worker-connections 9
