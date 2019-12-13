9c21c1ac638b67f937042f391720ca2834ba9684
captainmeta4 <captainmeta4@gmail.com>
2019-12-13 18:29:34 -0500

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -95,7 +95,7 @@ def after_request(response):
 
     if request.method=="POST" and response.status_code in [301, 302] and request.path=="/signup":
         link=f'https://{app.config["SERVER_NAME"]}/@{request.form.get("username")}'
-        thread=threading.Thread(target=lambda:log_event(name="Signup attempt", text=text))
+        thread=threading.Thread(target=lambda:log_event(name="Signup attempt", link=link))
                                 )
         thread.start()
             
