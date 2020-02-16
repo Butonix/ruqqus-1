d7c252dce569e29fc3ac6df87666928dbff78144
kek7198 <memezdank140@gmail.com>
2020-02-16 14:44:19 -0600

    Update get_gif.js

--- ruqqus/assets/js/get_gif.js
+++ ruqqus/assets/js/get_gif.js
@@ -26,9 +26,9 @@
       cancelBtn.innerHTML = null;
     }
     else {
-      backBtn.innerHTML = '<button class="btn btn-link py-3 pl-3 pr-0" id="gifs-back-btn" onclick="getGif();"><i class="fas fa-long-arrow-left text-muted"></i></button>';
+      backBtn.innerHTML = '<button class="btn btn-link py-3 pl-3 pr-0" id="gifs-back-btn" onclick="getGif('');"><i class="fas fa-long-arrow-left text-muted"></i></button>';
 
-      cancelBtn.innerHTML = '<button class="btn btn-link py-3 pr-3 pl-0" id="gifs-cancel-btn" onclick="getGif();"><i class="fas fa-times text-muted"></i></button>';
+      cancelBtn.innerHTML = '<button class="btn btn-link py-3 pr-3 pl-0" id="gifs-cancel-btn" onclick="getGif('');"><i class="fas fa-times text-muted"></i></button>';
     };
 
     console.log('searchTerm is: ', searchTerm)
