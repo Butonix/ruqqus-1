4d176b0e540eae1b39a4cf67e7dfd75974cd6eee
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-02-17 14:43:38 -0500

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -29,6 +29,7 @@ app.config['SECRET_KEY']=environ.get('MASTER_KEY')
 app.config["SERVER_NAME"]=environ.get("domain", None)
 app.config["VERSION"]="0.1.0"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['SESSION_PERMANENT']=True
 app.config["SESSION_COOKIE_SECURE"]=True
 app.config["SESSION_COOKIE_SAMESITE"]="Lax"
 app.config["PERMANENT_SESSION_LIFETIME"]=60*60*24
