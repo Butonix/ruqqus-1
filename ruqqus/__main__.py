a19d335e9ba4b9317fb58c61a7ba1a70eb733207
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-02-17 16:08:31 -0500

    fix!

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -15,7 +15,7 @@ import requests
 
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-_version = "2.1.9"
+_version = "2.1.10"
 
 app = Flask(__name__,
             template_folder='./templates',
