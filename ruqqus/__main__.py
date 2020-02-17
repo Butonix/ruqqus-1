08c0a673f51f315195cc4e824bec4bbbb466dc31
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-02-17 16:25:02 -0500

    fix 26th notif getting cleared

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -15,7 +15,7 @@ import requests
 
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-_version = "2.1.11"
+_version = "2.1.12"
 
 app = Flask(__name__,
             template_folder='./templates',
