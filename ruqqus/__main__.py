60a4690b8c9972c17ecacd7224c32b8a17122f13
captainmeta4 <captainmeta4@gmail.com>
2020-02-17 21:18:52 -0500

    dropdown

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -15,7 +15,7 @@ import requests
 
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-_version = "2.1.3"
+_version = "2.1.3.1"
 
 app = Flask(__name__,
             template_folder='./templates',
