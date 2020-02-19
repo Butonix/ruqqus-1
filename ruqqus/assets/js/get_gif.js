afaf3a173457e29869dbf69e2f085706071a888e
kek7198 <memezdank140@gmail.com>
2020-02-18 21:08:59 -0600

    Update get_gif.js

--- ruqqus/assets/js/get_gif.js
+++ ruqqus/assets/js/get_gif.js
@@ -56,7 +56,7 @@
           }
         }
         else if (max <= 0) {
-            container.innerHTML = '<div class="d-flex justify-content-center"><button class="btn btn-sm btn-outline-gray-500 mr-2" onclick="getGif(\'agree\');">Agree</button><button class="btn btn-sm btn-outline-gray-500 mr-2" onclick="getGif(\'disagree\');">Disagree</button><button class="btn btn-sm btn-outline-gray-500" onclick="getGif(\'laugh\');">Laugh</button></div>';
+            container.innerHTML = '<div class="text-center"><div class="py-4"><i class="fad fa-frown text-muted" style="font-size: 3.5rem;"/></div><p>Aw shucks. No GIFs found...</p></div><div class="d-flex justify-content-center"><button class="btn btn-sm btn-outline-gray-500 mr-2" onclick="getGif(\'agree\');">Agree</button><button class="btn btn-sm btn-outline-gray-500 mr-2" onclick="getGif(\'disagree\');">Disagree</button><button class="btn btn-sm btn-outline-gray-500" onclick="getGif(\'laugh\');">Laugh</button></div>';
         }
         else {
           for (var i = 0; i <= 15; i++) {
