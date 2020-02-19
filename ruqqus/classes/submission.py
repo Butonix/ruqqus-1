84109a60d8e3514ecf19316744581a6f2fd43401
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-02-19 16:53:06 -0500

    Update submission.py

--- ruqqus/classes/submission.py
+++ ruqqus/classes/submission.py
@@ -244,7 +244,7 @@ class Submission(Base, Stndrd, Age_times, Scores, Fuzzing):
     #@lazy
     def thumb_url(self):
 
-        elif self.domain=="i.ruqqus.com":
+        if self.domain=="i.ruqqus.com":
             return self.url
         elif self.has_thumb:
             return f"https://i.ruqqus.com/posts/{self.base36id}/thumb.png"
