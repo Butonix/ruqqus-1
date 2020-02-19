71f56df5054a06d9e73bcbdbbefb070998a421c7
kek7198 <memezdank140@gmail.com>
2020-02-18 20:39:49 -0600

    Update get_gif.js

--- ruqqus/assets/js/get_gif.js
+++ ruqqus/assets/js/get_gif.js
@@ -35,12 +35,12 @@
 
       cancelBtn.innerHTML = '<button class="btn btn-link pl-0 pr-3" id="gifs-cancel-btn" onclick="getGif();"><i class="fas fa-times text-muted"></i></button>';
 
-          console.log('searchTerm is: ', searchTerm)
-    $.ajax({
-      url: "//api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=eOTkZX92KQM80g9NcBsq0heqZxZSVP86",
-      type: "GET",
-      success: function(response) {
-        console.log(response)
+      console.log('searchTerm is: ', searchTerm)
+      $.ajax({
+        url: "//api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=eOTkZX92KQM80g9NcBsq0heqZxZSVP86",
+        type: "GET",
+        success: function(response) {
+          console.log(response)
         var max = response.data.length - 1 //length of response, minus 1 (cuz array starts at index 0)
         console.log('response.data.length is ', max)
         //var randomNumber = Math.round(Math.random() * max) //random number between 0 and max -1
@@ -49,9 +49,17 @@
         var gifURL = [];
 
         // loop for fetching mutliple GIFs and creating the card divs
-        for (var i = 0; i <= 15; i++) {
-          gifURL[i] = "https://media.giphy.com/media/" + response.data[i].id + "/giphy.gif";
-          container.innerHTML += ('<div class="card bg-secondary gif-keyboard-option" style="overflow: hidden" data-dismiss="modal" aria-label="Close" onclick="insertGIF(\'' + gifURL[i] + '\')"><img class="img-fluid" src="' + gifURL[i] + '"></div>');
+        if (max < 15) {
+          for (var i = 0; i <= max; i++) {
+            gifURL[i] = "https://media.giphy.com/media/" + response.data[i].id + "/giphy.gif";
+            container.innerHTML += ('<div class="card bg-secondary gif-keyboard-option" style="overflow: hidden" data-dismiss="modal" aria-label="Close" onclick="insertGIF(\'' + gifURL[i] + '\')"><img class="img-fluid" src="' + gifURL[i] + '"></div>');
+          }
+        }
+        else {
+          for (var i = 0; i <= 15; i++) {
+            gifURL[i] = "https://media.giphy.com/media/" + response.data[i].id + "/giphy.gif";
+            container.innerHTML += ('<div class="card bg-secondary gif-keyboard-option" style="overflow: hidden" data-dismiss="modal" aria-label="Close" onclick="insertGIF(\'' + gifURL[i] + '\')"><img class="img-fluid" src="' + gifURL[i] + '"></div>');
+          }
         }
         console.log(container);
       },
@@ -87,5 +95,5 @@
     var container = document.getElementById('GIFs');
 
     container.innerHTML = '';
-  
-})
+    
+  })
