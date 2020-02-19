e1b9e102822c8385913d296de76b38200830ca22
kek7198 <memezdank140@gmail.com>
2020-02-19 12:51:49 -0600

    giphy attribution

--- ruqqus/assets/js/expand_desktop_image.js
+++ ruqqus/assets/js/expand_desktop_image.js
@@ -1,9 +1,37 @@
 function expandDesktopImage(image) {
 
+// GIPHY attribution div
+
+var attribution = document.getElementById("modal-image-attribution");
+
 $("#expandImageModal").modal("show");
 
 document.getElementById("desktop-expanded-image").src = image;
 
 document.getElementById("desktop-expanded-image-link").href = image;
+
+if (image.includes("media.giphy.com")) {
+	attribution.innerHTML = '<img src="/assets/images/icons/PoweredBy_200px-Black_HorizLogo.png" style="width: 150px;">';
+}
+
+}
+
+// When image modal is closed
+
+$('#expandImageModal').on('hidden.bs.modal', function (e) {
+
+  	// GIPHY attribution div
+
+  	var attribution = document.getElementById("modal-image-attribution");
+
+  	// remove the attribution
+
+  	attribution.innerHTML = null;
+
+	// remove image src and link
+
+	document.getElementById("desktop-expanded-image").src = null;
+
+	document.getElementById("desktop-expanded-image-link").href = null;
 
 }
