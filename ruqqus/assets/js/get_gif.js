705bdada1275a11ad4001d36b8d68e0e4ac73b56
kek7198 <memezdank140@gmail.com>
2020-02-18 21:03:08 -0600

    Update get_gif.js

--- ruqqus/assets/js/get_gif.js
+++ ruqqus/assets/js/get_gif.js
@@ -49,7 +49,7 @@
         var gifURL = [];
 
         // loop for fetching mutliple GIFs and creating the card divs
-        if (max < 15) {
+        if (max < 15 && max > 0) {
           for (var i = 0; i <= max; i++) {
             gifURL[i] = "https://media.giphy.com/media/" + response.data[i].id + "/giphy.gif";
             container.innerHTML += ('<div class="card bg-secondary gif-keyboard-option" style="overflow: hidden" data-dismiss="modal" aria-label="Close" onclick="insertGIF(\'' + gifURL[i] + '\')"><img class="img-fluid" src="' + gifURL[i] + '"></div>');
