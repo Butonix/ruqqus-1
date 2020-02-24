bef6247a691be73f8dc61febc336cdde04567e0f
kek7198 <memezdank140@gmail.com>
2020-02-24 12:16:14 -0600

    hide comment actions when editing comment

--- ruqqus/assets/js/comment_form.js
+++ ruqqus/assets/js/comment_form.js
@@ -24,9 +24,11 @@ toggleEdit=function(id){
     comment=document.getElementById("comment-text-"+id);
     form=document.getElementById("comment-edit-"+id);
     box=document.getElementById('edit-box-comment-'+id);
+    actions = document.getElementById('comment-' + id +'-actions');
 
     comment.classList.toggle("d-none");
     form.classList.toggle("d-none");
+    actions.classList.toggle("d-none");
     autoExpand(box);
 }
 
@@ -35,8 +37,10 @@ togglePostEdit=function(){
     body=document.getElementById("post-body");
     form=document.getElementById("edit-post-body");
     box=document.getElementById("post-edit-box");
+    actions = document.getElementById('comment-' + id +'-actions');
 
     body.classList.toggle("d-none");
     form.classList.toggle("d-none");
+    actions.classList.toggle("d-none");
     autoExpand(box);
 }
