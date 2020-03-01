2ba349428a77676b020829804f0a09dfa4d939ef
arkit3kt <exodust@protonmail.com>
2020-02-29 22:22:03 -0500

    start custom user rss feeds TODO : fix submission query for user's personal feed

--- ruqqus/routes/feeds.py
+++ ruqqus/routes/feeds.py
@@ -2,7 +2,7 @@ from flask import *
 from ruqqus.__main__ import app, db, limiter
 from werkzeug.contrib.atom import AtomFeed
 from datetime import datetime
-from ruqqus.classes.submission import *
+from ruqqus.classes import *
 from ruqqus.helpers import *
 
 @app.route('/feeds/<sort>')
@@ -20,6 +20,48 @@ def feeds(sort=None):
     elif sort == "fiery":
         posts = posts.order_by(text("submissions.rank_fiery desc"))
     elif sort == "top":
+        posts = posts.order_by(text("submissions.score desc"))
+
+
+    feed = AtomFeed(title=f'Top 5 {sort} Posts from ruqqus',
+                    feed_url=request.url, url=request.url_root)
+
+    posts = posts.limit(5).all()
+
+    for post in posts:
+        feed.add(post.title, post.body_html,
+                 content_type='html',
+                 author=post.author.username,
+                 url=f"https://ruqqus.com{post.permalink}",
+                 updated=datetime.fromtimestamp(post.created_utc),
+                 published=datetime.fromtimestamp(post.created_utc))
+    return feed.get_response()
+
+@app.route('/feeds/@<username>/<key>/<sort>')
+def feeds(sort=None, username=None, key=None):
+    if not username and key:
+        return abort(501)
+
+    user = db.query(User).filter_by(username=username).first()
+
+    if not hmac.compare_digest(user.feedkey, key):
+        ##invalid feedkey
+        return abort(501)
+
+
+    cutoff = int(time.time()) - (60 * 60 * 24) # 1 day
+
+    posts = db.query(Submission).filter(Submission.created_utc>=cutoff,
+                                        Submission.is_banned == False,
+                                        Submission.is_deleted == False,
+                                        Submission.stickied == False,
+                                        Submission.is_public == True)
+
+    if sort == "hot":
+        posts = posts.order_by(text("submissions.rank_hot desc"))
+    elif sort == "fiery":
+        posts = posts.order_by(text("submissions.rank_fiery desc"))
+    elif sort == "top" or sort == None:
         posts = posts.order_by(text("submissions.score desc"))
 
 
