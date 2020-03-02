105cb17c416025e34922a263b94cdf2abd5304b3
kek7198 <memezdank140@gmail.com>
2020-03-02 16:56:26 -0600

    txt formatting

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -1120,3 +1120,71 @@ $('#expandImageModal').on('hidden.bs.modal', function (e) {
 	document.getElementById("desktop-expanded-image-link").href = null;
 
 });
+
+// Text Formatting
+
+// Bold Text
+
+makeBold = function (form) {
+    var text = document.getElementById(form);
+    var startIndex = text.selectionStart,
+        endIndex = text.selectionEnd;
+    var selectedText = text.value.substring(startIndex, endIndex);
+
+    var format = '**'
+    
+    if (selectedText.includes('**')) {
+    text.value = selectedText.replace(/\*/g, '');
+    
+    }
+    else if (selectedText.length == 0) {
+    text.value = text.value.substring(0, startIndex) + selectedText + text.value.substring(endIndex);
+    }
+    else {
+        text.value = text.value.substring(0, startIndex) + format + selectedText + format + text.value.substring(endIndex);
+    }
+}
+
+// Italicize Comment Text
+
+makeItalics = function (form) {
+    var text = document.getElementById(form);
+    var startIndex = text.selectionStart,
+        endIndex = text.selectionEnd;
+    var selectedText = text.value.substring(startIndex, endIndex);
+
+    var format = '*'
+    
+    if (selectedText.includes('*')) {
+    text.value = selectedText.replace(/\*/g, '');
+    
+    }
+    else if (selectedText.length == 0) {
+    text.value = text.value.substring(0, startIndex) + selectedText + text.value.substring(endIndex);
+    }
+    else {
+        text.value = text.value.substring(0, startIndex) + format + selectedText + format + text.value.substring(endIndex);
+    }
+}
+
+// Quote Comment Text
+
+makeQuote = function (form) {
+    var text = document.getElementById(form);
+    var startIndex = text.selectionStart,
+        endIndex = text.selectionEnd;
+    var selectedText = text.value.substring(startIndex, endIndex);
+
+    var format = '>'
+    
+    if (selectedText.includes('>')) {
+    text.value = selectedText.replace(/\>/g, '');
+    
+    }
+    else if (selectedText.length == 0) {
+    text.value = text.value.substring(0, startIndex) + selectedText + text.value.substring(endIndex);
+    }
+    else {
+        text.value = text.value.substring(0, startIndex) + format + selectedText + text.value.substring(endIndex);
+    }
+}
