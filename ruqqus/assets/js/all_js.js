8bb19e7c7dc93991d713ec57004289831755d502
captainmeta4 <captainmeta4@gmail.com>
2020-03-14 16:11:25 -0400

    Update all_js.js

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -419,6 +419,7 @@ document.addEventListener('input', function (event) {
 function switch_css() {
   css = document.getElementById("css-link");
   dswitch = document.getElementById("dark-switch");
+  dswitchmobile = document.getElementById("dark-switch-mobile");
 
   if (css.href.endsWith("/assets/style/main.css")) {
     post("/settings/dark_mode/1",
@@ -426,6 +427,8 @@ function switch_css() {
         css.href="/assets/style/main_dark.css";
         dswitch.classList.remove("fa-toggle-off");
         dswitch.classList.add("fa-toggle-on");
+        dswitchmobile.classList.remove("fa-toggle-off");
+        dswitchmobile.classList.add("fa-toggle-on");
       }
     );
   }
@@ -435,6 +438,8 @@ function switch_css() {
         css.href="/assets/style/main.css";
         dswitch.classList.remove("fa-toggle-on");
         dswitch.classList.add("fa-toggle-off");
+        dswitchmobile.classList.remove("fa-toggle-on");
+        dswitchmobile.classList.add("fa-toggle-off");
       }
     );
   }
