932c4cc2a44ad63d287dd96632b9008fa03c7a5d
captainmeta4 <captainmeta4@gmail.com>
2020-04-05 13:21:37 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -23,7 +23,8 @@ _version = "2.6.2"
 
 app = Flask(__name__,
             template_folder='./templates',
-            static_folder='./static'
+            static_folder='./static',
+            host_matching=True
            )
 app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=2)
 
@@ -43,7 +44,6 @@ app.config["PERMANENT_SESSION_LIFETIME"]=60*60*24*365
 app.jinja_env.cache = {}
 
 app.config["UserAgent"]="Ruqqus webserver ruqqus.com"
-app.url_map.host_matching = True
 
 if "localhost" in app.config["SERVER_NAME"]:
     app.config["CACHE_TYPE"]="null"
