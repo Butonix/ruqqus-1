adb386cf2fd925fc6645d0c35df8d9f4f69851ad
captainmeta4 <captainmeta4@gmail.com>
2020-04-05 09:31:05 -0400

    remove unused functions

--- ruqqus/helpers/jinja2.py
+++ ruqqus/helpers/jinja2.py
@@ -7,15 +7,6 @@ from ruqqus.classes.user import User
 from .get import *
 from ruqqus.__main__ import app, db, cache
 
-
-
-@app.template_filter("total_users")
-@cache.memoize(timeout=60)
-def total_users(x):
-
-    return db.query(User).filter_by(is_banned=0).count()
-
-
 @app.template_filter("source_code")
 @cache.memoize(timeout=60*60*24)
 def source_code(file_name):
@@ -26,16 +17,3 @@ def source_code(file_name):
 def full_link(url):
 
     return f"https://{app.config['SERVER_NAME']}{url}"
-
-@app.template_filter("js_str_escape")
-def js_str_escape(s):
-    
-    s=s.replace("'", r"\'")
-
-    return s
-
-@app.template_filter("is_mod")
-@cache.memoize(60)
-def jinja_is_mod(uid, bid):
-
-    return bool(get_mod(uid, bid))
