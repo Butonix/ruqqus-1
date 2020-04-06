21f68ff90ccd2488719d961ddb55b43f920fc4c8
captainmeta4 <captainmeta4@gmail.com>
2020-04-06 18:23:10 -0400

    fix

--- ruqqus/helpers/wrappers.py
+++ ruqqus/helpers/wrappers.py
@@ -216,3 +216,21 @@ def no_cors(f):
 
     wrapper.__name__=f.__name__
     return wrapper
+
+#wrapper for api-related things that discriminates between an api url
+#and an html url for the same content
+# f should return {'api': lambda:function(), 'html':lambda:function()}
+
+def api(f):
+
+    def wrapper(*args, **kwargs):
+
+        x=f(*args, **kwargs)
+
+        if requests.path.startswith('/api/v1'):
+            return jsonify(x['api']())
+        else:
+            return x['html']()
+
+    wrapper.__name__=f.__name__
+    return wrapper
