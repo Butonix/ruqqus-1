cd97a681d8189822b6c3729f6275d7fc417663d5
arkit3kt <exodust@protonmail.com>
2020-04-19 01:40:16 -0400

    test .all()

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -543,7 +543,7 @@ class User(Base, Age_times, Stndrd):
         if self.karma + self.comment_karma < 50:
             return False
 
-        if (len(self.boards_created) + len(self.moderates.board)) >= 10:
+        if (len(self.boards_created.all()) + len(self.moderates.all())) >= 10:
             return False
 
         return True
