4155d5445b34b29d6e7ffb14fc10c22d61c465fc
captainmeta4 <captainmeta4@gmail.com>
2020-04-19 10:18:18 -0400

    update

--- scripts/mailing_list.py
+++ scripts/mailing_list.py
@@ -6,22 +6,21 @@ from flask import render_template
 title = input("Title: ")
 subject = input("Email subject: ")
 
-
-with app.app_context():
-    html=render_template(
-        "email/mailing.html",
-        title=title,
-        preheader=content[0:100],
-        content=content
-        )
-
 #for user in db.query(classes.user.User).filter_by(is_activated=True, is_banned=0, is_deleted=False):
 for user in db.query(classes.user.User).filter_by(id=7).all():
 
+    with app.app_context():
+        html=render_template(
+            "email/mailing.html",
+            title=title,
+            content=content,
+            user=user
+            )
+        
     send_mail(
         to_address=user.email,
         subject=subject,
-        html=html,
-        plaintext=content)
+        html=html
+        )
 
     
