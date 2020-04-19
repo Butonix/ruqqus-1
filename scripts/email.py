9bdc45ad5c13c97cdf4eb85222971325ba6dd0da
captainmeta4 <captainmeta4@gmail.com>
2020-04-19 08:45:38 -0400

    fix

---
+++ scripts/email.py
@@ -0,0 +1,27 @@
+from flask import *
+import requests
+from ruqqus.__main__ import *
+from ruqqus.mail import send_mail
+
+title = input("Title: ")
+subject = input("Email subject: ")
+preheader= input("preheader text")
+
+content=input("content:")
+
+html=render_template("email/mailing.html",
+	title=title,
+	preheader=preheader,
+	content=content
+	)
+
+#for user in db.query(User).filter_by(is_activated=True, is_banned=0, is_deleted=False):
+for user in db.query(User).filter_by(id=7).all():
+
+	send_mail(
+		to_address=user.email,
+		subject=subject,
+		html=html,
+		plaintext=content)
+
+    
