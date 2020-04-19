f6681cc743edf2cbdc859b2aa46fb0e05f0121c2
captainmeta4 <captainmeta4@gmail.com>
2020-04-19 09:39:48 -0400

    Update mailing_list.py

--- scripts/mailing_list.py
+++ scripts/mailing_list.py
@@ -1,34 +1,29 @@
-from ruqqus.__main__ import db
+from ruqqus.__main__ import app, db
 from ruqqus import classes
 from ruqqus.mail import send_mail
-import jinja2
-
-def render_jinja_html(template_loc,file_name,**context):
-
-    return jinja2.Environment(
-            loader=jinja2.FileSystemLoader(template_loc+'/')
-            ).get_template(file_name).render(context)
+from flask import render_template
 
 title = input("Title: ")
 subject = input("Email subject: ")
 
 content=input("content:")
 
-html=render_jinja_html(
-        "/app/ruqqus/templates",
+
+with app.app_context():
+    html=render_template(
         "email/mailing.html",
-	title=title,
-	preheader=content[0:100],
-	content=content
-	)
+        title=title,
+        preheader=content[0:100],
+        content=content
+        )
 
 #for user in db.query(classes.user.User).filter_by(is_activated=True, is_banned=0, is_deleted=False):
 for user in db.query(classes.user.User).filter_by(id=7).all():
 
-	send_mail(
-		to_address=user.email,
-		subject=subject,
-		html=html,
-		plaintext=content)
+    send_mail(
+        to_address=user.email,
+        subject=subject,
+        html=html,
+        plaintext=content)
 
     
