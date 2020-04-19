485084c1a8a28ccd4452002e08e594743c8d434c
captainmeta4 <captainmeta4@gmail.com>
2020-04-19 11:20:50 -0400

    Update mailing_list.py

--- scripts/mailing_list.py
+++ scripts/mailing_list.py
@@ -6,20 +6,30 @@ from flask import render_template
 title = input("Title: ")
 subject = input("Email subject: ")
 
-#for user in db.query(classes.user.User).filter_by(is_activated=True, is_banned=0, is_deleted=False):
-for user in db.query(classes.user.User).filter_by(id=7).all():
+x=db.query(classes.user.User).filter_by(is_activated=True, is_banned=0, is_deleted=False)
+print(f"total mail to send: {x.count()}")
 
-    with app.app_context():
-        html=render_template(
-            "email/mailing.html",
-            title=title,
-            user=user
-            )
+for user in x.all():
+#for user in db.query(classes.user.User).filter_by(id=7).all():
+
+	try:
+	    with app.app_context():
+    	    html=render_template(
+        	    "email/mailing.html",
+            	title=title,
+         	   user=user
+            	)
         
-    send_mail(
-        to_address=user.email,
-        subject=subject,
-        html=html
-        )
+	    send_mail(
+    	    to_address=user.email,
+        	subject=subject,
+	        html=html
+	        )
+	    print(f"mailed to @{user.username}")
+	except:
+		print(f"unable to mail to @{user.username}")
+
+print("all done")
+
 
     
