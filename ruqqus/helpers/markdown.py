1470178a85e3cffdfb2ec53b7df9286b0f18dd4b
captainmeta4 <captainmeta4@gmail.com>
2020-04-26 11:08:03 -0400

    Update markdown.py

--- ruqqus/helpers/markdown.py
+++ ruqqus/helpers/markdown.py
@@ -70,11 +70,8 @@ class CustomRenderer(HTMLRenderer):
         target = token.target[1]
 
         if "post_id" not in self.__dict__:
-            return f"@{target}"
+            return f"[no op found]"
 
         post=get_submission(self.post_id)
         user=post.author
-
-
-
         return f'{space}<a href="{user.permalink}" class="d-inline-block"><img src="/@{user.username}/pic/profile" class="profile-pic-20 mr-1">@{user.username}</a>'
