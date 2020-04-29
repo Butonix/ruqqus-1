06ba9df0cba12b90ea209060dbc9b99f33840301
captainmeta4 <captainmeta4@gmail.com>
2020-04-29 18:57:40 -0400

    Update posts.py

--- ruqqus/routes/posts.py
+++ ruqqus/routes/posts.py
@@ -341,7 +341,7 @@ def submit_post(v):
                         over_18=(bool(request.form.get("over_18","")) or board.over_18),
                         post_public=not board.is_private,
                         author_name=user_name,
-                        guild_name=board.name
+                        guild_name=board.name,
                         repost_id=repost.id if repost else None
                         )
 
