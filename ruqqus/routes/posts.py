78a633c8327d649120e640f5802e1a581ab11d25
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-06-05 08:33:18 -0400

    Update posts.py

--- ruqqus/routes/posts.py
+++ ruqqus/routes/posts.py
@@ -33,7 +33,7 @@ BUCKET="i.ruqqus.com"
 @app.route("/post/<base36id>", methods=["GET"])
 @app.route("/post/<base36id>/<anything>", methods=["GET"])
 @auth_desired
-def post_base36id(base36id, v=None):
+def post_base36id(base36id, anything=None, v=None):
     
     post=get_post_with_comments(base36id, v=v, sort_type=request.args.get("sort","top"))
 
