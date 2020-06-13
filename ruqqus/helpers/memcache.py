83d940e868d18d2a182ed80b69f041eb2be86068
captainmeta4 <captainmeta4@gmail.com>
2020-06-13 10:15:16 -0400

    Update memcache.py

--- ruqqus/helpers/memcache.py
+++ ruqqus/helpers/memcache.py
@@ -1,9 +1,9 @@
 from os import environ
 import pylibmc
-import flask_caching
+import flask_caching.backends as backend
 
 
-class Custom_Cache(pylibmc.Client, flask_caching.backends.base.BaseCache):
+class Custom_Cache(pylibmc.Client, backend.base.BaseCache):
     pass
 
 def custom_memcache(app, config, *args, **kwargs):
