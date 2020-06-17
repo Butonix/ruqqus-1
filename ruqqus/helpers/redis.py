0573b0e3b853e82cc1c57d8151331dd1e40db702
captainmeta4 <captainmeta4@gmail.com>
2020-06-16 21:37:18 -0400

    Update redis.py

--- ruqqus/helpers/redis.py
+++ ruqqus/helpers/redis.py
@@ -98,7 +98,7 @@ class CustomCache(flask_caching.backends.rediscache.RedisCache):
 		cache=self.key_to_cache(key)
 		cache.inc(key, delta=delta)
 
-	def dec(self, key, delta=1:)
+	def dec(self, key, delta=1)
 		cache=self.key_to_cache(key)
 		cache.dec(key, delta=delta)
 
