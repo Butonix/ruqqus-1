26e3c8d74e08031895e0aa54a37b5f88339521ab
captainmeta4 <captainmeta4@gmail.com>
2020-06-16 20:39:04 -0400

    Update redis.py

--- ruqqus/classes/redis.py
+++ ruqqus/classes/redis.py
@@ -19,15 +19,70 @@ class CustomCache(flask_caching.backends.rediscache.RedisCache):
 
 	def key_to_cache(self, key):
 
-		hash_object=hashlib.md5(bytes(key, 'utf-8'))
+		return self.caches[self.key_to_cache_number]
 
-		hexstring=hash_object.hexdigest()
+	def key_to_cache_number(self, key):
 
-		x=int(hexstring[-5:], 16) % len(self.caches)
+		return int(hashlib.md5(bytes(key, 'utf-8')).hexdigest()[-5:], 16) % len(self.caches)
 
-		return self.caches[x]
+	def get(self, key):
 
-	
+		cache=self.key_to_cache(key)
+
+		return cache.get(key)
+
+	def get_many(self, *keys):
+
+		sharded_keys={i: [] for i in range(len(self.caches))}
+		for key in keys:
+			cache=self.key_to_cache_number(key)
+			sharded_keys[cache].append(key)
+
+		objects={i: self.caches[i].get_many[sharded_keys[i]] for i in range(len(self.caches))}
+
+		output=[]
+		for i in objects:
+			output+=objects[i]
+
+		output.sort(key=lambda x:keys.index(x))
+
+		return output
 
 
+    def set(self, key, value, timeout=None):
 
+    	cache=self.key_to_cache(key)
+    	return cache.set(key, value, timeout=timeout)
+
+    def add(self, key, value, timeout=None):
+    	cache=self.key_to_cache(key)
+    	return cache.add(key, value, timeout=timeout)
+
+    def set_many(self, mapping, timeout=None):
+
+    	caches={i:{} for i in range(len(self.caches))}
+    	for key, value in mapping:
+    		caches[self.key_to_cache_number(key)][key]=value
+
+    	for i in cache:
+    		self.caches[i].set_many(caches[i], timeout=timeout)
+
+    def delete(self, key)
+
+    	cache=self.key_to_cache(key)
+    	return cache.delete(key)
+
+	def delete_many(self, *keys):
+
+		if not keys:
+			return
+
+		sharded_keys={i: [] for i in range(len(self.caches))}
+		for key in keys:
+			cache=self.key_to_cache_number(key)
+			sharded_keys[cache].append(key)
+
+		for i in sharded_keys:
+			self.caches[i].delete_many(sharded_keys[i])
+
+		return True
