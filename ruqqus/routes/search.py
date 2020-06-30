4f920162639cbbc51b5a241196ffcdee0ce0c009
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-06-30 17:32:22 -0400

    search fix

--- ruqqus/routes/search.py
+++ ruqqus/routes/search.py
@@ -13,13 +13,13 @@ def searchlisting(q, v=None, page=1, sort="hot"):
 
 
     if not (v and v.over_18):
-        posts=posts.filter_by(over_18=False)
+        posts=posts.filter(Submisison.over_18==False)
 
     if v and v.hide_offensive:
-        posts=posts.filter_by(is_offensive=False)
+        posts=posts.filter(Submission.is_offensive==False)
 
     if not(v and v.admin_level>=3):
-        posts=posts.filter_by(is_deleted=False, is_banned=False)
+        posts=posts.filter(Submission.is_deleted==False, Submission.is_banned==False)
 
     if v and v.admin_level >= 4:
         pass
