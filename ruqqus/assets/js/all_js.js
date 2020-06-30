ac7e680e5a775f3eb81d1bc85585206144c78e94
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-06-30 17:17:09 -0400

    fix edit clikc

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -1608,6 +1608,7 @@ comment_edit=function(id){
     if (xhr.status==200) {
       commentForm=document.getElementById('comment-text-'+id);
       commentForm.innerHTML=JSON.parse(xhr.response)["html"];
+      document.getElementById('cancel-edit-'+id).click()
       $('#toast-comment-success').toast('dispose');
       $('#toast-comment-error').toast('dispose');
       $('#toast-comment-success').toast('show');
