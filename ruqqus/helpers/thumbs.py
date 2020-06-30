47e98ea84bb6867d3ba2b1c8052922d76bd92730
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-06-30 18:16:14 -0400

    Update thumbs.py

--- ruqqus/helpers/thumbs.py
+++ ruqqus/helpers/thumbs.py
@@ -6,13 +6,13 @@ from PIL import Image as PILimage
 from flask import g
 
 from .get import *
-from ruqqus.__main__ import app
+from ruqqus.__main__ import app, db_session
 
 headers={"User-Agent":app.config["UserAgent"]}
 
 def thumbnail_thread(pid):
     
-    db=app.config["databases"]()
+    db=db_session()
 
     post=get_post(pid, session=db)
 
