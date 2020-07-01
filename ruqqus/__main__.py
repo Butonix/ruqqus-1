dfcd127d1b7dd3da68b80ec0827a8ba811641315
captainmeta4 <captainmeta4@gmail.com>
2020-06-30 21:36:32 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -66,7 +66,7 @@ app.config["CACHE_KEY_PREFIX"]="flask_caching_"
 app.config["REDIS_POOL_SIZE"]=int(environ.get("REDIS_POOL_SIZE", 30))
 
 redispool=BlockingConnectionPool(max_connections=app.config["REDIS_POOL_SIZE"])
-app.config["CACHE_OPTIONS"]={'connection_pool':redispool}
+#app.config["CACHE_OPTIONS"]={'connection_pool':redispool}
 
 Markdown(app)
 cache=Cache(app)
