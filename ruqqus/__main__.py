5467835aaf7b02c117e62b73aee60c85f4d53473
captainmeta4 <captainmeta4@gmail.com>
2020-07-01 00:40:03 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -35,7 +35,8 @@ app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=2)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_CONNECTION_POOL_URL", environ.get("DATABASE_URL"))
 app.config['SQLALCHEMY_READ_URIS']=[
-     environ.get("HEROKU_POSTGRESQL_JADE_URL", environ.get("HEROKU_POSTGRESQL_BROWN_URL"))
+     environ.get("HEROKU_POSTGRESQL_JADE_URL", environ.get("HEROKU_POSTGRESQL_BROWN_URL")),
+     environ.get("HEROKU_POSTGRESQL_PINK_URL", environ.get("HEROKU_POSTGRESQL_GOLD_URL"))
      ]
 
 app.config['SECRET_KEY']=environ.get('MASTER_KEY')
