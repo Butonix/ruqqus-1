4e3baf3540e08e7cd30b91800272e32cf5f873b7
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-07-01 14:18:04 -0400

    Update submission.py

--- ruqqus/classes/submission.py
+++ ruqqus/classes/submission.py
@@ -56,7 +56,7 @@ class Submission(Base, Stndrd, Age_times, Scores, Fuzzing):
     board_id=Column(Integer, ForeignKey("boards.id"), default=None)
     original_board_id=Column(Integer, ForeignKey("boards.id"), default=None)
     over_18=Column(Boolean, default=False)
-    original_board=relationship("Board", lazy="dynamic", primaryjoin="Board.id==Submission.original_board_id")
+    original_board=relationship("Board", primaryjoin="Board.id==Submission.original_board_id")
     creation_ip=Column(String(64), default="")
     mod_approved=Column(Integer, default=None)
     accepted_utc=Column(Integer, default=0)
