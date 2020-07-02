545f9e8a0717ef16cd41e0a041fb9b4d447a6c6a
captainmeta4 <captainmeta4@gmail.com>
2020-07-01 22:45:26 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -105,11 +105,11 @@ class RoutingSession(Session):
             else:
                 return random.choice(engines['followers'])
         except:
-            if self._flushing or request.method=="POST":
+            if self._flushing:
                 return engines['leader']
             else:
                 return random.choice(engines['followers'])
-        
+
 db_session=scoped_session(sessionmaker(class_=RoutingSession))
 #db_session=scoped_session((sessionmaker(bind=engines["leader"]))
 
