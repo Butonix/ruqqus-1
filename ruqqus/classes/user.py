6c4d61cfd7db043e980e3367bf4225c8bfc37af3
arkit3kt <exodust@protonmail.com>
2020-07-05 00:13:42 -0400

    allow older accounts to create guilds with 50 rep

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -549,14 +549,7 @@ class User(Base, Stndrd):
 
     @property
     def can_make_guild(self):
-
-        if self.true_score < 250:
-            return False
-
-        if len(self.boards_modded) >= 10:
-            return False
-
-        return True
+        return (self.true_score > 250 or self.created_utc <= 1592974538 and self.true_score > 50) and len(self.boards_modded) <= 10
     
     @property
     def can_join_gms(self):
