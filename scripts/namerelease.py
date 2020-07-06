9b2beef6f5bc0afc96c283596c9813beed88e1c7
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-07-06 10:34:04 -0400

    Update namerelease.py

--- scripts/namerelease.py
+++ scripts/namerelease.py
@@ -8,8 +8,8 @@ now=(int)
 
 deleted_accounts=db.query(User).filter_by(is_deleted=True)
 
-displayed_post_owners=db.query(Submission.author_id).filter(is_deleted=False, is_banned=False).distinct().subquery()
-displayed_comment_owners=db.query(Comment.author_id).filter(is_deleted=False, is_banned=False).distinct().subquery()
+displayed_post_owners=db.query(Submission.author_id).filter_by(is_deleted=False, is_banned=False).distinct().subquery()
+displayed_comment_owners=db.query(Comment.author_id).filter_by(is_deleted=False, is_banned=False).distinct().subquery()
 
 banned_accounts=db.query(User).filter_by(unban_utc=0).filter(User.is_banned>0, User.id.notin_(displayed_post_owners), User.id.notin_(displayed_comments_owners))
 
