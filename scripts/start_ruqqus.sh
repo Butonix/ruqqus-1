3d7e111e1ae4c3c159f199c9024fbc00a5301a71
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-07-07 17:04:11 -0400

    Update start_ruqqus.sh

--- scripts/start_ruqqus.sh
+++ scripts/start_ruqqus.sh
@@ -1,4 +1,4 @@
 cd ~
 source ruqqus_configs.sh
 cd ~/ruqqus
-newrelic-admin run-program gunicorn ruqqus.__main__:app --daemon -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload
+newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload
