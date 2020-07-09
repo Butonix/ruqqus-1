05611f3bd3ef49ab9a42c7f9a17bf90bb5a4a152
captainmeta4 <captainmeta4@gmail.com>
2020-07-08 20:09:08 -0400

    Update patreon.py

--- ruqqus/routes/patreon.py
+++ ruqqus/routes/patreon.py
@@ -1,5 +1,6 @@
 from os import environ
 import requests
+import urllib
 
 from flask import *
 
@@ -13,7 +14,17 @@ PATREON_SECRET=environ.get("PATREON_SECRET")
 
 PATREON_CLIENT_ID=environ.get("PATREON_CLIENT_ID")
 PATREON_CLIENT_SECRET=environ.get("PATREON_CLIENT_SECRET")
-@app.route("/api/patreon", methods=["POST"])
-def api_patreon():
 
-	pass
+@app.route("/patreon_authorize", methods=["GET"])
+@auth_required
+def patreon_authorize(v):
+
+	redirect_uri=urllib.parse.quote("https://ruqqus.com/redirect/patreon", safe='')
+
+	state=generate_hash(f"{session.get('session_id')}+{v.id}")
+
+	url=f"www.patreon.com/oauth2/authorize?response_type=code&client_id={PATREON_CLIENT_ID}&redirect_uri={redirect_uri}&state={state}"
+
+	return redirect(url)
+
+	
