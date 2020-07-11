f2adb265b6bb3fb08295baf5b99dbabcceb7495e
Adam LaDine <captainmeta4@users.noreply.github.com>
2020-07-11 14:22:38 -0400

    Update user.py

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -554,7 +554,7 @@ class User(Base, Stndrd):
 
     @property
     def can_make_guild(self):
-        return (self.true_score > 250 or self.created_utc <= 1592974538 and self.true_score > 50 or self.patreon_pledge_cents>500) and len(self.boards_modded) < 10
+        return (self.true_score > 250 or self.created_utc <= 1592974538 and self.true_score > 50 or (self.patreon_pledge_cents and self.patreon_pledge_cents>500)) and len(self.boards_modded) < 10
     
     @property
     def can_join_gms(self):
@@ -573,7 +573,7 @@ class User(Base, Stndrd):
 
     @property
     def can_submit_image(self):
-        return self.patreon_pledge_cents>=500 or self.true_score >= 1000 or (self.created_utc <= 1592974538 and self.true_score >= 500)
+        return (self.patreon_pledge_cents and self.patreon_pledge_cents>=500) or self.true_score >= 1000 or (self.created_utc <= 1592974538 and self.true_score >= 500)
 
     @property
     def can_upload_avatar(self):
