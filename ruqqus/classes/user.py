f839f3377ee949f0eea61efb9187d390b2a50539
captainmeta4 <captainmeta4@gmail.com>
2020-07-11 10:29:51 -0400

    Update user.py

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -68,7 +68,7 @@ class User(Base, Stndrd):
     show_nsfl=Column(Boolean, default=False)
     is_private=Column(Boolean, default=False)
     read_announcement_utc=Column(Integer, default=0)
-    discord_id=Column(Integer, default=None)
+    #discord_id=Column(Integer, default=None)
     unban_utc=Column(Integer, default=0)
     is_deleted=Column(Boolean, default=False)
     delete_reason=Column(String(500), default='')
