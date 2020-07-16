e48155b782248be5d8272f4c423e4a3353916f50
Foxite <the@dirkkok.nl>
2020-07-16 16:44:32 +0200

    removed unnecessary property from user.py, which was covered by Stndrd (created_date)

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -408,12 +408,6 @@ class User(Base, Stndrd):
     def permalink(self):
         return self.url
 
-    @property
-    @lazy
-    def created_date(self):
-
-        return time.strftime("%d %B %Y", time.gmtime(self.created_utc))
-
     def __repr__(self):
         return f"<User(username={self.username})>"
 
