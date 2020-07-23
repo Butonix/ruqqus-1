dd03f49e5024b88a2b541f5faf1cfa1acfc330fb
kek7198 <memezdank140@gmail.com>
2020-07-23 18:18:05 -0500

    detect mouse

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -1,3 +1,15 @@
+// Using mouse
+
+document.body.addEventListener('mousedown', function() {
+  document.body.classList.add('using-mouse');
+});
+
+document.body.addEventListener('keydown', function(event) {
+  if (event.keyCode === 9) {
+    document.body.classList.remove('using-mouse');
+  }
+});
+
 // 2FA toggle modal
 
 $('#2faModal').on('hidden.bs.modal', function () {
