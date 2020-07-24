dfab039adf505000afe4b0e4eeeffb3fc568b499
captainmeta4 <captainmeta4@users.noreply.github.com>
2020-07-24 15:03:56 -0400

    2.16.1

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -27,7 +27,7 @@ from redis import BlockingConnectionPool
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
-_version = "2.16.0"
+_version = "2.16.1"
 
 app = Flask(__name__,
             template_folder='./templates',
