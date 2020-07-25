1de173fd16719c7fd004fba7fa317b871139d867
captainmeta4 <captainmeta4@gmail.com>
2020-07-25 00:47:57 -0400

    Revert "Merge pull request #341 from ruqqus/mobile-voting"
    This reverts commit 9308caebdf52c7d00e102b2af5e36a64b1d1bed5, reversing
    changes made to bd15c67e531e07954199944e7281cf9d62641408.

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -1,15 +1,3 @@
-// Using mouse
-
-document.body.addEventListener('mousedown', function() {
-  document.body.classList.add('using-mouse');
-});
-
-document.body.addEventListener('keydown', function(event) {
-  if (event.keyCode === 9) {
-    document.body.classList.remove('using-mouse');
-  }
-});
-
 // 2FA toggle modal
 
 $('#2faModal').on('hidden.bs.modal', function () {
@@ -896,139 +884,10 @@ function toggle_sidebar_expand() {
 
 }
 
-// Voting
 
-var upvote = function(event) {
-  var type = event.target.dataset.contentType;
-  var id = event.target.dataset.idUp;
+//Voting
 
-  var downvoteButton = document.getElementsByClassName(type + '-' + id + '-down');
-  var upvoteButton = document.getElementsByClassName(type + '-' + id + '-up');
-  var scoreText = document.getElementsByClassName(type + '-score-' + id);
 
-  for (var j = 0; j < upvoteButton.length && j < downvoteButton.length && j < scoreText.length; j++) {
-
-    var thisUpvoteButton = upvoteButton[j];
-    var thisDownvoteButton = downvoteButton[j];
-    var thisScoreText = scoreText[j];
-    var thisScore = Number(thisScoreText.textContent);
-
-    if (thisUpvoteButton.classList.contains('active')) {
-      thisUpvoteButton.classList.remove('active')
-      thisScoreText.textContent = thisScore - 1
-      voteDirection = "0"
-    } else if (thisDownvoteButton.classList.contains('active')) {
-      thisUpvoteButton.classList.add('active')
-      thisDownvoteButton.classList.remove('active')
-      thisScoreText.textContent = thisScore + 2
-      voteDirection = "1"
-    } else {
-      thisUpvoteButton.classList.add('active')
-      thisScoreText.textContent = thisScore + 1
-      voteDirection = "1"
-    }
-
-    if (thisUpvoteButton.classList.contains('active')) {
-      thisScoreText.classList.add('score-up')
-      thisScoreText.classList.remove('score-down')
-      thisScoreText.classList.remove('score')
-    } else if (thisDownvoteButton.classList.contains('active')) {
-      thisScoreText.classList.add('score-down')
-      thisScoreText.classList.remove('score-up')
-      thisScoreText.classList.remove('score')
-    } else {
-      thisScoreText.classList.add('score')
-      thisScoreText.classList.remove('score-up')
-      thisScoreText.classList.remove('score-down')
-    }
-  }
-
-  for (var n = 0; n < 1; n++) {
-    callback=function() {
-      console.log('voted')
-    }
-    post("/api/vote/" + type + "/" + id + "/" + voteDirection, callback, "Unable to vote at this time. Please try again later.")
-  }
-}
-
-var downvote = function(event) {
-  var type = event.target.dataset.contentType;
-  var id = event.target.dataset.idDown;
-
-  var downvoteButton = document.getElementsByClassName(type + '-' + id + '-down');
-  var upvoteButton = document.getElementsByClassName(type + '-' + id + '-up');
-  var scoreText = document.getElementsByClassName(type + '-score-' + id);
-
-  for (var j = 0; j < upvoteButton.length && j < downvoteButton.length && j < scoreText.length; j++) {
-
-    var thisUpvoteButton = upvoteButton[j];
-    var thisDownvoteButton = downvoteButton[j];
-    var thisScoreText = scoreText[j];
-    var thisScore = Number(thisScoreText.textContent);
-
-    if (thisDownvoteButton.classList.contains('active')) {
-      thisDownvoteButton.classList.remove('active')
-      thisScoreText.textContent = thisScore + 1
-      voteDirection = "0"
-    } else if (thisUpvoteButton.classList.contains('active')) {
-      thisDownvoteButton.classList.add('active')
-      thisUpvoteButton.classList.remove('active')
-      thisScoreText.textContent = thisScore - 2
-      voteDirection = "-1"
-    } else {
-      thisDownvoteButton.classList.add('active')
-      thisScoreText.textContent = thisScore - 1
-      voteDirection = "-1"
-    }
-
-    if (thisUpvoteButton.classList.contains('active')) {
-      thisScoreText.classList.add('score-up')
-      thisScoreText.classList.remove('score-down')
-      thisScoreText.classList.remove('score')
-    } else if (thisDownvoteButton.classList.contains('active')) {
-      thisScoreText.classList.add('score-down')
-      thisScoreText.classList.remove('score-up')
-      thisScoreText.classList.remove('score')
-    } else {
-      thisScoreText.classList.add('score')
-      thisScoreText.classList.remove('score-up')
-      thisScoreText.classList.remove('score-down')
-    }
-  }
-
-  for (var n = 0; n < 1; n++) {
-    callback=function() {
-      console.log('voted')
-    }
-    post("/api/vote/" + type + "/" + id + "/" + voteDirection, callback, "Unable to vote at this time. Please try again later.")
-  }
-}
-
-var upvoteButtons = document.getElementsByClassName('upvote-button')
-
-var downvoteButtons = document.getElementsByClassName('downvote-button')
-
-var voteDirection = 0
-
-for (var i = 0; i < upvoteButtons.length; i++) {
-  upvoteButtons[i].addEventListener('click', upvote, false);
-  upvoteButtons[i].addEventListener('keydown', function(event) {
-    if (event.keyCode === 13) {
-      upvote(event)
-    }
-  }, false)
-};
-
-for (var i = 0; i < downvoteButtons.length; i++) {
-  downvoteButtons[i].addEventListener('click', downvote, false);
-  downvoteButtons[i].addEventListener('keydown', function(event) {
-    if (event.keyCode === 13) {
-      downvote(event)
-    }
-  }, false)
-};
-
-/*
 
 function vote(post_id, direction) {
   url="/api/vote/post/"+post_id+"/"+direction;
@@ -1106,7 +965,6 @@ function vote(post_id, direction) {
   post(url, callback, "Unable to vote at this time. Please try again later.");
 };
 
-*/
 
 function vote_comment(comment_id, direction) {
   url="/api/vote/comment/"+ comment_id +"/"+direction;
@@ -1440,7 +1298,7 @@ document.addEventListener('paste', function (event) {
   if (nothingFocused) {
 
     if (document.getElementById('guild-name-reference')) {
-      var guild = document.getElementById('guild-name-reference').innerText;
+        var guild = document.getElementById('guild-name-reference').innerText;
     }
 
     var clipText = event.clipboardData.getData('Text');
@@ -1575,9 +1433,9 @@ function exile_from_guild(boardid) {
         window.location.reload(true);
       }
       else {
-        $('#toast-exile-error').toast('dispose');
-        $('#toast-exile-error').toast('show');
-        exileError.textContent = JSON.parse(xhr.response)["error"];
+      $('#toast-exile-error').toast('dispose');
+      $('#toast-exile-error').toast('show');
+      exileError.textContent = JSON.parse(xhr.response)["error"];
       }
     }
     xhr.send(f)
@@ -1682,9 +1540,9 @@ block_user=function() {
         window.location.reload(true);
       }
       else {
-        $('#toast-exile-error').toast('dispose');
-        $('#toast-exile-error').toast('show');
-        exileError.textContent = JSON.parse(xhr.response)["error"];
+      $('#toast-exile-error').toast('dispose');
+      $('#toast-exile-error').toast('show');
+      exileError.textContent = JSON.parse(xhr.response)["error"];
       }
     }
     xhr.send(f)
@@ -1711,7 +1569,7 @@ post_comment=function(fullname){
     if (xhr.status==200) {
       commentForm=document.getElementById('comment-form-space-'+fullname);
       commentForm.innerHTML=JSON.parse(xhr.response)["html"];
-      $('#toast-comment-success').toast('dispose');
+        $('#toast-comment-success').toast('dispose');
       $('#toast-comment-error').toast('dispose');
       $('#toast-comment-success').toast('show');
     }
@@ -1719,7 +1577,7 @@ post_comment=function(fullname){
       $('#toast-comment-success').toast('dispose');
       $('#toast-comment-error').toast('dispose');
       $('#toast-comment-error').toast('show');
-      commentError.textContent = JSON.parse(xhr.response)["error"];
+     commentError.textContent = JSON.parse(xhr.response)["error"];
     }
   }
   xhr.send(form)
@@ -1728,14 +1586,14 @@ post_comment=function(fullname){
 //part of submit page js
 
 hide_image=function(){
-  x=document.getElementById('image-upload-block');
-  url=document.getElementById('post-URL').value;
-  if (url.length>=1){
-    x.classList.add('d-none');
-  }
-  else {
-    x.classList.remove('d-none');
-  }
+    x=document.getElementById('image-upload-block');
+    url=document.getElementById('post-URL').value;
+    if (url.length>=1){
+        x.classList.add('d-none');
+    }
+    else {
+        x.classList.remove('d-none');
+    }
 }
 
 
@@ -1765,7 +1623,7 @@ comment_edit=function(id){
       $('#toast-comment-success').toast('dispose');
       $('#toast-comment-error').toast('dispose');
       $('#toast-comment-error').toast('show');
-      commentError.textContent = JSON.parse(xhr.response)["error"];
+     commentError.textContent = JSON.parse(xhr.response)["error"];
     }
   }
   xhr.send(form)
