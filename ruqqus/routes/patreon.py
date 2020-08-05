a5959253a6ef96c8e9dc0d7050b689a6658bacfa
captainmeta4 <captainmeta4@gmail.com>
2020-08-05 16:22:32 -0400

    Update patreon.py

--- ruqqus/routes/patreon.py
+++ ruqqus/routes/patreon.py
@@ -167,7 +167,7 @@ def webhook_patreon():
     event_type=request.headers.get("X-Patreon-Event")
 
     if event_type in ["members:pledge:create","members:pledge:update"]:
-        user.patreon_pledge_cents=data["data"]["attributes"]["amount_cents"]
+        user.patreon_pledge_cents=data["data"]["attributes"]["pledge_amount_cents"]
     elif event_type=="members:pledge:delete":
         user.patreon_pledge_cents=0
     else:
