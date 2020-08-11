4ab7384b4cd44b4ee85ab65055a44c1daaf8087b
Mark Pieciak <mpieciak18@msn.com>
2020-08-11 11:23:32 -0400

    Updated JSON for submissions

--- ruqqus/classes/submission.py
+++ ruqqus/classes/submission.py
@@ -325,7 +325,9 @@ class Submission(Base, Stndrd, Age_times, Scores, Fuzzing):
                 'embed_url':self.embed_url,
                 'is_archived':self.is_archived,
                 'author_title':self.author.title.json if self.author.title else None,
-                'original_guild_name':self.original_board.name
+                'original_guild_name':self.original_board.name,
+                'comment_count':self.comment_count
+                'score':self.score
                 }
 
         if "_voted" in self.__dict__:
