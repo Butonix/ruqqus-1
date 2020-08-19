e7b0da74e5d699420422d191d233fb8e91e8f55b
arkit3kt <exodust@protonmail.com>
2020-08-19 15:44:37 -0400

    "true"

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -248,7 +248,7 @@ def after_request(response):
 
     response.headers.add("Access-Control-Allow-Origin", domain)
     if request.headers.get("Origin") == domain:
-        response.headers.add("Access-Control-Allow-Credentials", True)
+        response.headers.add("Access-Control-Allow-Credentials", "true")
     response.headers.add("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
     response.headers.add("Cache-Control",
                          "maxage=600")
