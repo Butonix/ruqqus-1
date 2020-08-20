b730e47c83ea423dac948c96eb4fb8a4bdcb1bf0
captainmeta4 <captainmeta4@gmail.com>
2020-08-20 10:52:13 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -101,9 +101,12 @@ app.config["RATELIMIT_STORAGE_URL"]=environ.get("REDIS_URL", "memory://")
 app.config["RATELIMIT_KEY_PREFIX"]="flask_limiting_"
 app.config["RATELIMIT_ENABLED"]=bool(int(environ.get("RATELIMIT_ENABLED", True)))
 
+def limiter_key_func():
+    return request.remote_addr
+
 limiter = Limiter(
     app,
-    key_func=get_remote_address,
+    key_func=limiter_key_func,
     default_limits=["100/minute"],
     headers_enabled=True,
     strategy="fixed-window"
