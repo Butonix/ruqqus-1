9fd6385aad1019f2e55ef88aaf6ac4d90d146c8d
foxite <d.kok.2000@gmail.com>
2020-08-21 23:30:05 +0200

    .strip() the 2fa code

--- ruqqus/routes/login.py
+++ ruqqus/routes/login.py
@@ -118,7 +118,7 @@ def login_post():
                             ):
             return redirect("/login")
         
-        if not account.validate_2fa(request.form.get("2fa_token", "")):
+        if not account.validate_2fa(request.form.get("2fa_token", "").strip()):
             hash=generate_hash(f"{account.id}+{time}+2fachallenge")
             return render_template("login_2fa.html",
                                    v=account,
