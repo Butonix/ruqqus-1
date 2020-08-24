6c1e3e244ce0196d3856ec838d6eb4ebe854519d
captainmeta4 <captainmeta4@gmail.com>
2020-08-24 12:10:36 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -68,7 +68,7 @@ app.config["CACHE_DIR"]=environ.get("CACHE_DIR", "ruqquscache")
 
 #captcha configs
 app.config["HCAPTCHA_SITEKEY"]=environ.get("HCAPTCHA_SITEKEY")
-app.config["HCAPTCHA_SECRET"]=eniviron.get("HCAPTCHA_SECRET")
+app.config["HCAPTCHA_SECRET"]=environ.get("HCAPTCHA_SECRET")
 
 
 
