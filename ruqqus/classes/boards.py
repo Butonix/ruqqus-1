e066777500083c556548748acdad5d73361dcf90
captainmeta4 <captainmeta4@gmail.com>
2020-08-25 13:50:33 -0400

    Update boards.py

--- ruqqus/classes/boards.py
+++ ruqqus/classes/boards.py
@@ -69,7 +69,7 @@ class Board(Base, Stndrd, Age_times):
     @property
     def mods_list(self):
 
-        z= [x for x in self.moderators.all() if x.accepted and not x.user.is_deleted]
+        z= [x for x in self.moderators if x.accepted and not x.user.is_deleted]
 
         z=sorted(z, key=lambda x: x.id)
         return z
