c339ba0de18fc039f26647ca7ad80f0db64fb17b
captainmeta4 <captainmeta4@gmail.com>
2020-08-25 14:57:37 -0400

    Update boards.py

--- ruqqus/classes/boards.py
+++ ruqqus/classes/boards.py
@@ -226,7 +226,7 @@ class Board(Base, Stndrd, Age_times):
         if user is None:
             return False
 
-        return self.contributors.filter_by(user_id=user.id, is_active=True).first()
+        return g.db.query(ContributorRelationship).filter_by(user_id=user.id, is_active=True).first()
 
     def can_submit(self, user):
 
