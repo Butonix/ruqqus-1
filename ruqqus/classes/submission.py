e08a7e6c1d998a874a9fee969aaa0bb36597b649
captainmeta4 <captainmeta4@gmail.com>
2020-08-26 09:10:43 -0400

    fix

--- ruqqus/classes/submission.py
+++ ruqqus/classes/submission.py
@@ -428,10 +428,10 @@ class Submission(Base, Stndrd, Age_times, Scores, Fuzzing):
 
     @property
     def flag_count(self):
-        return g.db.query(Flag).filter_by(post_id=self.id).count()
+        return len(self.flags)
 
     @property
     def report_count(self):
-        return g.db.query(Report).filter_by(post_id=self.id).count()
+        return len(self.reports)
     
     
