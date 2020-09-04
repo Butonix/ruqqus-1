424a8da800f4dad0273a8cd62d448f3f47acc895
captainmeta4 <captainmeta4@gmail.com>
2020-09-04 19:19:44 -0400

    fix

--- scripts/start_ruqqus.sh
+++
@@ -1,7 +0,0 @@
-cd ~/ruqqus
-pip3 install -r requirements.txt
-export PYTHONPATH=$PYTHONPATH:~/ruqqus
-cd ~
-source env.sh
-cd ~/ruqqus/ruqqus
-newrelic-admin run-program gunicorn __main__:app --daemon -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload
---
+++ startup.sh
@@ -0,0 +1,6 @@
+cd ~/ruqqus
+pip3 install -r requirements.txt
+export PYTHONPATH=$PYTHONPATH:~/ruqqus
+cd ~
+source env.sh
+newrelic-admin run-program gunicorn ruqqus.__main__:app --daemon -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload
