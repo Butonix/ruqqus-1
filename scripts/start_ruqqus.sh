82b7172368f82cf88b8cb0fcdc99b91c9d32ca13
captainmeta4 <captainmeta4@gmail.com>
2020-09-04 17:46:13 -0400

    Update start_ruqqus.sh

--- scripts/start_ruqqus.sh
+++ scripts/start_ruqqus.sh
@@ -1,4 +1,6 @@
+pip3 install -r requirements.txt
+export PATH=$PATH:~/ruqqus/ruqqus
 cd ~
 source ruqqus_configs.sh
-cd ~/ruqqus
-newrelic-admin run-program gunicorn ruqqus.__main__:app --daemon -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload
+cd ~/ruqqus/ruqqus
+newrelic-admin run-program gunicorn __main__:app --daemon -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload
