441843287030cc080dcdbb34f86fba7a98277d40
captainmeta4 <captainmeta4@gmail.com>
2020-09-04 19:34:21 -0400

    fix discord link

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 pip3 install -r ruqqus/requirements.txt
 export PYTHONPATH=$PYTHONPATH:~/ruqqus
+export CACHE_TYPE=filesystem
 cd ~
 source env.sh
 newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload
