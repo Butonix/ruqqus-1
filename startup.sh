9f64920ab8302bd6cd18c4d86b8b9922958e2246
captainmeta4 <captainmeta4@gmail.com>
2020-09-05 11:29:01 -0400

    venv

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
+cd ~
+source ~/venv/bin/activate
 cd ~/ruqqus
 pip3 install -r requirements.txt
 export PYTHONPATH=$PYTHONPATH:~/ruqqus
 export CACHE_TYPE=filesystem
 cd ~
 source env.sh
-gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000
+gunicorn __main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000
+deactivate
