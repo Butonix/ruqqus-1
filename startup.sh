40af6efb5f512e1fea01fc978714fae5b0741e16
captainmeta4 <captainmeta4@gmail.com>
2020-09-04 20:46:31 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ export PYTHONPATH=$PYTHONPATH:~/ruqqus
 export CACHE_TYPE=filesystem
 cd ~
 source env.sh
-newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload -b 0.0.0.0
+newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 0.0.0.0:8000
