2f97dc4ee45150694772bec290e078eda899e59b
captainmeta4 <captainmeta4@gmail.com>
2020-09-05 11:34:54 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,6 +6,7 @@ pip3 install -r requirements.txt
 export PYTHONPATH=$PYTHONPATH:~/ruqqus
 export CACHE_TYPE=filesystem
 export CACHE_DIR=cachetemp
+export S3_BUCKET_NAME=i.ruqqus.com
 cd ~
 gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000
 deactivate
