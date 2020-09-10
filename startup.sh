1a29a88b776badd68aa41679105f92d6f471c7d5
captainmeta4 <captainmeta4@gmail.com>
2020-09-10 12:20:55 -0400

    fix

--- startup.sh
+++ startup.sh
@@ -9,7 +9,7 @@ pip3 install -r requirements.txt
 export domain=ruqqus.com
 export PYTHONPATH=$PYTHONPATH:~/ruqqus
 export S3_BUCKET_NAME=i.ruqqus.com
-export CACHE_TYPE=filesystem
+export CACHE_TYPE="filesystem"
 cd ~/
 NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000
 deactivate
