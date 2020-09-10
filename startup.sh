6ff5b1bba6858f951ecd55f8db5cde3c0988402c
captainmeta4 <captainmeta4@gmail.com>
2020-09-10 15:32:24 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,6 +10,7 @@ export domain=ruqqus.com
 export PYTHONPATH=$PYTHONPATH:~/ruqqus
 export S3_BUCKET_NAME=i.ruqqus.com
 export CACHE_TYPE="filesystem"
+export HCAPTCHA_SITEKEY="22beca86-6e93-421c-8510-f07c6914dadb"
 cd ~/
 NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000
 deactivate
