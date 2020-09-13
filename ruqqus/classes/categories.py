cf5b89930f763ae1b56c836dfb335bc289710171
arkit3kt <exodust@protonmail.com>
2020-09-12 22:17:20 -0400

    add category relationship data to json for boardCategories

--- ruqqus/classes/categories.py
+++ ruqqus/classes/categories.py
@@ -12,7 +12,7 @@ class BoardCategories(Base):
     category_id = Column(Integer, ForeignKey("categories.id"))
     board_id = Column(Integer, ForeignKey("boards.id"))
     created_utc = Column(Integer)
-
+    category = relationship("Categories", lazy="dynamic")
     def __repr__(self):
         return f"<BoardCategories(id={self.id}, board_id={self.board_id})>"
 
@@ -21,8 +21,10 @@ class BoardCategories(Base):
         return {'id': self.id,
                 'category_id': self.category_id,
                 'board_id': self.board_id,
-                'created_utc': self.created_utc
-                }
+                'created_utc': self.created_utc,
+                'name':self.category.name,
+                'description': self.category.description
+                ,}
 
 
 class Categories(Base):
@@ -30,7 +32,7 @@ class Categories(Base):
     __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True)
-    type = Column(String(64))
+    name = Column(String(64))
     description = Column(String(256))
     created_utc = Column(Integer)
 
@@ -40,7 +42,7 @@ class Categories(Base):
     @property
     def json(self):
         return {'id': self.id,
-                'type': self.type,
+                'type': self.name,
                 'description': self.description,
                 'created_utc': self.created_utc
                 }
