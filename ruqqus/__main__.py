980d5d0aa47f0cf78e77e0ca07fcaca5a6381b08
captainmeta4 <captainmeta4@gmail.com>
2020-09-18 19:57:45 -0400

    bumpversion

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -27,7 +27,7 @@ from redis import BlockingConnectionPool
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
-_version = "2.18.3"
+_version = "2.19.0"
 
 app = Flask(__name__,
             template_folder='./templates',
