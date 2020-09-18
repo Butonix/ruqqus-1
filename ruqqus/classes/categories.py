3f1d6a959de88cb97d7b93cced2cd24f4db44837
arkit3kt <exodust@protonmail.com>
2020-09-17 22:05:11 -0400

    update categories.py

--- ruqqus/classes/categories.py
+++ ruqqus/classes/categories.py
@@ -22,9 +22,9 @@ class BoardCategories(Base):
                 'category_id': self.category_id,
                 'board_id': self.board_id,
                 'created_utc': self.created_utc,
-                'name':self.category.name,
+                'name': self.category.name,
                 'description': self.category.description
-                ,}
+                }
 
 
 class Categories(Base):
@@ -42,7 +42,7 @@ class Categories(Base):
     @property
     def json(self):
         return {'id': self.id,
-                'type': self.name,
+                'name': self.name,
                 'description': self.description,
                 'created_utc': self.created_utc
                 }
