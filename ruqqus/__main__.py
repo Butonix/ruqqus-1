5e938b030343933f84cdd4458fc0a964ae414af1
captainmeta4 <captainmeta4@gmail.com>
2020-09-17 22:08:05 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -81,10 +81,10 @@ app.config["CACHE_REDIS_URL"]=environ.get("REDIS_URL").rstrip()
 app.config["CACHE_DEFAULT_TIMEOUT"]=60
 app.config["CACHE_KEY_PREFIX"]="flask_caching_"
 
-app.config["REDIS_POOL_SIZE"]=int(environ.get("REDIS_POOL_SIZE", 30))
+#app.config["REDIS_POOL_SIZE"]=int(environ.get("REDIS_POOL_SIZE", 30))
 
-redispool=BlockingConnectionPool(max_connections=app.config["REDIS_POOL_SIZE"])
-app.config["CACHE_OPTIONS"]={'connection_pool':redispool}
+#redispool=BlockingConnectionPool(max_connections=app.config["REDIS_POOL_SIZE"])
+#app.config["CACHE_OPTIONS"]={'connection_pool':redispool}
 
 
 #setup env vars - convenience statement
