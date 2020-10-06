a341c91a09ade299a5fd0d6928fb628ffad7a5ed
arkit3kt <exodust@protonmail.com>
2020-10-05 22:10:37 -0400

    pep8

--- scripts/mailing_list.py
+++ scripts/mailing_list.py
@@ -6,30 +6,27 @@ from flask import render_template
 title = input("Title: ")
 subject = input("Email subject: ")
 
-x=db.query(classes.user.User).filter_by(is_activated=True, is_banned=0)
+x = db.query(classes.user.User).filter_by(is_activated=True, is_banned=0)
 print(f"total mail to send: {x.count()}")
 
 for user in x.all():
-#for user in db.query(classes.user.User).filter_by(id=7).all():
+    # for user in db.query(classes.user.User).filter_by(id=7).all():
 
     try:
         with app.app_context():
-            html=render_template(
+            html = render_template(
                 "email/mailing.html",
                 title=title,
                 user=user
-                )
-        
+            )
+
         send_mail(
             to_address=user.email,
             subject=subject,
             html=html
-            )
+        )
         print(f"mailed to @{user.username}")
-    except:
+    except BaseException:
         print(f"unable to mail to @{user.username}")
 
 print("all done")
-
-
-    
