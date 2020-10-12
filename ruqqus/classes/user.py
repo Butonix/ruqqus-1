383f7f4d9a714cf6e0f1bcfdc17c400c60ba568d
captainmeta4 <captainmeta4@gmail.com>
2020-10-11 20:07:15 -0400

    @ruqqus user karma

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -388,12 +388,12 @@ class User(Base, Stndrd, Age_times):
     @property
     @cache.memoize(timeout=3600)  # 1hr cache time for user rep
     def karma(self):
-        return int(self.energy) - self.post_count
+        return 503 if self.id==1 else int(self.energy) - self.post_count
 
     @property
     @cache.memoize(timeout=3600)
     def comment_karma(self):
-        return int(self.comment_energy) - self.comments.filter(
+        return 503 if self.id==1 else int(self.comment_energy) - self.comments.filter(
             Comment.parent_submission is not None).filter_by(is_banned=False).count()
 
     @property
@@ -692,7 +692,7 @@ class User(Base, Stndrd, Age_times):
     @property
     def total_karma(self):
 
-        return max(self.karma + self.comment_karma, -5)
+        return 503 if self.id==1 else max(self.karma + self.comment_karma, -5)
 
     @property
     def can_use_darkmode(self):
