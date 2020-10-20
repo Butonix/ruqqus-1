82d50393934a44ce93e6483135cd90e754d71669
captainmeta4 <captainmeta4@gmail.com>
2020-10-20 19:32:08 -0400

    Update login.py

--- ruqqus/routes/login.py
+++ ruqqus/routes/login.py
@@ -324,9 +324,9 @@ def sign_up_post(v):
     #counteract gmail username+2 and extra period tricks - convert submitted email to actual inbox
     if email and email.endswith("@gmail.com"):
         parts=re.split("\+.*@", email)
-        username=parts[0]
-        username=username.replace(".","")
-        email=f"{username}@gmail.com"
+        gmail_username=parts[0]
+        gmail_username=gmail_username.replace(".","")
+        email=f"{gmail_username}@gmail.com"
 
 
     existing_account = g.db.query(User).filter(
