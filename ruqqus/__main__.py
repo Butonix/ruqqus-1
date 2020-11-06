2758df8f799ff40a830bda676d216655d18b955c
captainmeta4 <captainmeta4@gmail.com>
2020-11-05 20:10:45 -0500

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -153,40 +153,49 @@ class RoutingSession(Session):
             else:
                 return random.choice(engines['followers'])
 
+def retry(f):
+    def wrapper(*args, **kwargs):
+        i=0
+        while i<=3
+            i+=1
+
+            try:
+                return f(*args, **kwargs)
+
+            except OperationalError:
+                sleep(2**i)
+
+            except:
+                self.session.rollback()
+
+        abort(500)
+
+    wrapper.__name__=f.__name__
+    return wrapper
+
 class RetryingQuery(_Query):
     __max_retry_count__ = 3
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def __iter__(self):
-        attempts = 0
-        while True:
-            attempts += 1
-            try:
-                return super().__iter__()
-            except OperationalError as ex:
-                if "server closed the connection unexpectedly" not in str(ex):
-                    raise
-                if attempts <= self.__max_retry_count__:
-                    sleep_for = 2 ** (attempts - 1)
-             #       logging.error(
-             #           "/!\ Database connection error: retrying Strategy => sleeping for {}s"
-             #       " and will retry (attempt #{} of {}) \n Detailed query impacted: {}".format(
-             #           sleep_for, attempts, self.__max_retry_count__, ex)
-             #   )
-                    sleep(sleep_for)
-                    continue
-                else:
-                    raise
-            except StatementError as ex:
-                if "reconnect until invalid transaction is rolled back" not in str(ex):
-                    raise
-                self.session.rollback()
+    @retry
+    def all(self):
+        return super().all()
 
-            except:
+    @retry
+    def count(self):
+        return super().count()
 
-                self.session.rollback()
+    @retry
+    def first(self):
+        return super().firszt()
+
+    @retry
+    def distinct(self):
+        return super().distict()
+
+
 
 
 db_session = scoped_session(sessionmaker(class_=RoutingSession, query_cls=RetryingQuery))
