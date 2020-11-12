ae4b33f633971985faefd7ba92c320d791f9451e
captainmeta4 <captainmeta4@gmail.com>
2020-11-12 13:52:45 -0500

    Update embed.py

--- ruqqus/helpers/embed.py
+++ ruqqus/helpers/embed.py
@@ -12,7 +12,7 @@ ruqqus_regex = re.compile("^.*ruqqus.com/post/+\w+/(\w+)(/\w+/(\w+))?")
 twitter_regex=re.compile("/status/(\d+)")
 
 FACEBOOK_TOKEN=environ.get("FACEBOOK_TOKEN","").lstrip().rstrip()
-print(FACEBOOK_TOKEN)
+
 
 
 def youtube_embed(url):
