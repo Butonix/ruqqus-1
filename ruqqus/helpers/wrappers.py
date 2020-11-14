ea1b23d9690c0d9f4ff4b2ab2c94386705fa66f5
captainmeta4 <captainmeta4@gmail.com>
2020-11-14 13:50:26 -0500

    Update wrappers.py

--- ruqqus/helpers/wrappers.py
+++ ruqqus/helpers/wrappers.py
@@ -40,8 +40,8 @@ def get_logged_in_user():
         if not uid:
             return None, None
         v = g.db.query(User).options(
-            joinedload(User.moderates).joinedload(ModRelationship.board), #joinedload(Board.reports),
-            joinedload(User.subscriptions),
+            #joinedload(User.moderates).joinedload(ModRelationship.board), #joinedload(Board.reports),
+            #joinedload(User.subscriptions),
             joinedload(User.notifications)
             ).filter_by(id=uid).first()
         if v and nonce < v.login_nonce:
