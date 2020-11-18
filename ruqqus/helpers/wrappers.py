5623e8c01d6a8018ecb4ed86f34b75ee9c08f72c
Adam LaDine <captainmeta4@users.noreply.github.com>
2020-11-18 07:58:56 -0500

    Update wrappers.py

--- ruqqus/helpers/wrappers.py
+++ ruqqus/helpers/wrappers.py
@@ -309,7 +309,7 @@ def no_cors(f):
 
         origin = request.headers.get("Origin", None)
 
-        if origin and origin != "https://" + app.config["SERVER_NAME"] and app.config["SERVER_NAME"] != "localhost":
+        if origin and origin != "https://" + app.config["SERVER_NAME"] and (app.config["SERVER_NAME"] != "localhost" or app.config["SERVER_NAME"].startswith('localhost:')):
 
             return "This page may not be embedded in other webpages.", 403
 
