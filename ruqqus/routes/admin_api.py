2394238dce0ff4d973e51ac2086da6811bd549ef
captainmeta4 <captainmeta4@gmail.com>
2020-11-21 17:17:01 -0500

    Update admin_api.py

--- ruqqus/routes/admin_api.py
+++ ruqqus/routes/admin_api.py
@@ -72,8 +72,7 @@ def unban_user(user_id, v):
     if not user:
         abort(400)
 
-    alts = request.form.get("alts", False)
-    user.unban(include_alts=alts)
+    user.unban()
 
     send_notification(user,
                       "Your Ruqqus account has been reinstated. Please carefully review and abide by the [terms of service](/help/terms) and [content policy](/help/rules) to ensure that you don't get suspended again.")
