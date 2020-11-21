940dd31dcf77ae31caa29f6ebf259ed4cde1e676
captainmeta4 <captainmeta4@gmail.com>
2020-11-21 16:05:53 -0500

    Update discord.py

--- ruqqus/routes/discord.py
+++ ruqqus/routes/discord.py
@@ -88,6 +88,7 @@ def discord_redirect(v):
 
     #add user to discord
     url=f"https://discord.com/api/guilds/{SERVER_ID}/members/{x['id']}"
+    print(url)
     headers={
         'Authorization': f"Bot {BOT_TOKEN}",
         'Content-Type': "application/json"
