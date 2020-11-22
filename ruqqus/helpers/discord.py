74195820e7217ac50ba8bde211034fc569b3624b
captainmeta4 <captainmeta4@gmail.com>
2020-11-22 10:13:23 -0500

    Update discord.py

--- ruqqus/helpers/discord.py
+++ ruqqus/helpers/discord.py
@@ -37,6 +37,7 @@ def add_role(user, role_name):
     role_id = ROLES[role_name]
     url = f"{DISCORD_ENDPOINT}/guilds/{SERVER_ID}/members/{user.discord_id}/roles/{role_id}"
     headers = {"Authorization": f"Bot {BOT_TOKEN}"}
+    print(url)
     x=requests.put(url)
     print(x.status_code)
     return True
@@ -46,6 +47,7 @@ def delete_role(user, role_name):
     role_id = ROLES[role_name]
     url = f"{DISCORD_ENDPOINT}/guilds/{SERVER_ID}/members/{user.discord_id}/roles/{role_id}"
     headers = {"Authorization": f"Bot {BOT_TOKEN}"}
+    print(url)
     x=requests.delete(url)
     print(x.status_code)
     return True
