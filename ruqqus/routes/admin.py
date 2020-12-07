1eaa68e993e25b58d74523e29ebafc2e3d6166d8
captainmeta4 <captainmeta4@gmail.com>
2020-12-07 11:54:11 -0500

    Update admin.py

--- ruqqus/routes/admin.py
+++ ruqqus/routes/admin.py
@@ -567,7 +567,6 @@ def admin_ban_analysis(v):
 
                 else:
                     print(f"Repeat - @{user.username}")
-                    seen_so_far.add(user.id)
                     continue
 
                 alts=user.alts
