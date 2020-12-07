ad9d2511464533102b4f22cd5c29f6726f326a16
captainmeta4 <captainmeta4@gmail.com>
2020-12-07 16:23:29 -0500

    try

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ cd ~/
 sudo cp ruqqus/nginx.txt /etc/nginx/sites-available/ruqqus.com.conf
 sudo nginx -s reload
 . ~/venv/bin/activate
-. ~/env.sh
+bash ~/env.sh
 cd ~/ruqqus
 pip3 install -r requirements.txt
 export PYTHONPATH=$PYTHONPATH:~/ruqqus
