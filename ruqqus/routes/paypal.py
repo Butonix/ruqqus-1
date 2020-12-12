916d14bf9114dc63a0b4bf3fe3deeab636012d4f
captainmeta4 <captainmeta4@gmail.com>
2020-12-12 16:29:13 -0500

    landing

--- ruqqus/routes/paypal.py
+++ ruqqus/routes/paypal.py
@@ -146,7 +146,12 @@ def shop_buy_coins_completed(v):
 
     g.db.add(v)
 
-    return redirect("/settings/premium?msg=success")
+    return render_template(
+        "single_txn.html", 
+        v=v, 
+        txns=[txn],
+        msg="The Royal Bank has minted your Coins. Here is a copy of your order."
+        )
 
 @app.route("/shop/paypal_webhook", methods=["POST"])
 def paypal_webhook_handler():
