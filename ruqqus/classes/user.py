b2987997bd62e68b422847febd5ce7dbd5985bc6
captainmeta4 <captainmeta4@gmail.com>
2020-12-18 15:50:49 -0500

    Update user.py

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -675,7 +675,7 @@ class User(Base, Stndrd, Age_times):
     @property
     def json_core(self):
 
-    	now=int(time.time())
+        now=int(time.time())
         if self.is_banned and now < self.unban_utc:
             return {'username': self.username,
                     'permalink': self.permalink,
