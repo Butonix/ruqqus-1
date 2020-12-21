399156d16c095256b9d7c510c99ba0b0385f0335
captainmeta4 <captainmeta4@gmail.com>
2020-12-21 18:01:26 -0500

    subcats

--- ruqqus/classes/boards.py
+++ ruqqus/classes/boards.py
@@ -32,24 +32,6 @@ from ruqqus.__main__ import Base, cache
 #     Sports="Sports"
 #     Technology="Technology"
 
-CATEGORIES=[
-    "Arts",
-    "Culture",
-    "Discussion",
-    "Food",
-    "Entertainment",
-    "Gaming",
-    "Hobby",
-    "Humor",
-    "News",
-    "Offensive",
-    "Photography",
-    "Politics",
-    "Science",
-    "Sports",
-    "Technology"
-    ]
-
 
 class Board(Base, Stndrd, Age_times):
 
@@ -534,3 +516,147 @@ class Board(Base, Stndrd, Age_times):
     def user_guild_rep(self, user):
 
         return user.guild_rep(self)
+
+
+CATEGORIES=[
+        { id: 0,
+          name: 'all guilds',
+          subCats: [],
+          icon: 'fa-globe',
+          color: null,
+          visible: false
+        },
+        { id: 1,
+          name: 'arts',
+          subCats: [{name:'animation'}, {name:'production'}, {name:'photography'}, {name:'crafts'}],
+          icon: 'fa-palette',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 2,
+          name: 'business',
+          subCats: [{name:'finance'}, {name:'cryptocurrency'}, {name:'entrepreneur'}],
+          icon: 'fa-chart-line',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 3,
+          name: 'culture',
+          subCats: [{name:'history'}, {name:'language'}],
+          icon: 'fa-users',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 4,
+          name: 'discussion',
+          subCats: [{name:'casual'}, {name:'serious'}, {name:'Q&A'}],
+          icon: 'fa-podium',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 5,
+          name: 'entertainment',
+          subCats: [{name:'celebrities'}, {name:'industry news'}, {name:'film & TV'}],
+          icon: 'fa-theater-masks',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 6,
+          name: 'gaming',
+          subCats: [{name:'PC'}, {name:'console'}, {name:'industry news'}, {name:'development'}],
+          icon: 'fa-alien-monster',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 7,
+          name: 'hobby',
+          subCats: [{name:'crafts'}, {name:'outdoors'}, {name:'DIY'}, {name:'niche'}],
+          icon: 'fa-wrench',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 8,
+          name: 'health',
+          subCats: [{name:'medical'}, {name:'fitness'}, {name:'mental health'}],
+          icon: 'fa-heart',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 5,
+          name: 'entertainment',
+          subCats: [{name:'celebrities'}, {name:'industry news'}, {name:'film & TV'}],
+          icon: 'fa-theater-masks',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 6,
+          name: 'gaming',
+          subCats: [{name:'PC'}, {name:'console'}, {name:'industry news'}, {name:'development'}],
+          icon: 'fa-alien-monster',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 7,
+          name: 'hobby',
+          subCats: [{name:'crafts'}, {name:'outdoors'}, {name:'DIY'}, {name:'niche'}],
+          icon: 'fa-wrench',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 8,
+          name: 'health',
+          subCats: [{name:'medical'}, {name:'fitness'}, {name:'mental health'}],
+          icon: 'fa-heart',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 9,
+          name: 'lifestyle',
+          subCats: [{name:'fashion'}, {name:'beauty'}, {name:'food'}, {name:'relationships'}],
+          icon: 'fa-tshirt',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 10,
+          name: 'memes',
+          subCats: [{name:'casual'}, {name:'dank'}, {name:'political'}],
+          icon: 'fa-grin',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 11,
+          name: 'news',
+          subCats: [{name:'local'}, {name:'north america'}, {name:'world'}, {name:'upbeat'}],
+          icon: 'fa-newspaper',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 12,
+          name: 'politics',
+          subCats: [{name:'left'}, {name:'right'}, {name:'authoritarian'}, {name:'libertarian'}, {name:'activism'}, {name:'offbeat'}, {name:'news'}],
+          icon: 'fa-university',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 13,
+          name: 'science',
+          subCats: [{name:'biology'}, {name:'physics'}, {name:'AI'}, {name:'space'}, {name:'news'}],
+          icon: 'fa-flask',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 14,
+          name: 'sports',
+          subCats: [{name:'baseball'}, {name:'basketball'}, {name:'American football'}, {name:'soccer'}, {name:'tennis'}, {name:'hockey'}, {name:'combat'}, {name:'news'}],
+          icon: 'fa-baseball-ball',
+          color: 'purple-400',
+          visible: false
+        },
+        { id: 15,
+          name: 'technology',
+          subCats: [{name:'gadgets'}, {name:'programming'}, {name:'hardware'}, {name:'software'}, {name:'design'}, {name:'industry news'}],
+          icon: 'fa-microchip',
+          color: 'purple-400',
+          visible: false
+        }
+    ]
