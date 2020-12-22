352667c42ff24ef4037ca17ab30550be73245b91
captainmeta4 <captainmeta4@gmail.com>
2020-12-21 22:28:00 -0500

    Update boards.py

--- ruqqus/classes/boards.py
+++ ruqqus/classes/boards.py
@@ -61,7 +61,7 @@ class Board(Base, Stndrd, Age_times):
     stored_subscriber_count=Column(Integer, default=1)
     all_opt_out=Column(Boolean, default=False)
     subcat=Column(String(32), default=None)
-    is_siegable
+    is_siegable=Column(Boolean, default=True)
 
 
     moderators=relationship("ModRelationship")
