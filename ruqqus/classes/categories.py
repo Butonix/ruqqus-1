002009a1d213f6323f07512a34947814f971c0f1
Adam LaDine <captainmeta4@users.noreply.github.com>
2020-12-27 15:30:30 -0500

    Catsv2 (#610)
    * cat classes
    
    * subcat
    
    * Update categories.py
    
    * Update categories.py
    
    * add guildCat rel table
    
    * Update categories.py
    
    * Update categories.py
    
    * cats_update
    
    * Update boards.py
    
    * Update boards.py
    
    * Update front.py
    
    * subcat
    
    * Update boards.py
    
    * Update boards.py
    
    * Update categories.py
    
    * Update front.py
    
    * Update front.py
    
    * Update home.html
    
    * Update home.html
    
    * Update home.html
    
    * Update front.py
    
    * Update boards.py
    
    * Update home.html
    
    * Update categories.py
    
    * Update categories.py
    
    * Update categories.py
    
    * Update categories.py
    
    * Update front.py
    
    * Update boards.py
    
    * Update boards.py
    
    * cat selector
    
    * Update front.py
    
    * Update settings.html
    
    * name
    
    * Update boards.py
    
    * Update boards.py
    
    * Update board.html
    
    * Update home.html
    
    * Update home.html
    
    * Update home.html
    
    * Update home.html
    
    * Update board.html
    
    * Update board.html
    
    * cachebust and email
    
    * terms
    
    * remove tiems
    
    * Update categories.py
    
    Co-authored-by: arkit3kt <exodust@protonmail.com>

--- ruqqus/classes/categories.py
+++ ruqqus/classes/categories.py
@@ -8,7 +8,7 @@ from .mix_ins import *
 import time
 
 
-class Category(Base, Stndrd, Age_times):
+class Category(Base, Stndrd):
     __tablename__ = "categories"
     id = Column(BigInteger, primary_key=True)
     name = Column(String(20), default="")
@@ -16,15 +16,7 @@ class Category(Base, Stndrd, Age_times):
     icon = Column(String(256), default="")
     color = Column(String(128), default="805ad5")
     visible = Column(Boolean, default=True)
-    created_utc = Column(Integer, default=0)
-
     _subcats = relationship("SubCategory", lazy="joined", primaryjoin="SubCategory.cat_id==Category.id")
-
-    def __init__(self, *args, **kwargs):
-        if "created_utc" not in kwargs:
-            kwargs["created_utc"] = int(time.time())
-
-        super().__init__(*args, **kwargs)
 
     @property
     @lazy
@@ -47,22 +39,15 @@ class Category(Base, Stndrd, Age_times):
         }
 
 
-class SubCategory(Base, Stndrd, Age_times):
+class SubCategory(Base, Stndrd):
     __tablename__ = "subcategories"
     id = Column(BigInteger, primary_key=True)
     cat_id = Column(Integer, ForeignKey("categories.id"))
     name = Column(String(20), default="")
     description = Column(String(250), default="")
-    created_utc = Column(Integer, default=0)
     _visible = Column(Boolean, default=0)
 
     category = relationship("Category", lazy="joined")
-
-    def __init__(self, *args, **kwargs):
-        if "created_utc" not in kwargs:
-            kwargs["created_utc"] = int(time.time())
-
-        super().__init__(*args, **kwargs)
 
     @property
     def visible(self):
