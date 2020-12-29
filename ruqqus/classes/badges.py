6e726c88cd20f520ae818b32ac6d3351337eb357
captainmeta4 <captainmeta4@gmail.com>
2020-12-29 00:09:53 -0500

    Update badges.py

--- ruqqus/classes/badges.py
+++ ruqqus/classes/badges.py
@@ -83,7 +83,8 @@ class Badge(Base):
         return {'text': self.text,
                 'name': self.name,
                 'created_utc': self.created_utc,
-                'url': self.url
+                'url': self.url,
+                'icon_url':f"https://{app.config['SERVER_NAME']}{self.path}"
                 }
 
     property
