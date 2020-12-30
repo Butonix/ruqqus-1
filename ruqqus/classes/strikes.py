c29a1665ec87a9bd8579aee632165d5da13fdb12
arkit3kt <exodust@protonmail.com>
2020-12-30 01:30:49 -0500

    add rels

--- ruqqus/classes/strikes.py
+++ ruqqus/classes/strikes.py
@@ -11,10 +11,12 @@ from ruqqus.__main__ import Base, cache
 class Strike(Base, Stndrd, Age_times):
 
     __tablename__ = "strikes"
-    id = Column(BigInteger, primary_key=True)
+    id = Column(BigInteger, ForeignKey("punishments.i") primary_key=True)
     name = Column(String(256))
     description = Column(String)
     created_utc = Column(BigInteger, default=0)
+
+    punishments = relationship("Punishment", primaryjoin="Strike.id==Punishment.strike_id")
 
     def __init__(self, *args, **kwargs):
         if "created_utc" not in kwargs:
@@ -47,14 +49,15 @@ class EarnedStrike(Base, Stndrd, Age_times):
 
     __tablename__ = "earnedstrikes"
     id = Column(BigInteger, primary_key=True)
+    strike_id = Column(Integer, ForeignKey("strikes.id"))
     user_id = Column(Integer, ForeignKey("users.id"))
     post_id = Column(Integer, ForeignKey("submissions.id"))
     comment_id = Column(Integer, ForeignKey("submissions.id"))
     board_id = Column(Integer, ForeignKey("boards.id"))
-    punishments = Column(String)
     target_user_id = Column(Integer, ForeignKey("users.id"))
     created_utc = Column(BigInteger, default=0)
 
+    strike = relationship("Strike", primaryjoin="EarnedStrike.strike_id==Strike.id")
     def __init__(self, *args, **kwargs):
         if "created_utc" not in kwargs:
             kwargs["created_utc"] = int(time.time())
@@ -62,3 +65,6 @@ class EarnedStrike(Base, Stndrd, Age_times):
 
     def __repr__(self):
         return f"<EarnedStrike(id={self.id}, board_id={self.board_id})>"
+
+    def evalPunishment(self):
+        return self.strike.punishments.punishment
