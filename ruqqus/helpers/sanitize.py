5156c506fcff59cce3f1a0d556183d99979a9034
captainmeta4 <captainmeta4@gmail.com>
2021-01-04 10:23:15 -0500

    Update sanitize.py

--- ruqqus/helpers/sanitize.py
+++ ruqqus/helpers/sanitize.py
@@ -168,7 +168,7 @@ def sanitize(text, bio=False, linkgen=False):
         #disguised link preventer
         for tag in soup.find_all("a"):
 
-            tag.contents=[x.string for x in tag.contents]
+            tag.contents=[x.string for x in tag.contents if x.string]
 
             display=''.join(tag.contents)
             display=re.sub("\s",'', display)
