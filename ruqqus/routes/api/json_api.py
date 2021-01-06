53cbb33835abdf029504e87c889ab0bbd5349cba
arkit3kt <exodust@protonmail.com>
2021-01-05 21:59:11 -0500

    add endpoints for add/removal favorite post/comment

--- ruqqus/routes/api/json_api.py
+++ ruqqus/routes/api/json_api.py
@@ -25,3 +25,74 @@ def user_info(v, username):
 
     user = get_user(username, v=v)
     return jsonify(user.json)
+
+@app.route('/api/v1/addfavorite/post/<pid>', methods=['GET'])
+@auth_required
+@api('read')
+def addFavoritePost(pid, v):
+
+    post = get_post(pid)
+
+    if not post:
+        abort(404)
+
+    if not v.favorites.filter_by(post_id=post.id).first():
+        new_fav = Favorite(user_id=v.id,
+                           post_id=post.id)
+        g.db.add(new_fav)
+
+    else:
+        return jsonify({"msg": "post has already been favorited"})
+
+    return jsonify({"msg": "success"})
+
+
+@app.route('/api/v1/removefavorite/post/<pid>', methods=['GET'])
+@auth_required
+@api('read')
+def removeFavoritePost(pid, v):
+    post = get_post(pid)
+
+    if not post:
+        abort(404)
+
+    fav = v.favorites.filter_by(post_id=post.id).first()
+    if fav:
+        g.db.delete(fav)
+
+    return jsonify({"msg": "success"})
+
+
+@app.route('/api/v1/addfavorite/comment/<pid>', methods=['GET'])
+@auth_required
+@api('read')
+def addFavoriteComment(cid, v):
+    comment = get_comment(cid)
+
+    if not comment:
+        abort(404)
+
+    if not v.favorites.filter_by(comment_id=comment.id).first():
+        new_fav = Favorite(user_id=v.id,
+                           comment_id=comment.id)
+        g.db.add(new_fav)
+
+    else:
+        return jsonify({"msg": "post has already been favorited"})
+
+    return jsonify({"msg": "success"})
+
+@app.route('/api/v1/removefavorite/comment/<cid>', methods=['GET'])
+@auth_required
+@api('read')
+def removeFavoriteComment(cid, v):
+    comment = get_comment(cid)
+
+    if not comment:
+        abort(404)
+
+    fav = v.favorites.filter_by(comment_id=comment.id).first()
+    if fav:
+        g.db.delete(fav)
+
+    return jsonify({"msg": "success"})
