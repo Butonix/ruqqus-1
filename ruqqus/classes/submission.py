d00313b784af9137845c3576d018f8823046f68e
captainmeta4 <captainmeta4@gmail.com>
2021-01-15 15:27:28 -0500

    Update submission.py

--- ruqqus/classes/submission.py
+++ ruqqus/classes/submission.py
@@ -479,7 +479,8 @@ class Submission(Base, Stndrd, Age_times, Scores, Fuzzing):
 
     def is_guildmaster(self, perm=None):
         mod=self.__dict__.get('_is_guildmaster', False)
-        print(mod)
+        if self.board_id=2:
+            print(mod)
         if not mod:
             output= False
         elif not perm:
