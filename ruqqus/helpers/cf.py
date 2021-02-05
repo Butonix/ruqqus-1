956bb6820c37afb7ac10002ec08c616c255f02d4
captainmeta4 <captainmeta4@gmail.com>
2021-02-05 08:31:28 -0500

    fix

--- ruqqus/helpers/cf.py
+++ ruqqus/helpers/cf.py
@@ -24,13 +24,8 @@ url=f"https://api.cloudflare.com/client/v4/zones/{CF_ZONE}/settings/security_lev
 r=redis.Redis(host=app.config["CACHE_REDIS_URL"][8:], decode_responses=True, ssl_cert_reqs=None)
 
 config={}
-config['UNDER_ATTACK']=int(r.get("under_attack")) or 0
-config['TIMEOUT_STAMP']=int(r.get("timeout_stamp") or 0)
 
-print(f"Under attack: {config['UNDER_ATTACK']}")
-print(f"TS: {config['TIMEOUT_STAMP']}")
-
-config['COUNTER']=0
+config['COUNTER']=-1
 
 def site_performance(t):
 
@@ -40,6 +35,11 @@ def site_performance(t):
     if not config['COUNTER']%100:
         UNDER_ATTACK=int(r.get("under_attack")) or 0
         TIMEOUT_STAMP=int(r.get("timeout_stamp")) or 0
+        try:
+            print(f"Under attack: {config['UNDER_ATTACK']}")
+            print(f"TS: {config['TIMEOUT_STAMP']}")
+        except:
+            pass
 
     recent_reqs.append(t)
 
