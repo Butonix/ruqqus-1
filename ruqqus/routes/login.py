45c5d6d159d1c9f140f14dfda6e7f400947472a9
captainmeta4 <captainmeta4@gmail.com>
2021-02-07 22:26:32 -0500

    2fa recovery

--- ruqqus/routes/login.py
+++ ruqqus/routes/login.py
@@ -514,7 +514,10 @@ def get_reset():
 
 
 @app.route("/reset", methods=["POST"])
-def post_reset():
+@auth_desired
+def post_reset(v):
+    if v:
+        return redirect('/')
 
     user_id = request.form.get("user_id")
     timestamp = int(request.form.get("time"))
@@ -551,3 +554,101 @@ def post_reset():
     return render_template("message_success.html",
                            title="Password reset successful!",
                            text="Login normally to access your account.")
+
+@app.route("/lost_2fa")
+@auth_desired
+def lost_2fa(v):
+    if v:
+        return redirect('/')
+
+    return render_template("lost_2fa.html")
+
+@app.route("/request_2fa_disable", methods=["POST"])
+@auth_desired
+def request_2fa_disable(v):
+
+    if v:
+        return redirect('/')
+
+    username=request.form.get("username")
+    user=get_user(username, graceful=True)
+    if not user or not user.email or not user.mfa_secret:
+        return render_template("message.html",
+                           title="Removal request received",
+                           text="If username, password, and email match, we will send you an email.")
+
+
+    email=request.form.get("email")
+    if email and email.endswith("@gmail.com"):
+        gmail_username=email.split('@')[0]
+        gmail_username=gmail_username.split('+')[0]
+        gmail_username=gmail_username.replace('.','')
+        email=f"{gmail_username}@gmail.com"
+
+    if email != user.email:
+        return render_template("message.html",
+                           title="Removal request received",
+                           text="If username, password, and email match, we will send you an email.")
+
+
+    password =request.form.get("password")
+    if not user.verifyPass(password):
+        return render_template("message.html",
+                           title="Removal request received",
+                           text="If username, password, and email match, we will send you an email.")
+
+    #compute token
+    valid=int(time.time())+60*60*24
+    token=generate_hash(f"{user.id}+{user.username}+disable2fa+{valid}+{user.mfa_secret}")
+
+    action_url=f"https://{app.config["SERVER_NAME"]}/reset_2fa?id={user.base36id}&t={valid}&token={token}"
+    
+    send_mail(to_address=user.email,
+              subject="Ruqqus - 2FA Removal Request",
+              html=render_template("email/2fa_remove.html",
+                                   action_url=url,
+                                   v=user)
+              )
+
+    return render_template("message.html",
+                           title="Removal request received",
+                           text="If username, password, and email match, we will send you an email.")
+
+@app.route("/reset_2fa", methods=["GET"])
+@auth_desired
+def reset_2fa(v):
+
+    if v:
+        return redirect('/')
+
+
+    now=int(time.time())
+    t=int(request.args.get("t"))
+
+    if now<t:
+        return render_template("message.html",
+                           title="Inactive Link",
+                           text="That link isn't active yet. Try again later.")
+    elif now > t+3600*24:
+        return render_template("message.html",
+                           title="Expired Link",
+                           text="That link has expired.")
+
+    token=request.args.get("token")
+    uid=request.args.get("id")
+
+    user=get_account(uid)
+
+    if not validate_hash(f"{user.id}+{user.username}+disable2fa+{t}+{user.mfa_secret}", token):
+        abort(403)
+
+    #validation successful, remove 2fa
+    user.mfa_secret=None
+
+    g.db.add(user)
+    g.db.commit()
+
+    return render_template("message_success.html",
+                           title="Two-factor authentication removed.",
+                           text="Login normally to access your account.")
+
