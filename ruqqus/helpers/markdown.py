33c1626a7f5f09a15e34259b2e242b91d781586f
captainmeta4 <captainmeta4@gmail.com>
2021-03-23 19:29:15 -0400

    Update markdown.py

--- ruqqus/helpers/markdown.py
+++ ruqqus/helpers/markdown.py
@@ -70,7 +70,7 @@ class CustomRenderer(HTMLRenderer):
         if (not user or (user.is_banned and not user.unban_utc) or user.is_deleted):
             return f"{space}@{target}"
 
-        return f'{space}<a href="{user.permalink}" class="d-inline-block mention-user" data-original-name="{user.original_name}"><img src="/uid/{user.base36id}/pic/profile" class="profile-pic-20 mr-1">@{user.username}</a>'
+        return f'{space}<a href="{user.permalink}" class="d-inline-block mention-user" data-original-name="{user.original_username}"><img src="/uid/{user.base36id}/pic/profile" class="profile-pic-20 mr-1">@{user.username}</a>'
 
     def render_board_mention(self, token):
         space = token.target[0]
