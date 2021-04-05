0a155db1ba2d3944413a27cc614e513fea587591
captainmeta4 <captainmeta4@gmail.com>
2021-04-05 17:38:13 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -11,8 +11,7 @@ export S3_BUCKET_NAME=i.ruqqus.com
 export CACHE_TYPE="redis"
 export HCAPTCHA_SITEKEY="22beca86-6e93-421c-8510-f07c6914dadb"
 cd ~/
-echo "starting chat worker"
-gunicorn ruqqus.__main__:app -k eventlet -D -w 1 --worker-connections 50 --preload --bind 127.0.0.1:5001
 echo "starting regular workers"
-NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w 2 --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000
-
+NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn ruqqus.__main__:app -D -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000
+echo "starting chat worker"
+gunicorn ruqqus.__main__:app -k eventlet  -w 1 --worker-connections 1000 --max-requests 100000 --preload --bind 127.0.0.1:5001
