5291d5dfa712fb268535404d0f2ddb5688d57ff6
captainmeta4 <captainmeta4@gmail.com>
2021-04-05 11:49:32 -0400

    Update chat.py

--- ruqqus/chat/chat.py
+++ ruqqus/chat/chat.py
@@ -16,24 +16,45 @@ REDIS_URL = app.config["CACHE_REDIS_URL"]
 
 redis = redis.from_url(REDIS_URL)
 
+
+def socket_auth_required(f):
+
+    def wrapper(*args, **kwargs)
+
+        v, client=get_logged_in_user()
+
+        if client or not v:
+            send("Not logged in")
+            return
+
+        f(*args, v, **kwargs)
+
+    wrapper.__name__=f.__name__
+    return wrapper
+
+
 @socketio.on('connect')
-def socket_connect_auth_user():
+@socket_auth_required
+def socket_connect_auth_user(v):
 
-    v, client=get_logged_in_user()
-
-    if client or not v:
-        send("Not logged in")
+    if not v:
         disconnect()
 
+@socketio.on('join room')
+@socket_auth_required
+def join_room(data):
 
+    guild=get_guild(data["guild"])
+    if guild.has_ban(v):
+        emit("error", {"error":f"You are banned from +{guild.name}"})
+        return
 
+    join_room(guild.fullname)
 
-@socketio.on('my event')
-#@auth_desired
-def socket_test(json, v=None):
-
-    print(f"received json {str(json)}")
-    emit('my response',{'test':'foobar'})
+@socketio.on('leave room')
+@socket_auth_required
+def leave_room(data, v):
+    leave_room(data["guild"]):
 
 
 @app.route("/socket_home")
@@ -42,109 +63,3 @@ def socket_home(v=None):
 
     return render_template("chat/chat_test.html", v=v)
 
-
-# class ChatBackend(object):
-#     """Interface for registering and updating WebSocket clients."""
-
-#     def __init__(self, guildname):
-#         self.clients = list()
-#         self.pubsub = redis.pubsub()
-#         self.pubsub.subscribe(guildname)
-
-#     def __iter_data(self):
-#         for message in self.pubsub.listen():
-#             data = message.get('data')
-#             if message['type'] == 'message':
-#                 app.logger.info(u'Sending message: {}'.format(data))
-#                 yield data
-
-#     def register(self, client):
-#         """Register a WebSocket connection for Redis updates."""
-#         self.clients.append(client)
-
-#     def send(self, client, data):
-#         """Send given data to the registered client.
-#         Automatically discards invalid connections."""
-#         try:
-#             client.send(data)
-#         except Exception:
-#             self.clients.remove(client)
-
-#     def run(self):
-#         """Listens for new messages in Redis, and sends them to clients."""
-#         for data in self.__iter_data():
-#             for client in self.clients:
-#                 gevent.spawn(self.send, client, data)
-#             if not self.clients:
-#                 gevent.sleep(10)
-#                 if not self.clients:
-#                     break
-
-#     def start(self):
-#         """Maintains Redis subscription in the background."""
-#         gevent.spawn(self.run)
-
-# CHATS = {}
-
-
-# @sockets.route('/chat/<guildname>/inbox')
-# #@is_not_banned
-# def inbox(ws, guildname):
-
-#     print('submit websocket')
-#     """Receives incoming chat messages, inserts them into Redis."""
-
-#     #guild=get_guild(guildname, graceful=True)
-#     guild=get_guild(guildname, graceful=True)
-#     #if not guild or guild.is_banned:
-#     #    ws.close()
-#     #    return
-
-#     if guild.name not in CHATS:
-#         CHATS[guild.name]=ChatBackend(guild.name)
-#         CHATS[guild.name].start()
-
-#     while not ws.closed:
-#         # Sleep to prevent *constant* context-switches.
-#         gevent.sleep(0.1)
-#         message = ws.receive()
-
-#         if message:
-#             #app.logger.info(f'Inserting message: {message}')
-#             redis.publish(guild.name, message)
-
-# @sockets.route('/chat/<guildname>/outbox')
-# #@is_not_banned
-# def outbox(ws, guildname):
-#     #print(args)
-
-#     #guild=get_guild(guildname, graceful=True)
-    
-#     guild=get_guild(guildname, graceful=True)
-#     #if not guild or guild.is_banned:
-#     #    ws.close()
-#     #    return
-
-#     if guild.name not in CHATS:
-#         CHATS[guild.name]=ChatBackend(guild.name)
-#         CHATS[guild.name].start()
-
-#     """Sends outgoing chat messages, via `ChatBackend`."""
-#     CHATS[guild.name].register(ws)
-
-#     while not ws.closed:
-#         # Context switch while `ChatBackend.start` is running in the background.
-#         gevent.sleep(0.1)
-
-
-# @app.route("/+<guildname>/chat", methods=["GET"])
-# @is_not_banned
-# def guild_chat_get(guildname, v):
-
-#     b=get_guild(guildname)
-
-#     return render_template(
-#         "chat/chat.html",
-#         v=v,
-#         b=b
-#         )
