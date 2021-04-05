ea3b4016c266fcb779ccda86dcb44916383cd54e
captainmeta4 <captainmeta4@gmail.com>
2021-04-04 22:35:19 -0400

    Update boards.py

--- ruqqus/routes/boards.py
+++ ruqqus/routes/boards.py
@@ -1322,7 +1322,7 @@ def mod_board_images_profile(bid, board, v):
 def mod_board_images_banner(bid, board, v):
 
     if request.headers.get("cf-ipcountry")=="T1" and not v.is_activated:
-        return jsonfiy({"error":"You must have a verified email address to upload images via Tor."}), 401
+        return jsonify({"error":"You must have a verified email address to upload images via Tor."}), 401
 
     board.set_banner(request.files["banner"])
 
