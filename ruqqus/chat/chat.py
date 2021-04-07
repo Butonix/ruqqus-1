98a49f290075434167a8b6b246c837743541aaa8
captainmeta4 <captainmeta4@gmail.com>
2021-04-07 11:37:12 -0400

    Update chat.py

--- ruqqus/chat/chat.py
+++ ruqqus/chat/chat.py
@@ -275,9 +275,9 @@ def speak_guild(data, v, guild):
 @command('random')
 def random_post(args, guild, v):
 
-    total=g.db.query(Submission).options(lazyload('*')).filter_by(board_id=guild.id, is_deleted=False, is_banned=False).count()
-    offset=random.randint(0, total)
-    post=g.db.query(Submission).options(lazyload('*')).filter_by(board_id=guild.id, is_deleted=False, is_banned=False).order_by(Submission.id.asc()).offset(offset).first()
+    total=g.db.query(Submission).options(lazyload('*')).filter_by(board_id=guild.id, deleted_utc=0, is_banned=False).count()
+    offset=random.randint(0, total-1)
+    post=g.db.query(Submission).options(lazyload('*')).filter_by(board_id=guild.id, deleted_utc=0, is_banned=False).order_by(Submission.id.asc()).offset(offset).first()
     speak(post.permalink, v, guild)
 
 
