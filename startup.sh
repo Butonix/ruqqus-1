505b8f129acf0502bacbfd091591fd9106118cb1
captainmeta4 <captainmeta4@gmail.com>
2021-04-07 21:46:50 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -11,8 +11,8 @@ export S3_BUCKET_NAME=i.ruqqus.com
 export CACHE_TYPE="redis"
 export HCAPTCHA_SITEKEY="22beca86-6e93-421c-8510-f07c6914dadb"
 cd ~/
-echo "starting regular workers"
-gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000 -D
 echo "starting chat worker"
-gunicorn ruqqus.__main__:app -k eventlet  -w 1 --worker-connections 1000 --max-requests 100000 --preload --bind 127.0.0.1:5001 
+gunicorn ruqqus.__main__:app -k eventlet  -w 1 --worker-connections 1000 --max-requests 100000 --preload --bind 127.0.0.1:5001 -D
+echo "starting regular workers"
+gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000
 
