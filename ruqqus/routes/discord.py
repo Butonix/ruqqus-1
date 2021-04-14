d483f7e8e4db7eb992815fb69121ad59d4bd64ce
captainmeta4 <captainmeta4@gmail.com>
2021-04-14 10:25:49 -0400

    Update discord.py

--- ruqqus/routes/discord.py
+++ ruqqus/routes/discord.py
@@ -13,7 +13,7 @@ from ruqqus.__main__ import app
 SERVER_ID = environ.get("DISCORD_SERVER_ID",'').rstrip()
 CLIENT_ID = environ.get("DISCORD_CLIENT_ID",'').rstrip()
 CLIENT_SECRET = environ.get("DISCORD_CLIENT_SECRET",'').rstrip()
-BOT_TOKEN = environ.get("DISCORD_BOT_TOKEN").rstrip()
+BOT_TOKEN = environ.get("DISCORD_BOT_TOKEN",'').rstrip()
 DISCORD_ENDPOINT = "https://discordapp.com/api/v6"
 
 
