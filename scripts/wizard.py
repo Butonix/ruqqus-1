9816cc74ded3f55aabb776f40aea340ad619f580
captainmeta4 <captainmeta4@gmail.com>
2021-04-14 10:33:19 -0400

    Update wizard.py

--- scripts/wizard.py
+++ scripts/wizard.py
@@ -340,8 +340,23 @@ killall gunicorn
 killall python
 cd {path}/ruqqus
 git pull
-source startup.sh
+cd {path}
+sudo cp {path}/ruqqus/nginx.txt /etc/nginx/sites-available/ruqqus.com.conf
+sudo nginx -s reload
+. {path}/venv/bin/activate
+. {path}/env.sh
+cd {path}/ruqqus
+pip3 install -r requirements.txt
+export PYTHONPATH=$PYTHONPATH:~/ruqqus
+cd {path}
+echo "starting regular workers"
+newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000 -D
+echo "starting chat worker"
+gunicorn ruqqus.__main__:app -k eventlet  -w 1 --worker-connections 1000 --max-requests 100000 --preload --bind 127.0.0.1:5001 -D
+echo "starting background worker"
+python {path}/ruqqus/scripts/recomputes.py
 """
+
 with open(f"{path}/go.sh", "w+") as f:
     f.write(start_script)
 
