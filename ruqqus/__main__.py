0350d0d62f98749422c8354c6cad6001b129d016
captainmeta4 <captainmeta4@gmail.com>
2021-04-16 10:10:21 -0400

    64

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -35,7 +35,7 @@ from redis import BlockingConnectionPool
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
-_version = "2.35.63"
+_version = "2.35.64"
 
 app = Flask(__name__,
             template_folder='./templates',
