7fe2074f179d0d79b5d0562e9a61baf036d1723c
captainmeta4 <captainmeta4@gmail.com>
2021-04-16 23:30:09 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -115,7 +115,7 @@ redispool=BlockingConnectionPool(
     max_connections=app.config["REDIS_POOL_SIZE"],
     host=app.config["CACHE_REDIS_URL"][8:]
     )
-app.config["CACHE_OPTIONS"]={'connection_pool':redispool}
+#app.config["CACHE_OPTIONS"]={'connection_pool':redispool}
 
 
 Markdown(app)
