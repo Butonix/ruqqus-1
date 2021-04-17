2f5f0b7213351367a101270917705af832041b54
captainmeta4 <captainmeta4@gmail.com>
2021-04-16 21:36:16 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -13,4 +13,4 @@ gunicorn ruqqus.__main__:app -k eventlet  -w 1 --worker-connections 1000 --max-r
 echo "starting background worker"
 python ruqqus/scripts/recomputes.py
 echo "starting regular workers"
-newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000
+NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000
