b0c4e6cdb4ca03928e302683eb2462d7884568e6
captainmeta4 <captainmeta4@gmail.com>
2021-04-17 09:08:23 -0400

    Update chat_routes.py

--- ruqqus/chat/chat_routes.py
+++ ruqqus/chat/chat_routes.py
@@ -1,3 +1,10 @@
+from ruqqus.helpers.wrappers import *
+from ruqqus.helpers.get import *
+from ruqqus.helpers.session import *
+from ruqqus.helpers.base36 import *
+from ruqqus.helpers.aws import *
+from ruqqus.__main__ import app
+
 ##############
 #            #
 #  PICTURES  #
