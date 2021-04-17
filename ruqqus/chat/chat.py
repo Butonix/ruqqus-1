6d870571ef2ae42a609dadfaca8cd23710de6bf5
captainmeta4 <captainmeta4@gmail.com>
2021-04-17 09:08:46 -0400

    Update chat.py

--- ruqqus/chat/chat.py
+++ ruqqus/chat/chat.py
@@ -363,50 +363,6 @@ def typing_indicator(data, v, guild):
 
 
 
-
-
-##############
-#            #
-#   ROUTES   #
-#            #
-##############
-
-
-
-@app.route("/socket_home")
-#@auth_required
-def socket_home(v=None):
-
-    return render_template("chat/chat_test.html", v=v)
-
-
-@app.route("/+<guildname>/chat", methods=["GET"])
-@is_not_banned
-def guild_chat(guildname, v):
-
-
-
-    board=get_guild(guildname)
-
-
-    if board.over_18 and not (v and v.over_18) and not session_over18(board):
-        t = int(time.time())
-        return render_template(
-            "errors/nsfw.html",
-            v=v,
-            t=t,
-            lo_formkey=make_logged_out_formkey(t),
-            board=board
-            )
-
-    return render_template(
-        "chat/chat.html", 
-        b=board, 
-        v=v,
-        is_subscribed=(v and board.has_subscriber(v))
-        )
-
-
 ##############
 #            #
 #  COMMANDS  #
@@ -855,30 +811,3 @@ def direct_message(args, guild, v):
     for sid in SIDS.get(v.id,[]):
         emit('msg-out', data, to=sid)
 
-##############
-#            #
-#  PICTURES  #
-#            #
-##############
-
-
-@app.route("/chat_upload", methods=["POST"])
-@is_not_banned
-def chat_upload_image(v):
-
-    if not v.has_premium:
-        abort(403)
-
-
-    file = request.files['image']
-    if not file.content_type.startswith('image/'):
-        abort(400)
-
-    now=int(time.time())
-    now=base36encode(now)
-    name = f'chat/{now}/{secrets.token_urlsafe(8)}'
-    upload_file(name, file)
-
-    check_csam_url(f"https://{BUCKET}/{name}", v, lambda:delete_file(name))
-
-    return jsonify({'url':f"https://{BUCKET}/{name}"})
