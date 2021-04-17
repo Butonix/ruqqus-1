9db2178589c3961854d49c218da861fed6480a97
captainmeta4 <captainmeta4@gmail.com>
2021-04-17 19:42:49 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -110,7 +110,7 @@ app.config["CACHE_KEY_PREFIX"] = "flask_caching_"
 
 app.config["S3_BUCKET"]=environ.get("S3_BUCKET_NAME","i.ruqqus.com").lstrip().rstrip()
 
-app.config["REDIS_POOL_SIZE"]=int(environ.get("REDIS_POOL_SIZE", 80))
+app.config["REDIS_POOL_SIZE"]=int(environ.get("REDIS_POOL_SIZE", 200))
 
 redispool=BlockingConnectionPool(
     max_connections=app.config["REDIS_POOL_SIZE"],
