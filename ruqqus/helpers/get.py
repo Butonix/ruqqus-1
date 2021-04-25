d8497c4fe695ca32d10786322f79f99ce024a1ea
captainmeta4 <captainmeta4@gmail.com>
2021-04-25 16:59:20 -0400

    Update get.py

--- ruqqus/helpers/get.py
+++ ruqqus/helpers/get.py
@@ -359,7 +359,7 @@ def get_post_with_comments(pid, sort_type="top", v=None):
         elif sort_type == "new":
             comments = comms.order_by(Comment.created_utc.desc()).all()
         elif sort_type == "old":
-            comments = comms.order_by(Comment.created_utc.sc.all()
+            comments = comms.order_by(Comment.created_utc.asc()).all()
         elif sort_type == "disputed":
             comments = comms.order_by(Comment.score_disputed.desc()).all()
         elif sort_type == "random":
