265b35be8e28383fd8a4b044cd227b84c5d4e351
captainmeta4 <captainmeta4@gmail.com>
2021-04-26 16:22:41 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,9 +8,10 @@ cd ~/ruqqus
 pip3 install -r requirements.txt
 export PYTHONPATH=$PYTHONPATH:~/ruqqus
 cd ~/
-echo "starting regular workers"
-NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000 -D
 echo "starting background worker"
-python ruqqus/scripts/recomputes.py 
+python ruqqus/scripts/recomputes.py -D
+echo "starting regular workers"
+NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn ruqqus.__main__:app -k gevent -w $WEB_CONCURRENCY --worker-connections $WORKER_CONNECTIONS --max-requests 10000 --max-requests-jitter 500 --preload --bind 127.0.0.1:5000
+
 #echo "starting chat worker"
 #gunicorn ruqqus.__main__:app load_chat -k eventlet  -w 1 --worker-connections 1000 --max-requests 100000 --preload --bind 127.0.0.1:5001
