26d22a5781cd22c539b79317e5de7ab882d62719
captainmeta4 <captainmeta4@gmail.com>
2021-04-27 08:03:17 -0400

    Update wrappers.py

--- ruqqus/helpers/wrappers.py
+++ ruqqus/helpers/wrappers.py
@@ -120,7 +120,7 @@ def auth_required(f):
         if not v:
             abort(401)
         elif v and v.ban_evade and not v.is_suspended:
-            if random.randint(0,100) < v.ban_evade:
+            if random.randint(0,10) < v.ban_evade:
                 v.ban(reason="Evading a site-wide ban")
                 send_notification(v, "Your Ruqqus account has been permanently suspended for the following reason:\n\n> ban evasion")
 
