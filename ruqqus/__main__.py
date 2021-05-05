e29e2c6e165fe15196dbe6b3e6be0e08406b52ab
Captain Metaphor <captainmeta4@users.noreply.github.com>
2021-05-05 16:46:24 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -44,7 +44,7 @@ class Flask_Timeout(Flask):
         kwargs['timeout_value']="unable to complete request"
         value= gevent.with_timeout(10, super().full_dispatch_request, *args, **kwargs)
         if value==kwargs['timeout_value']:
-            print("internal timeout", request.method, request.path, request.session.get('user_id'))
+            print("internal timeout", request.method, request.path, request.session)
             
         return value
 
