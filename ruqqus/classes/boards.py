8a1be7abf75cd98f79d3714c219714c60c612829
Captain Metaphor <captainmeta4@users.noreply.github.com>
2021-05-05 19:41:18 -0400

    Update boards.py

--- ruqqus/classes/boards.py
+++ ruqqus/classes/boards.py
@@ -320,9 +320,8 @@ class Board(Base, Stndrd, Age_times):
 
         return self.id in [
             x.board_id for x in user.subscriptions if x.is_active]
-
-	
-	@property
+    
+    @property
     def is_subscribed(self):
 		return self.__dict__.get("_is_subscribed", False)
 
