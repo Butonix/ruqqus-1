a32de3bc0c005afc0672b34a0d72729f7a56b202
Captain Metaphor <captainmeta4@users.noreply.github.com>
2021-05-06 14:48:47 -0400

    Update login.py

--- ruqqus/routes/login.py
+++ ruqqus/routes/login.py
@@ -124,10 +124,7 @@ def login_post():
             return redirect("/login")
         
         is_2fa=account.validate_2fa(request.form.get("2fa_token", "").strip())
-        is_recovery=validate_hash(
-                f"{account.mfa_secret}+{account.id}+P{account.original_username}",
-                request.form.get("2fa_token","").lower().replace(" ","")
-            )
+        is_recovery=safe_compare(request.form.get("2fa_token","").lower().replace(' ',''), v.mfa_removal_code)
         
         if not is_2fa and not is_recovery:
             
