6646498c6ac0bfc6e6e3474230e193772b98d245
Captain Metaphor <captainmeta4@users.noreply.github.com>
2021-05-07 17:55:02 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -69,7 +69,7 @@ class Flask_Timeout(Flask):
             return make_response("Your request took too long to process.", 500)
             
 
-app = Flask(__name__,
+app = Flask_Timeout(__name__,
             template_folder='./templates',
             static_folder='./static'
             )
