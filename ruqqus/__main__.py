af9aef25492af3efaefaf66f30f11e00434ad4ae
Captain Metaphor <captainmeta4@users.noreply.github.com>
2021-05-07 15:06:29 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -41,10 +41,14 @@ _version = "2.35.94"
 class Flask_Timeout(Flask):
             
     def full_dispatch_request(self, *args, **kwargs):
+            
+        def target(*args, **kwargs):
+            
+            return super().full_dispatch_request(*args, **kwargs)
         
         timeout=gevent.Timeout(15, gevent.Timeout)
         timeout.start()
-        req_thread=gevent.spawn(super().full_dispatch_request, *args, **kwargs)
+        req_thread=gevent.spawn(target, *args, **kwargs)
         try:
             req_thread.join()
             return req_thread.value
