c5879c1874dc1fe45bd17c02e47ceabadb248a4c
captainmeta4 <captainmeta4@gmail.com>
2021-05-08 11:14:39 -0400

    Revert "Update __main__.py"
    This reverts commit c6a32cbb0a7237faff530954c5dcca8003449861.

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -18,7 +18,6 @@ from flask_socketio import SocketIO
 from time import sleep
 from collections import deque
 import psycopg2
-import sqlalchemy.exc.OperationalError
 
 from flaskext.markdown import Markdown
 from sqlalchemy.ext.declarative import declarative_base
