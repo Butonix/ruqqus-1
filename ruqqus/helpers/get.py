45cd8f5b300fe2647439f1a66420956adec2cba1
captainmeta4 <captainmeta4@gmail.com>
2021-05-08 15:36:24 -0400

    Update get.py

--- ruqqus/helpers/get.py
+++ ruqqus/helpers/get.py
@@ -627,7 +627,7 @@ def get_comments(cids, v=None, nSession=None, sort_type="new",
             #comment._is_blocked = c[3] or 0
             comment._is_guildmaster=post._is_guildmaster
             
-            comment._is_exiled_for=c[3]
+            comment._is_exiled_for=c[2]
             #comment._is_exiled_for=c[4]
             output.append(comment)
 
