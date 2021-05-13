2565f2735d0009e792c11cb07df5797749b9805e
captainmeta4 <captainmeta4@gmail.com>
2021-05-12 21:58:06 -0400

    Update boards.py

--- ruqqus/routes/boards.py
+++ ruqqus/routes/boards.py
@@ -2155,7 +2155,6 @@ def siege_guild(v):
                     ),
                 #option 2: ruqqus adds user as mod due to siege
                 and_(
-                    ModAction.user_id==1,
                     ModAction.target_user_id.in_(tuple(ids)),
                     ModAction.kind=="add_mod",
                     ModAction.board_id==guild.id
