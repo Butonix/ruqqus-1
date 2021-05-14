c6cb6846a11e0e83b5cc09da8c7399b71f68838f
captainmeta4 <captainmeta4@gmail.com>
2021-05-14 18:05:34 -0400

    Update admin.py

--- ruqqus/routes/admin.py
+++ ruqqus/routes/admin.py
@@ -1160,6 +1160,7 @@ def admin_siege_guild(v):
                 #option 1: mod action by user
                 and_(
                     ModAction.user_id.in_(tuple(ids)),
+                    ModAction.created_utc > cutoff,
                     ModAction.board_id==guild.id
                     ),
                 #option 2: ruqqus adds user as mod due to siege
@@ -1168,8 +1169,8 @@ def admin_siege_guild(v):
                     ModAction.target_user_id.in_(tuple(ids)),
                     ModAction.kind=="add_mod",
                     ModAction.board_id==guild.id
-                    ),
-                ModAction.created_utc > cutoff
+                    ModAction.created_utc > cutoff,
+                    )
                 )
             ).first()
         if ma:
