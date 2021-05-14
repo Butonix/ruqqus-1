5be13552473b56fed5025ad86fb05002d9a2d5ca
Captain Metaphor <captainmeta4@users.noreply.github.com>
2021-05-14 12:30:50 -0400

    Update front.py

--- ruqqus/routes/front.py
+++ ruqqus/routes/front.py
@@ -183,7 +183,10 @@ def frontlist(v=None, sort=None, page=1, nsfw=False, nsfl=False,
         posts=posts.filter(Board.subcat_id.in_(tuple(categories)))
         
     if (v and v.hide_offensive) or not v:
-        posts=posts.filter(Board.subcat_id != 108)
+        posts=posts.filter(
+            Board.subcat_id != 108,
+            Board.id != 1
+            )
 
     posts=posts.options(contains_eager(Submission.board))
 
