c9ef3c8b06977233bc6c7ec832aab2bae2167766
Dan Habot <dan.habot@gmail.com>
2021-05-26 15:21:00 -0400

    imporved RSS compliance (#750)
    Co-authored-by: Captain Metaphor <captainmeta4@users.noreply.github.com>

--- ruqqus/routes/feeds.py
+++ ruqqus/routes/feeds.py
@@ -1,5 +1,6 @@
 from flask import *
 import hmac
+import html
 from .front import frontlist
 from werkzeug.contrib.atom import AtomFeed
 from datetime import datetime
@@ -7,6 +8,7 @@ from ruqqus.classes import *
 from ruqqus.helpers.security import *
 from ruqqus.helpers.jinja2 import full_link
 from ruqqus.helpers.get import *
+from yattag import Doc
 
 from ruqqus.__main__ import app, limiter
 
@@ -62,21 +64,58 @@ def feeds_user(sort=None, username=None, key=None):
     ids = user.idlist(sort=sort, page=page, t=t)
     posts = get_posts(ids, sort=sort, v=user)
 
-    feed = AtomFeed(title=f'Top 5 {sort} Posts from ruqqus',
-                    feed_url=request.url, url=request.url_root)
+    domain = environ.get(
+    "domain", environ.get(
+        "SERVER_NAME", None)).lstrip().rstrip()
 
-    for post in posts:
+    doc, tag, text = Doc().tagtext()
 
-        feed.add(post.title, post.body_html,
-                 content_type='html',
-                 author=post.author.username,
-                 url=full_link(post.permalink),
-                 updated=datetime.fromtimestamp(post.created_utc),
-                 published=datetime.fromtimestamp(post.created_utc),
-                 links=[{'href': post.url}]
-                 )
+    with tag("feed", ("xmlns:media","http://search.yahoo.com/mrss/"), xmlns="http://www.w3.org/2005/Atom",):
+        with tag("title", type="text"):
+            text(f"{sort} posts from {domain}")
 
-    return feed.get_response()
+        doc.stag("link", href=request.url)
+        doc.stag("link", href=request.url_root)
+
+        for post in posts:
+            print("POST IMAGE "+ str( post.is_image ))
+            board_name = f"+{post.board.name}"
+            with tag("entry", ("xml:base", request.url)):
+                with tag("title", type="text"):
+                    text(post.title)
+
+                with tag("id"):
+                    text(post.fullname)
+
+                if (post.edited_utc > 0):
+                    with tag("updated"):
+                        text(datetime.utcfromtimestamp(post.edited_utc).isoformat())
+
+                with tag("published"):
+                    text(datetime.utcfromtimestamp(post.created_utc).isoformat())
+                
+                doc.stag("link", href=post.url)
+
+                with tag("author"):
+                    with tag("name"):
+                        text(post.author.username)
+                    with tag("uri"):
+                        text(f'https://{domain}/@{post.author.username}')
+
+                doc.stag("link", href=full_link(post.permalink))
+
+                doc.stag("category", term=board_name, label=board_name, schema=full_link("/" + board_name))
+
+                image_url = post.thumb_url or post.embed_url or post.url
+                print("IS IMAGE")
+
+                doc.stag("media:thumbnail", url=image_url)
+
+                if len(post.body_html) > 0:
+                    with tag("content", type="html"):
+                        text(html.escape(f"<img src={image_url}/><br/>{post.body_html}"))
+
+    return Response( "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+ doc.getvalue(), mimetype="application/xml")
 
 
 @app.route('/feeds/+<guildname>/<sort>', methods=["GET"])
