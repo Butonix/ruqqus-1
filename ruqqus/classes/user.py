073677424c329e77897ef0c033e1f594174b9714
captainmeta4 <captainmeta4@gmail.com>
2021-05-27 21:01:32 -0400

    Update user.py

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -627,8 +627,8 @@ class User(Base, Stndrd, Age_times):
             notifications = notifications.filter(Notification.read == False)
 
         if replies_only:
-            cs=g.db.query(Comment.id).filter(Comment.author_id==self.id).as_scalar()
-            ps=g.db.query(Submission.id).filter(Submission.author_id==self.id).as_scalar()
+            cs=g.db.query(Comment.id).filter(Comment.author_id==self.id)
+            ps=g.db.query(Submission.id).filter(Submission.author_id==self.id)
             notifications=notifications.filter(
                 or_(
                     Comment.parent_comment_id.in_(cs),
@@ -641,8 +641,8 @@ class User(Base, Stndrd, Age_times):
 
 
         elif mentions_only:
-            cs=g.db.query(Comment.id).filter(Comment.author_id==self.id).as_scalar()
-            ps=g.db.query(Submission.id).filter(Submission.author_id==self.id).as_scalar()
+            cs=g.db.query(Comment.id).filter(Comment.author_id==self.id)
+            ps=g.db.query(Submission.id).filter(Submission.author_id==self.id)
             notifications=notifications.filter(
                 and_(
                     Comment.parent_comment_id.notin_(cs),
