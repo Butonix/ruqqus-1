5c08c906197d5a20053969fc3aec71135faafdd5
captainmeta4 <captainmeta4@gmail.com>
2021-05-29 17:50:58 -0400

    Update boards.py

--- ruqqus/classes/boards.py
+++ ruqqus/classes/boards.py
@@ -322,7 +322,7 @@ class Board(Base, Stndrd, Age_times):
     @lazy
     def is_subscribed(self):
 
-        print(self._is_subscribed)
+        #print(self._is_subscribed)
 
         return self.__dict__.get("_is_subscribed")
     
