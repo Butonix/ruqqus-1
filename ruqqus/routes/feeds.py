03dda63c7fe28e278646ada1970bcceae86530b4
captainmeta4 <captainmeta4@gmail.com>
2021-05-29 16:51:07 -0400

    Update feeds.py

--- ruqqus/routes/feeds.py
+++ ruqqus/routes/feeds.py
@@ -12,36 +12,36 @@ from yattag import Doc
 from ruqqus.__main__ import app, limiter
 
 
-@app.route('/feeds/<sort>', methods=["GET"])
-def feeds_public(sort=None):
+# @app.route('/feeds/<sort>', methods=["GET"])
+# def feeds_public(sort=None):
 
-    page = int(request.args.get("page", 1))
-    t = request.args.get('t')
+#     page = int(request.args.get("page", 1))
+#     t = request.args.get('t')
 
-    posts = frontlist(
-        sort=sort,
-        page=page,
-        t=t,
-        v=None,
-        hide_offensive=False,
-        ids_only=False)
+#     posts = frontlist(
+#         sort=sort,
+#         page=page,
+#         t=t,
+#         v=None,
+#         hide_offensive=False,
+#         ids_only=False)
 
-    feed = AtomFeed(title=f'Top 5 {sort} Posts from ruqqus',
-                    feed_url=request.url,
-                    url=request.url_root
-                    )
+#     feed = AtomFeed(title=f'Top 5 {sort} Posts from ruqqus',
+#                     feed_url=request.url,
+#                     url=request.url_root
+#                     )
 
-    for post in posts:
-        feed.add(post.title, post.body_html,
-                 content_type='html',
-                 author=post.author.username,
-                 url=full_link(post.permalink),
-                 updated=datetime.fromtimestamp(post.created_utc),
-                 published=datetime.fromtimestamp(post.created_utc),
-                 links=[{'href': post.url}]
-                 )
+#     for post in posts:
+#         feed.add(post.title, post.body_html,
+#                  content_type='html',
+#                  author=post.author.username,
+#                  url=full_link(post.permalink),
+#                  updated=datetime.fromtimestamp(post.created_utc),
+#                  published=datetime.fromtimestamp(post.created_utc),
+#                  links=[{'href': post.url}]
+#                  )
 
-    return feed.get_response()
+#     return feed.get_response()
 
 
 @app.route('/feeds/@<username>/<key>/<sort>', methods=["GET"])
@@ -118,29 +118,29 @@ def feeds_user(sort=None, username=None, key=None):
 
 
 #@app.route('/feeds/+<guildname>/<sort>', methods=["GET"])
-def feeds_guild(sort=None, guildname=None):
-    if not guildname:
-        abort(404)
-    guild = get_guild(guildname)
+# def feeds_guild(sort=None, guildname=None):
+#     if not guildname:
+#         abort(404)
+#     guild = get_guild(guildname)
 
-    page = int(request.args.get("page", 1))
-    t = request.args.get('t')
+#     page = int(request.args.get("page", 1))
+#     t = request.args.get('t')
 
-    ids = guild.idlist(sort=sort, page=page, t=t)
-    posts = get_posts(ids, sort=sort)
+#     ids = guild.idlist(sort=sort, page=page, t=t)
+#     posts = get_posts(ids, sort=sort)
 
-    feed = AtomFeed(title=f'{sort.capitalize()} posts from +{guild.name} on ruqqus',
-                    feed_url=request.url, url=request.url_root)
+#     feed = AtomFeed(title=f'{sort.capitalize()} posts from +{guild.name} on ruqqus',
+#                     feed_url=request.url, url=request.url_root)
 
-    for post in posts:
+#     for post in posts:
 
-        feed.add(post.title, post.body_html,
-                 content_type='html',
-                 author=post.author.username,
-                 url=full_link(post.permalink),
-                 updated=datetime.fromtimestamp(post.created_utc),
-                 published=datetime.fromtimestamp(post.created_utc),
-                 links=[{'href': post.url}]
-                 )
+#         feed.add(post.title, post.body_html,
+#                  content_type='html',
+#                  author=post.author.username,
+#                  url=full_link(post.permalink),
+#                  updated=datetime.fromtimestamp(post.created_utc),
+#                  published=datetime.fromtimestamp(post.created_utc),
+#                  links=[{'href': post.url}]
+#                  )
 
-    return feed.get_response()
+#     return feed.get_response()
