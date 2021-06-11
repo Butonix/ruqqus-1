6ba2e50fe9046369187c15ba7c9fd19fdcdbde78
kek7198 <memezdank140@gmail.com>
2021-06-11 18:22:55 -0500

    styling

--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -62,7 +62,7 @@ $('#new_email').on('input', function () {
         
       var container = document.getElementById('EMOJIS');
 
-      container.innerHTML = '<button class="emojicard" onclick="getEmoji(\'poggers\')" style="overflow: hidden;"><img width=25 src="/assets/images/emojis/poggers.gif"></button><button class="emojicard" onclick="getEmoji(\'poggers-excited\')" style="overflow: hidden;"><img width=25 src="/assets/images/emojis/poggers-excited.gif"></button><button class="emojicard" onclick="getEmoji(\'ruqqking\')" style="overflow: hidden;"><img width=25 src="/assets/images/emojis/ruqqking.gif"></button>'
+      container.innerHTML = '<button class="btn my-2 px-2 py-0" onclick="getEmoji(\'poggers\')" style="overflow: hidden;"><img width=25 src="/assets/images/emojis/poggers.gif"></button><button class="btn my-2 px-2 py-0" onclick="getEmoji(\'poggers-excited\')" style="overflow: hidden;"><img width=25 src="/assets/images/emojis/poggers-excited.gif"></button><button class="btn my-2 px-2 py-0" onclick="getEmoji(\'ruqqking\')" style="overflow: hidden;"><img width=25 src="/assets/images/emojis/ruqqking.gif"></button>'
 
   }
 
