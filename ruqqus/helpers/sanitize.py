6d677d7e30e2aab23e50ac13c452acc41f029515
kek7198 <memezdank140@gmail.com>
2021-06-12 11:38:27 -0500

    wrap img in span

--- ruqqus/helpers/sanitize.py
+++ ruqqus/helpers/sanitize.py
@@ -213,6 +213,6 @@ def sanitize(text, bio=False, linkgen=False):
         sanitized = _clean_wo_links.clean(text)
 
     for i in re.finditer(':(.{1,30}?):', sanitized):
-      if os.path.isfile(f'/home/ubuntu/ruqqus/ruqqus/assets/images/emojis/{i.group(1)}.gif'): sanitized = sanitized.replace(f':{i.group(1)}:', f'<img height=30 data-toggle="tooltip" data-original-title=":{i.group(1)}:" src="/assets/images/emojis/{i.group(1)}.gif"/>')
+      if os.path.isfile(f'/home/ubuntu/ruqqus/ruqqus/assets/images/emojis/{i.group(1)}.gif'): sanitized = sanitized.replace(f':{i.group(1)}:', f'<span data-toggle="tooltip" data-original-title=":{i.group(1)}:"><img height=30 src="/assets/images/emojis/{i.group(1)}.gif"/></span>')
 
     return sanitized
