5fba67c12b47706a1905e16460ae3ec334be45a3
captainmeta4 <captainmeta4@gmail.com>
2021-06-23 22:54:02 -0400

    37.2

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -38,7 +38,7 @@ from redis import BlockingConnectionPool, ConnectionPool
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
-_version = "2.37.1"
+_version = "2.37.2"
 
 # def time_limit(s):
 #     def wrapper_maker(f):
