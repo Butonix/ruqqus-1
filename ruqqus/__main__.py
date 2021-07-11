b51a6559232d914180f0eb9b5a9938c6785d6f2a
captainmeta4 <captainmeta4@gmail.com>
2021-07-11 09:12:31 -0400

    oops

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -38,7 +38,7 @@ from redis import BlockingConnectionPool, ConnectionPool
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
-_version = "2.37.4"
+_version = "2.37.5"
 
 # def time_limit(s):
 #     def wrapper_maker(f):
--- ruqqus/assets/js/all_js.js
+++ ruqqus/assets/js/all_js.js
@@ -490,7 +490,7 @@ function switch_css() {
   if (css.href.includes("/assets/style/main.css")) {
     post("/settings/dark_mode/1",
       callback=function(){
-        css.href="/assets/style/main_dark.css?v=2.37.4";
+        css.href="/assets/style/main_dark.css?v=2.37.5";
         dswitch.classList.remove("fa-toggle-off");
         dswitch.classList.add("fa-toggle-on");
         dswitchmobile.classList.remove("fa-toggle-off");
@@ -501,7 +501,7 @@ function switch_css() {
   else {
     post("/settings/dark_mode/0",
       callback=function(){
-        css.href="/assets/style/main.css?v=2.37.4";
+        css.href="/assets/style/main.css?v=2.37.5";
         dswitch.classList.remove("fa-toggle-on");
         dswitch.classList.add("fa-toggle-off");
         dswitchmobile.classList.remove("fa-toggle-on");
@@ -770,9 +770,10 @@ $('.kick-button-listing').click(function(event) {
 
   post_response('/mod/kick/'+bid+'/'+pid, callback=function(xhr){
     $("#post-"+bid).replaceWith(JSON.parse(xhr.response)['data'])
-    }
-  )
-}
+      }
+    )
+  }
+)
 
 //POST
 
