efc1ccefe3ace30a5a048819d776c7cb5f19dfd3
captainmeta4 <captainmeta4@gmail.com>
2021-07-11 19:18:25 -0400

    Update boards.py

--- ruqqus/routes/boards.py
+++ ruqqus/routes/boards.py
@@ -1326,11 +1326,7 @@ def board_edit_css(bid, board, v):
     if any([x in new_css for x in ["@import", "http"]]):
         return jsonify({"error": "@import and external urls are not allowed"}), 400
 
-    print(css)
-    print(css.cssText)
-    print(str(css))
-
-    board.css = css.cssText
+    board.css = decode(css.cssText, 'utf-8')
     board.css_nonce += 1
 
     g.db.add(board)
