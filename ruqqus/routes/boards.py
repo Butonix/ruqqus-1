de154c6e534487e7b5078aec1b9fcb3a4b3aa1e7
captainmeta4 <captainmeta4@gmail.com>
2021-07-13 18:36:23 -0400

    Update boards.py

--- ruqqus/routes/boards.py
+++ ruqqus/routes/boards.py
@@ -1329,13 +1329,20 @@ def board_edit_css(bid, board, v):
         cssutils.css.MarginRule,
         cssutils.css.CSSMediaRule,
         cssutils.css.CSSStyleRule,
-        cssutils.css.CSSUnknownRule
+        cssutils.css.CSSStyleDeclaration,
+        cssutils.css.CSSVariablesRule
     ]
 
-    for rule in css:
-
+    def clean_block(rule):
         if not any([isinstance(rule, x) for x in allowed_rules]):
             return jsonify({"error": f"Invalid rule: {str(rule)}"}), 422
+
+        for child in rule.children():
+            clean_block(child)
+
+
+    for rule in css:
+        clean_block(rule)
 
     css=css.cssText.decode('utf-8')
 
