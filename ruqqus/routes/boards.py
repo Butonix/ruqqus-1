1071ae773849a85653cba1fa2f294da16417d335
Captain Metaphor <captainmeta4@users.noreply.github.com>
2021-07-14 13:03:36 -0400

    Update boards.py

--- ruqqus/routes/boards.py
+++ ruqqus/routes/boards.py
@@ -1353,7 +1353,7 @@ def board_edit_css(bid, board, v):
             
             for property in rule.style.children():
                 for pv in property.propertyValue:
-                    if pv.type="URI":
+                    if pv.type=="URI":
                         return jsonify({"error":"No external links allowed."}), 422
 
         if getattr(rule, "children", None):
