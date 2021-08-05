208932fd78759876d20c559ec51b3cdc05eea0fc
Captain Metaphor <captainmeta4@users.noreply.github.com>
2021-08-05 11:30:18 -0400

    Update discord.py

--- ruqqus/helpers/discord.py
+++ ruqqus/helpers/discord.py
@@ -61,7 +61,7 @@ def discord_log_event(action, target_user, admin_user, reason=None):
         "embeds":[
             {
                 "title": action,
-                "url": f"https://ruqqus.com",
+                "url": f"https://{app.config['SERVER_NAME']}{target_user.permalink}",
                 "color": 8415957,
                 "author": {
                     "name": admin_user.username,
