760415f17e575f07576bcb0ce48daf87b32f45aa
Captain Metaphor <captainmeta4@users.noreply.github.com>
2021-08-11 16:43:39 -0400

    Update __main__.py

--- ruqqus/__main__.py
+++ ruqqus/__main__.py
@@ -213,7 +213,7 @@ limiter = Limiter(
     default_limits=["100/minute"],
     headers_enabled=True,
     strategy="fixed-window",
-    storage_options={'max_connections':1000}
+    storage_options={'connection_pool':redispool}
 )
 
 # setup db
